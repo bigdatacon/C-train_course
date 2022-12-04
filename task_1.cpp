92e0937cf1b280de1d8497a688c43cde42fc14fb
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2022-12-04 21:06:21 +0300

    Update task_1.cpp

--- task_1.cpp
+++ task_1.cpp
@@ -9,11 +9,12 @@ using namespace std;
 const int MAX_RESULT_DOCUMENT_COUNT = 5;
 
 /*
-Функция CreateSearchServer должна иметь пустой список параметров и возвращать SearchServer.
-В первую очередь создайте объект для заполнения: SearchServer search_server;.
-Затем считайте и задайте стоп-слова: search_server.SetStopWords(/* тут строка из ввода */
-/*);.
-И, наконец, считайте количество документов и в цикле с соответствующим числом итераций вызывайте метод search_server.AddDocument, передавая id документа и его содержимое.
+Перенесите FindAllDocuments в приватную часть класса, оставьте только один параметр — множество query_words. Переделать нужно все участки кода, в которых используется массив documents, множество stop_words и функция (теперь уже метод) SplitIntoWordsNoStop.
+Перенесите FindTopDocuments в публичную часть класса и проделайте те же преобразования.
+Перенесите функцию MatchDocument в приватную часть класса, сделав её статическим методом.ъ
+Функцию ParseQuery тоже нужно сделать методом класса.
+В функции main вызовите CreateSearchServer и положите результат в переменную (пока не константную!). Затем вызовите метод FindTopDocuments и запустите цикл по его результату с выводом ответа.
+Все используемые в методах класса сущности — константы, типы, функции — объявите до кода класса.
 */
 
 string ReadLine() {
@@ -59,7 +60,21 @@ vector<string> SplitIntoWordsNoStop(const string& text, const set<string>& stop_
     return words;
 }
 
+/*
+1 Перенесите FindAllDocuments в приватную часть класса, оставьте только один параметр — множество query_words. 
+????? 
+Переделать нужно все участки кода, в которых используется массив documents, множество stop_words и функция (теперь уже метод) SplitIntoWordsNoStop.
 
+3 Перенесите FindTopDocuments в публичную часть класса и проделайте те же преобразования.
+
+4 Перенесите функцию MatchDocument в приватную часть класса, сделав её статическим методом.ъ
+
+5 Функцию ParseQuery тоже нужно сделать методом класса.
+
+
+В функции main вызовите CreateSearchServer и положите результат в переменную (пока не константную!). Затем вызовите метод FindTopDocuments и запустите цикл по его результату с выводом ответа.
+Все используемые в методах класса сущности — константы, типы, функции — объявите до кода класса.
+*/
 
 class SearchServer {
 public:
@@ -74,6 +89,20 @@ public:
         }
         return stop_words_;
     }
+    
+    vector<Document> FindTopDocuments(/*const vector<DocumentContent>& documents,
+                                  const set<string>& stop_words,*/ const string& raw_query) {
+    const set<string> query_words = ParseQuery(raw_query, stop_words);
+    auto matched_documents = FindAllDocuments(documents, query_words);
+ 
+    sort(matched_documents.begin(), matched_documents.end(), HasDocumentGreaterRelevance);
+    if (matched_documents.size() > MAX_RESULT_DOCUMENT_COUNT) {
+        matched_documents.resize(MAX_RESULT_DOCUMENT_COUNT);
+    }
+    return matched_documents;
+    }
+    
+    
 private:
     struct DocumentContent {
         int id = 0;
@@ -90,6 +119,41 @@ private:
         }
     }
     return words;
+    }
+    
+    vector<Document> FindAllDocuments(/*const vector<DocumentContent>& documents,*/ const set<string>& query_words)
+    {vector<Document> matched_documents;
+    for (const auto& document : documents) {
+        const int relevance = MatchDocument(document, query_words);
+        if (relevance > 0) {
+            matched_documents.push_back({document.id, relevance});
+        }
+    }
+    return matched_documents;
+    }
+    
+    static int MatchDocument(const DocumentContent& content, const set<string>& query_words) {
+    if (query_words.empty()) {
+        return 0;
+    }
+    set<string> matched_words;
+    for (const string& word : content.words) {
+        if (matched_words.count(word) != 0) {
+            continue;
+        }
+        if (query_words.count(word) != 0) {
+            matched_words.insert(word);
+        }
+    }
+    return static_cast<int>(matched_words.size());
+    }
+    
+    set<string> ParseQuery(const string& text, const set<string>& stop_words) {
+    set<string> query_words;
+    for (const string& word : SplitIntoWordsNoStop(text, stop_words)) {
+        query_words.insert(word);
+    }
+    return query_words;
     }
     
 
@@ -119,3 +183,18 @@ SearchServer CreateSearchServer() {
 
     return search_server;
 }
+
+int main() {
+    
+    /*В функции main вызовите CreateSearchServer и положите результат в переменную (пока не константную!). Затем вызовите метод FindTopDocuments и запустите цикл по его результату с выводом ответа.
+Все используемые в методах класса сущности — константы, типы, функции — объявите до кода класса.*/
+    
+    SearchServer result = CreateSearchServer();
+    for (auto [document_id, relevance] : result.FindTopDocuments()) {
+        cout << "{ document_id = "s << document_id << ", relevance = "s << relevance << " }"s
+            << endl;
+    }
+    
+    
+}
+
