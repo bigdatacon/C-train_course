094c9198ca1632dd86306b4bf31d9532ad560ad0
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2022-12-06 15:16:20 +0300

    Update task_current.cpp

--- task_current.cpp
+++ task_current.cpp
@@ -2,14 +2,14 @@
 #include <string>
 #include <vector>
 #include <iostream>
-
+#include <algorithm>
 using namespace std;
 
 
 int CountAndAddNewDogs(const vector<string>& new_dogs, const map<string, int>& max_amount,
                        map<string, int>& shelter) {
-    return count_if(new_dogs.begin(), new_dogs.end(), [&shelter, max_amount](string breed_new_dog){
-        if (shelter[ breed_new_dog] < max_amount[breed_new_dog]){return true;}
+    return count_if(new_dogs.begin(), new_dogs.end(), [&shelter, &max_amount](string breed_new_dog){
+        if (shelter.at(breed_new_dog) < max_amount.at(breed_new_dog)){return true;}
         else {return false;}
                 });                                                                       
 }
@@ -22,16 +22,16 @@ int main() {
     map<string, int> max_amount = {{"landseer"s, 2}, {"otterhound"s, 3}, {"pekingese"s, 4}, {"pointer"s, 7}};
     vector<string> new_dogs = {"landseer"s, "otterhound"s, "otterhound"s, "otterhound"s, "pointer"s};
     
-    /*for (auto [breed, q] : shelter){cout << "breed " << breed << " q " << q << endl;}
+    for (auto [breed, q] : shelter){cout << "breed " << breed << " q " << q << endl;}
     int dog_arrival= 0;
     for (auto breed : new_dogs){
-        [&shelter, max_amount](string breed){
-            if (shelter[breed]!=0 && shelter[breed]<max_amount[breed] ){++shelter[breed]; ++dog_arrival;}
-        }
-        
-    }*/
+        [&shelter, max_amount, dog_arrival](string breed){
+            if (shelter.at(breed)!=0 && shelter.at(breed)<max_amount.at(breed) ){++shelter[breed]; ++dog_arrival;}
+        };    
+    }
     
-
-    сout << "NEW " <<  CountAndAddNewDogs(new_dogs, max_amount, shelter ) << endl;
+    for (auto [breed, q] : shelter){cout << "breednew " << breed << " q " << q << endl;}
+    
+    //cout << "NEW " <<  CountAndAddNewDogs(new_dogs, max_amount, shelter ) << endl;
     return 0;
 }
