dadd17cc035b7f6d0dd26783b262532ca23e597a
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2022-12-06 16:06:52 +0300

    Update task_current.cpp

--- task_current.cpp
+++ task_current.cpp
@@ -23,7 +23,7 @@ using namespace std;
 int CountAndAddNewDogs(const vector<string>& new_dogs, const map<string, int>& max_amount,
                        map<string, int>& shelter) {
     return count_if(new_dogs.begin(), new_dogs.end(), [&shelter, &max_amount](string breed_new_dog){
-        if (shelter.at(breed_new_dog) < max_amount.at(breed_new_dog)){return true;}
+        if (shelter.at(breed_new_dog) < max_amount.at(breed_new_dog)){++shelter[breed_new_dog]; return true;}
         else {return false;}
                 });                                                                       
 }
