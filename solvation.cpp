2b1fdbd9cd214a79f8e12a3cabb9ef930f98f6cd
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2022-12-08 22:16:33 +0300

    Update solvation.cpp

--- solvation.cpp
+++ solvation.cpp
@@ -5,6 +5,7 @@
 #include <utility>
 #include <vector>
 #include <map>
+#include <cmath>
 
 using namespace std;
 
@@ -53,7 +54,6 @@ struct DocumentQuery {
     set<string> pluswords;
 };
 
-/*Вместо vector<DocumentContent> documents_ объявите поле map<string, set<int>> word_to_documents_. В нём будет храниться инвертированный индекс документов. Структуру DocumentContent удалите.*/
 
 
 class SearchServer {
@@ -63,13 +63,20 @@ public:
             stop_words_.insert(word);
         }
     }
-    /*2. В методе AddDocument переберите в цикле все слова документа, кроме стоп-слов. Вставьте в множество документов, соответствующих очередному слову документа, id вставляемого документа. Так очередной документ будет добавлен в инвертированный индекс.*/
-    void AddDocument(int document_id, const string& document) {
+
+    void AddDocument(int document_id, const string& document, const int document_count) {
         const vector<string> words = SplitIntoWordsNoStop(document);
         //map<string, set<int>> word_to_documents_;
+        //map<string, double> word_idf_;
+
+
         if (words.size()!=0) {
         for (/*const*/ auto word :  words){word_to_documents_[word].insert(document_id);}
                            }
+        //добавляю в word_idf_ : словл : IDF 
+        for (const auto& [key, value]: word_to_documents_) {
+        word_idf_.insert({key, log(document_count/value.size())}); 
+    }
     }
 
     vector<Document> FindTopDocuments(const string& raw_query) const {
@@ -87,7 +94,8 @@ public:
     }
 
 private:
-
+    map<string, double> word_idf_;
+    map<string, map<int, double>> word_to_document_freqs_;
     map<string, set<int>> word_to_documents_;
 
     set<string> stop_words_;
@@ -149,16 +157,13 @@ private:
                                                                 
             return false; 
 }
-    
-    
-    /*3. В методе FindAllDocuments объявите переменную document_to_relevance типа map<int, int>. В ней ключ — id найденного документа, а значение — релевантность соответствующего документа. Она равна количеству плюс-слов, найденных в нём.*/
-    /*4. В методе FindAllDocuments переберите в цикле все плюс-слова поискового запроса. Если в word_to_documents_ есть плюс-слово, увеличьте в document_to_relevance релевантности всех документов, где это слово найдено. Так вы соберёте все документы, которые содержат плюс-слова запроса.*/
-    /*5. Исключите из результатов поиска все документы, в которых есть минус-слова. В методе FindAllDocuments переберите в цикле все минус-слова поискового запроса. Если в word_to_documents_ есть минус-слово, удалите из document_to_relevance все документы с этим минус-словом. Так в document_to_relevance останутся только подходящие документы.*/
-    /*6. Перенесите id и релевантности документов из document_to_relevance в vector<Document> и верните результирующий вектор.*/
-    
+    /*Как и раньше, для поиска документов и вычисления их релевантности хватит двойной вложенности циклов for: внешний — по словам запроса, внутренний — по документам, где это слово встречается.
+Помимо добавления количества документов, достаточно будет переделать словарь «слово → документы» в более сложную структуру map<string, map<int, double>> word_to_document_freqs_, которая сопоставляет каждому слову словарь «документ → TF». Подумайте, как с этими данными быстро вычислять IDF.
+Для хранения релевантности найденных документов используйте контейнер map<int, double>. Ключи в нём — id найденных документов, а значения вычисляются по формуле TF-IDF, описанной в уроке.*/
     vector<Document> FindAllDocuments(const set<string>& query_words, const set<string>& minus_words) const {
         map<int, int> document_to_relevance;
         vector<Document> matched_documents;
+        map<string, map<int, double>> word_to_document_freqs_;
         
         if (CheckAnyWordsinDoc(query_words)){
         
@@ -178,7 +183,8 @@ private:
             for (auto id : doc_ids) {/*--document_to_relevance[id]; */
                 std::map<int,int>::iterator it;
                 it = document_to_relevance.find(id);
-                                      document_to_relevance.erase (it);
+                if (it!=document_to_relevance.end()){
+                                      document_to_relevance.erase (it);};
                                     }
                                                          }
                                         } 
@@ -216,14 +222,17 @@ SearchServer CreateSearchServer() {
 
     const int document_count = ReadLineWithNumber();
     for (int document_id = 0; document_id < document_count; ++document_id) {
-        search_server.AddDocument(document_id, ReadLine());
+        search_server.AddDocument(document_id, ReadLine(), document_count);
     }
 
     return search_server;
 }
 
+// IDF  =log(количество документов / количество документов) где слово встречается. Рассчитывается для слова 
 
+// TF  = количество раз сколько слово встречается в документе / на количество слов в документе. Рассчитывается для каждого слова и докумена 
 
+// TF-IDF: считается для каждого документа и строки запроса. Для этого IDF каждого слова запроса * TF каждого слова в запроса  и далее суммируются ти произведения. Так получается TF IDF документа.
 
 int main() {
     const SearchServer search_server = CreateSearchServer();
