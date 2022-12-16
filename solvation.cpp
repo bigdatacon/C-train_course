91988c75697b498201d1880698a23ee231917d88
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2022-12-16 08:03:17 +0300

    Update solvation.cpp

--- solvation.cpp
+++ solvation.cpp
@@ -1,64 +1,109 @@
-#include <algorithm>
 #include <iostream>
+#include <map>
+#include <string>
+#include <tuple>
 #include <vector>
 
 using namespace std;
 
-/*Задание 2
-Введём статусы документов: ACTUAL (актуальные), EXPIRED (просроченные) и DELETED (удалённые). Исправьте компаратор так, чтобы после сортировки порядок документов был следующим:
-актуальные,
-просроченные,
-удалённые.
-Внутри каждого статуса документы должны быть упорядочены по убыванию рейтинга, а затем по убыванию релевантности.
-Код из тренажёра должен вывести следующее:
-104 0 0.3 5
-102 0 1.2 4
-100 0 0.5 4
-101 1 0.5 4
-103 2 1.2 4 */
-
-/*Подставить все три поля в пару, к сожалению, не выйдет. Но можно написать логическое выражение ещё большей вложенности. А можно скомбинировать два подхода: самостоятельно сравнить статусы и, если они равны, — сравнить пары двух оставшихся характеристик.*/
-
-enum class Status { ACTUAL, EXPIRED, DELETED };
-
-struct Document {
-    int id;
-    Status status;
-    double relevance;
-    int rating;
+// Перечислимый тип для статуса задачи
+enum class TaskStatus {
+    NEW,          // новая
+    IN_PROGRESS,  // в разработке
+    TESTING,      // на тестировании
+    DONE          // завершена
 };
 
-// было 
-/*void SortDocuments(vector<Document>& matched_documents) {
-    sort(matched_documents.begin(), matched_documents.end(),
-         [](const Document& lhs, const Document& rhs) {
-             return pair(lhs.rating, lhs.relevance) > pair(rhs.rating, rhs.relevance);
-         });
-}*/
+// Объявляем тип-синоним для map<TaskStatus, int>,
+// позволяющего хранить количество задач каждого статуса
+using TasksInfo = map<TaskStatus, int>;
 
-// стало
-void SortDocuments(vector<Document>& matched_documents) {
-    sort(matched_documents.begin(), matched_documents.end(),
-         [](const Document& lhs, const Document& rhs) {
-             if (lhs.status==rhs.status){
-             
-             return pair(lhs.rating, lhs.relevance) > pair(rhs.rating, rhs.relevance);}
-             return false;
-         });
-} 
+class TeamTasks {
+public:
+    // Получить статистику по статусам задач конкретного разработчика
+    const TasksInfo& GetPersonTasksInfo(const string& person) const{
+    	return persons_tasks.at(person);
 
+    };
+
+    // Добавить новую задачу (в статусе NEW) для конкретного разработчитка
+    void AddNewTask(const string& person){
+    	++persons_tasks[person][TaskStatus::NEW];
+    };
+
+    // Обновить статусы по данному количеству задач конкретного разработчика,
+    // подробности см. ниже
+    /*tuple<TasksInfo, TasksInfo>*/ void PerformPersonTasks(const string& person , int task_count){
+    	TasksInfo updated_tasks;
+    	TasksInfo untached_tasks;
+    	//int counts = task_count;
+        for(const auto [status, quantity] : persons_tasks[person])
+        {
+        	//cout <<"Persons task quantity: "s << quantity << " status "s<< status <<  endl;
+        	cout << static_cast<int>(persons_tasks[person][status])<< endl;
+        	cout << static_cast<int>(persons_tasks[person][static_cast<int>(status)+1])<< endl;
+        }
+        TasksInfo persons_tasks_statuses  = persons_tasks[person];
+        cout << persons_tasks_statuses[(TaskStatus)(0)]<< endl;
+        cout << persons_tasks_statuses[TaskStatus::NEW]<< endl;
+        cout << persons_tasks_statuses[TaskStatus::NEW]<< endl;
+        cout << static_cast<int>(persons_tasks[person][TaskStatus::NEW])<< endl;
+        cout << static_cast<int>(persons_tasks[person][(TaskStatus)(1)])<< endl;
+
+        cout << static_cast<int>(persons_tasks[person][TaskStatus::NEW]) ==  task_count << endl;
+
+
+    };
+
+
+private:
+// храним имя разработчика и статусы+количество его задач
+map<string, TasksInfo> persons_tasks;
+};
+
+
+
+/////////////////////////////////////////////////////////////////////////НЕ ОТНОСИТСЯ ////////////////////////////////
+
+// Принимаем словарь по значению, чтобы иметь возможность
+// обращаться к отсутствующим ключам с помощью [] и получать 0,
+// не меняя при этом исходный словарь.
+void PrintTasksInfo(TasksInfo tasks_info) {
+    cout << tasks_info[TaskStatus::NEW] << " new tasks"s
+         << ", "s << tasks_info[TaskStatus::IN_PROGRESS] << " tasks in progress"s
+         << ", "s << tasks_info[TaskStatus::TESTING] << " tasks are being tested"s
+         << ", "s << tasks_info[TaskStatus::DONE] << " tasks are done"s << endl;
+}
 
 int main() {
-    vector<Document> documents = {
-        {100, Status::ACTUAL, 0.5, 4}, {101, Status::EXPIRED, 0.5, 4},
-        {102, Status::ACTUAL, 1.2, 4}, {103, Status::DELETED, 1.2, 4},
-        {104, Status::ACTUAL, 0.3, 5},
-    };
-    SortDocuments(documents);
-    for (const Document& document : documents) {
-        cout << document.id << ' ' << static_cast<int>(document.status) << ' ' << document.relevance
-             << ' ' << document.rating << endl;
+    TeamTasks tasks;
+    tasks.AddNewTask("Ilia"s);
+    for (int i = 0; i < 3; ++i) {
+        tasks.AddNewTask("Ivan"s);
     }
+    cout << "Ilia's tasks: "s;
+    PrintTasksInfo(tasks.GetPersonTasksInfo("Ilia"s));
+    cout << "Ivan's tasks: "s;
+    PrintTasksInfo(tasks.GetPersonTasksInfo("Ivan"s));
 
-    return 0;
+    tasks.PerformPersonTasks("Ivan"s, 2);
+
+    //tie(updated_tasks, untouched_tasks) = tasks.PerformPersonTasks("Ivan"s, 2);
+
+
+
+    /*TasksInfo updated_tasks, untouched_tasks;
+
+    tie(updated_tasks, untouched_tasks) = tasks.PerformPersonTasks("Ivan"s, 2);
+    cout << "Updated Ivan's tasks: "s;
+    PrintTasksInfo(updated_tasks);
+    cout << "Untouched Ivan's tasks: "s;
+    PrintTasksInfo(untouched_tasks);
+
+    tie(updated_tasks, untouched_tasks) = tasks.PerformPersonTasks("Ivan"s, 2);
+    cout << "Updated Ivan's tasks: "s;
+    PrintTasksInfo(updated_tasks);
+    cout << "Untouched Ivan's tasks: "s;
+    PrintTasksInfo(untouched_tasks);*/
 }
+
