9b364a4964119fb6af83d6204f77c41471240499
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2022-12-18 22:02:11 +0300

    Update firstcode.cpp

--- firstcode.cpp
+++ firstcode.cpp
@@ -1,68 +1,29 @@
-#include <iostream>
-#include <map>
-#include <string>
-#include <vector>
-#include <algorithm>
-using namespace std;
 
 /*
 Задание
-Убедимся, что полученная шаблонная функция может иметь и более сложный параметр-тип. Примените шаблонную функцию ComputeTermFreqs и определите, какое животное встречается наибольшее число раз. Животное в этом задании задаётся парой pair<string, int>, содержащей имя и возраст.
-Если максимальное число раз встречаются несколько животных, выведите наименьшего из них. Гарантируется, что вектор содержит хотя бы одно животное.
-Попробуйте сделать то же самое со структурой Animal вместо пары. Ничего не получится, если не научите компилятор сравнивать эти структуры. Это нормально.
+Реализуйте шаблонную функцию ComputeTfIdfs, которая вычисляет TF-IDF заданного слова для каждого документа из набора.
+Первый параметр documents — контейнер документов. циклом for (const auto& document : documents) можно перебрать все документы в нём, а в каждом документе — все слова. Тип слова, документа и набора документов может быть произвольным — ваша функция должна быть готова к любым, если есть возможность итерирования. Гарантируется, что и набор документов, и каждый отдельный документ имеют методы begin, end и size. Например, документом может быть строка, а словом — символ.
+Второй параметр term — слово, у которого нужно вычислить TF-IDF. Его тип совпадает с типом слов, которые получаются при итерировании по документу.
+Функция должна вернуть вектор вещественных чисел, который совпадает по длине с количеством документов. В первом элементе должен лежать TF-IDF слова term для первого документа, в последнем элементе — TF-IDF слова term для последнего документа.
+Напоминаем, что TF-IDF — это произведение TF и IDF. TF слова term в документе — доля слов документа, совпадающих с term. IDF вычисляется для слова независимо от конкретного документа и равен логарифму (функция log из <cmath>) от documents.size() / document_freq, где знаменатель — это количество документов, содержащих term.
+
+Пример из тренажёра должен вывести 0.081093 0.101366 0.
+
+Ваша функция должна иметь два шаблонных параметра: Documents для набора документов и Term для слова. Указывайте их через запятую. Тип документа при этом никак не называйте: достаточно написать auto.
+Используйте алгоритм count для подсчёта количества вхождений слова в документ.
+Не забывайте перед делением приводить целые числа к double оператором static_cast.
 */
-
-/*
-Вызовите ComputeTermFreqs для вектора animals, проитерируйтесь по результату и найдите ключ с наибольшим значением. Если очередной ключ имеет частоту, равную уже найденной максимальной, не переписывайте им ключ-ответ.
-*/
-
-
-template <typename Term>
-map<Term, int> ComputeTermFreqs(const vector<Term>& terms) {
-    map<Term, int> term_freqs;
-    for (const Term& term : terms) {
-        ++term_freqs[term];
-    }
-    return term_freqs;
-}
-
-
-pair<string, int> FindMaxFreqAnimal(const vector<pair<string, int>>& animals) {
-    // верните животного с максимальной частотой
-    map<pair<string, int>, int> animal_freqs = ComputeTermFreqs(animals);
-
-    int max_q = 0;
-    pair<string, int> min_par;
-
-    for (auto [pair , q] : animal_freqs){/*cout << " pair.first : " <<  pair.first << " pair.second :"s << pair.second << " quantity : " <<  q << endl;*/
-    if (q>max_q){max_q = q; min_par = pair;}
-    else if ( (q==max_q) && (min_par<pair) ) {continue; }
-    else if ((q==max_q) && (min_par>pair) ) {min_par = pair;}
-    else continue;
-    }
-    pair<string, int> max_key = {"jd" , 0};
-    //sort(animal_freqs.begin(), animal_freqs.end()); - не работает
-    /*sort(animal_freqs.begin(), animal_freqs.end(), [](map<pair<string, int>& a, map<pair<string, int>& b)
-    {
-        return a.second > b.second;
-    }
-    );*/ //- Так тоже не работает
-
-    max_key = min_par;
-    return max_key;
-}
-
-
-
 
 int main() {
-    const vector<pair<string, int>> animals = {
-        {"Murka"s, 5},  // 5-летняя Мурка
-        {"Belka"s, 6},  // 6-летняя Белка
-        {"Murka"s, 7},  // 7-летняя Мурка не та же, что 5-летняя!
-        {"Murka"s, 5},  // Снова 5-летняя Мурка
-        {"Belka"s, 6},  // Снова 6-летняя Белка
+    const vector<vector<string>> documents = {
+        {"белый"s, "кот"s, "и"s, "модный"s, "ошейник"s},
+        {"пушистый"s, "кот"s, "пушистый"s, "хвост"s},
+        {"ухоженный"s, "пёс"s, "выразительные"s, "глаза"s},
     };
-    const pair<string, int> max_freq_animal = FindMaxFreqAnimal(animals);
-    cout << max_freq_animal.first << " "s << max_freq_animal.second << endl;
+    const auto& tf_idfs = ComputeTfIdfs(documents, "кот"s);
+    for (const double tf_idf : tf_idfs) {
+        cout << tf_idf << " "s;
+    }
+    cout << endl;
+    return 0;
 }
