f8103b1890c85b9738cc6391e9f263c599fa5c99
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2022-12-19 17:12:39 +0300

    Update task_1.cpp

--- task_1.cpp
+++ task_1.cpp
@@ -1,66 +1,62 @@
-#include <algorithm>
 #include <iostream>
 #include <vector>
-#include <tuple>
-
+#include <set>
 using namespace std;
 
-// Задание 
 /*
-Перепишите компаратор из предыдущего урока, применив кортежи.
-Код должен вывести следующее:
-104 0 0.3 5
-102 0 1.2 4
-100 0 0.5 4
-101 1 0.5 4
-103 2 1.2 4 
+Избавьтесь от дублирования кода. Вынесите общую функциональность вывода в шаблонную функцию Print, принимающую ссылку на поток вывода и объект шаблонного типа, элементы которого нужно вывести. Это означает, что последовательный вывод элементов контейнера теперь должен располагаться внутри функции Print. Вызовите её из операторов вывода вектора и множества.
+
+Не забудьте назвать вспомогательную функцию Print, первым её аргументом сделать ссылку на поток, а вторым — контейнер шаблонного типа. Точно так же вы делали с набором документов Documents.
 */
 
-
-/*Документы нужно упорядочить сначала по возрастанию статусов, а при их равенстве — по убыванию рейтинга, затем по убыванию релевантности. Но при сравнении кортежей все компоненты сравниваются одинаково. Поэтому при создании кортежа удобно умножить рейтинг и релевантность на −1.*/
-/*
-
-enum class Status { ACTUAL, EXPIRED, DELETED };
-
-struct Document {
-    int id;
-    Status status;
-    double relevance;
-    int rating;
-};
-
-
-// стало
-/*void SortDocuments(vector<Document>& matched_documents) {
-    vector<tuple> matched_tuple;
-    for (auto el : matched_documents){matched_tuple.push_back(tuple(el.id, el.status, el.relevance*-1, el.rating*-1))};
+template <typename Object, typename Term >
+ostream& Print(ostream& out,  const Object<Term>& container) {
+    bool first = true;
+    for ( const Term& element : container) {
+        if (!first){
+        out << ", "s;}
+        
+        first = false;
+        
+        out << element;
+    }
+    return out;
+}  
     
-    sort(matched_tuple.begin(), matched_tuple.end(),
-         [](const tuple& lhs, const tuple& rhs) {
+    
 
-             return lhs< rhs;
-         });
-} */
+ostream& operator<<(ostream& out,  const set<Term>& container) {
+    bool first = true;
+    for ( const Term& element : container) {
+        if (!first){
+        out << ", "s;}
+        
+        first = false;
+        
+        out << element;
+    }
+    return out;
+}  
 
+template <typename Term >
+ostream& operator<<(ostream& out,  const vector<Term>& container) {
+    bool first = true;
+    for ( const Term& element : container) {
+        if (!first){
+        out << ", "s;}
+        
+        first = false;
+        
+        out << element;
+    }
+    return out;
+}  
+   
 
 int main() {
-    vector<Document> documents = {
-        {100, Status::ACTUAL, 0.5, 4}, {101, Status::EXPIRED, 0.5, 4},
-        {102, Status::ACTUAL, 1.2, 4}, {103, Status::DELETED, 1.2, 4},
-        {104, Status::ACTUAL, 0.3, 5},
-    };
-    /*SortDocuments(documents);
-    for (const Document& document : documents) {
-        cout << document.id << ' ' << static_cast<int>(document.status) << ' ' << document.relevance
-             << ' ' << document.rating << endl;
-    }
-    */
-    // проверяю что работает заполнение тюпла 
-    vector<tuple<Document>> matched_tuple;
-    for (auto el : documents){matched_tuple.push_back(tuple(el.id, el.status, el.relevance*-1, el.rating*-1));};
-    
-    for (auto el : matched_tuple){cout << el.id << endl;}
-    
-    
+    const vector<int> ages = {10, 5, 2, 12};
+    cout << ages << endl;
+    const  set<string> cats = {"Мурка"s, "Белка"s, "Георгий"s, "Рюрик"s};
+    cout << cats << endl;
     return 0;
 }
