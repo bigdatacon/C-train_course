8376a06f49d7f48b40ad94eddc58b601d2f1628e
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2022-12-20 12:41:41 +0300

    Update firstcode.cpp

--- firstcode.cpp
+++ firstcode.cpp
@@ -1,114 +1,60 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
-#include <set>
-#include <map>
-using namespace std;
 
+using namespace std;
 /*
-Научитесь выводить словари. Содержимое словаря должно быть заключено в угловые скобки, а каждая пара ключ-значение — в круглые.
-Пусть функция Print выводит словарь без угловых скобок. Ни для каких других типов, кроме векторов, множеств и словарей, вызывать Print не нужно.
-{Белка, Георгий, Мурка, Рюрик}
-[10, 5, 2, 12]
-<(Белка, 5), (Георгий, 2), (Мурка, 10), (Рюрик, 12)> 
-Напишите ещё одну версию operator<< с двумя шаблонными параметрами — для map<Key, Value>.
-Функция Print, итерируясь по словарю, будет пытаться вывести пару — напишите и для неё operator<<.
-При вызове одной функции из другой компилятор «видит» только те из них, что написаны выше.
-Поэтому выше всех должен быть оператор вывода пары, а сразу за ним — универсальная Print.
+Задание 1
+Реализуйте функцию SortBy в соответствии с указанными требованиями. В процессе у вас могут возникнуть проблемы. Постарайтесь решить их самостоятельно: читайте подсказки.
+Код должен выводить:
+Мурка Белка Георгий Рюрик 
+Белка Георгий Мурка Рюрик 
+Мурка Георгий Рюрик Белка 
+
+ПОДСКАЗКИ:
+Чтобы написать компаратор, укажите типы параметров lhs и rhs. С ходу это сделать не получается, потому что известен только тип контейнера. Здесь поможет auto. Задайте список параметров const auto& lhs, const auto& rhs, и тогда сам компаратор окажется по сути шаблонной функцией.
+Чтобы вызвать в компараторе-лямбде key_mapper, захватите его: [key_mapper](const auto& lhs, const auto& rhs).
 */
 
 
-template <typename Container>
-void Print(ostream& out,  const Container& container) {
-    bool first = true;
-    for ( const auto& element : container) {
-        if (!first){
-        out << ", "s;}
-        
-        first = false;
-        
-        out <<  element;
-    }
+struct Animal {
+    string name;
+    int age;
+    double weight;
+};
+
+template <typename Container, typename KeyMapper>
+void SortBy(Container& container, KeyMapper key_mapper) {
+    // теперь можно сортировать контейнер
+    // с компаратором key_mapper(lhs) < key_mapper(rhs)
 }
 
-// пишу версию оператора для словаря
-/*template <typename Key,  typename Value>
-ostream& operator<<(ostream& out,  const map<Key, Value>& container) {
-    bool first = true;
-    for ( const auto& [key, value] : container) {
-        if (!first){
-        out << ", "s;}
-        
-        first = false;      
-        out << "("s;
-        out <<  key ;
-        out << ", "s;
-        out << value;
-        out << ")"s;
-        
+void PrintNames(const vector<Animal>& animals) {
+    for (const Animal& animal : animals) {
+        cout << animal.name << ' ';
     }
-    return  out;
-} */
-
-template <typename Key,  typename Value>
-void PrintMap(ostream& out,  const map<Key, Value>& container) {
-    bool first = true;
-    for ( const auto& [key, value] : container) {
-        if (!first){
-        out << ", "s;}
-        
-        first = false;      
-        out << "("s;
-        out <<  key ;
-        out << ", "s;
-        out << value;
-        out << ")"s;
-        
-    }
-} 
-template <typename Key,  typename Value>
-ostream& operator<<(ostream& out,  const map<Key, Value>& container) {
-    out << "<"s;
-    PrintMap(out, container);
-    out << ">"s;
-    return  out;
-}  
-
-
-    
-template <typename Term >
-ostream& operator<<(ostream& out,  const set<Term>& container) {
-    out << "{"s;
-    Print(out, container);
-    out << "}"s;
-    return  out;
-}  
-
-template <typename Term >
-ostream& operator<<(ostream& out,  const vector<Term>& container) {
-    out << "["s;
-    Print(out, container);
-    out << "]"s;
-    return out;
-}  
-   
+    cout << endl;
+}
 
 int main() {
-    /*const vector<int> ages = {10, 5, 2, 12};
-    cout << ages << endl;
-    const  set<string> cats = {"Мурка"s, "Белка"s, "Георгий"s, "Рюрик"s};
-    cout << cats << endl;
-    return 0;*/
-    
-    const set<string> cats = {"Мурка"s, "Белка"s, "Георгий"s, "Рюрик"s};
-    cout << cats << endl;
-    const vector<int> ages = {10, 5, 2, 12};
-    cout << ages << endl;
-    const map<string, int> cat_ages = {
-                                        {"Мурка"s, 10}, 
-                                        {"Белка"s, 5},
-                                        {"Георгий"s, 2}, 
-                                        {"Рюрик"s, 12}
-                                    };
-    cout << cat_ages<< endl;
+    vector<Animal> animals = {
+        {"Мурка"s, 10, 5},
+        {"Белка"s, 5, 1.5},
+        {"Георгий"s, 2, 4.5},
+        {"Рюрик"s, 12, 3.1},
+    };
+
+    PrintNames(animals);
+
+    SortBy(animals, [](const Animal& animal) {
+        return animal.name;
+    });
+    PrintNames(animals);
+
+    SortBy(animals, [](const Animal& animal) {
+        return -animal.weight;
+    });
+    PrintNames(animals);
+
     return 0;
 }
