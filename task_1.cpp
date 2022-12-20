90af85189fad0b98ba261bbe8a063b557b787863
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2022-12-20 04:45:28 +0300

    Update task_1.cpp

--- task_1.cpp
+++ task_1.cpp
@@ -1,62 +1,117 @@
 #include <iostream>
 #include <vector>
 #include <set>
+#include <map>
 using namespace std;
 
 /*
-Избавьтесь от дублирования кода. Вынесите общую функциональность вывода в шаблонную функцию Print, принимающую ссылку на поток вывода и объект шаблонного типа, элементы которого нужно вывести. Это означает, что последовательный вывод элементов контейнера теперь должен располагаться внутри функции Print. Вызовите её из операторов вывода вектора и множества.
-
-Не забудьте назвать вспомогательную функцию Print, первым её аргументом сделать ссылку на поток, а вторым — контейнер шаблонного типа. Точно так же вы делали с набором документов Documents.
+Научитесь выводить словари. Содержимое словаря должно быть заключено в угловые скобки, а каждая пара ключ-значение — в круглые.
+Пусть функция Print выводит словарь без угловых скобок. Ни для каких других типов, кроме векторов, множеств и словарей, вызывать Print не нужно.
+{Белка, Георгий, Мурка, Рюрик}
+[10, 5, 2, 12]
+<(Белка, 5), (Георгий, 2), (Мурка, 10), (Рюрик, 12)> 
+Напишите ещё одну версию operator<< с двумя шаблонными параметрами — для map<Key, Value>.
+Функция Print, итерируясь по словарю, будет пытаться вывести пару — напишите и для неё operator<<.
+При вызове одной функции из другой компилятор «видит» только те из них, что написаны выше.
+Поэтому выше всех должен быть оператор вывода пары, а сразу за ним — универсальная Print.
 */
 
-template <typename Object, typename Term >
-ostream& Print(ostream& out,  const Object<Term>& container) {
+// пишу версию оператора для словаря
+template <typename Key,  typename Value>
+ostream& operator<<(ostream& out,  const map<Key, Value>& container) {
     bool first = true;
-    for ( const Term& element : container) {
+    for ( const auto& [key, value] : container) {
         if (!first){
         out << ", "s;}
         
-        first = false;
+        first = false;      
+        out << "("s;
+        out <<  key ;
+        out << " "s;
+        out << value;
+        out << ")"s;
         
-        out << element;
     }
-    return out;
-}  
-    
-    
+    return  out;
+} 
 
-ostream& operator<<(ostream& out,  const set<Term>& container) {
+template <typename Container>
+void Print(ostream& out,  const Container& container) {
     bool first = true;
-    for ( const Term& element : container) {
+    for ( const auto& element : container) {
         if (!first){
         out << ", "s;}
         
         first = false;
         
-        out << element;
+        out <<  element;
     }
-    return out;
+}
+
+
+/*// Пишу отдельный принт для словаря 
+template <typename Key,  typename Value>
+void PrintMap(ostream& out,  const map<Key, Value>& container) {
+    bool first = true;
+    for ( const auto& [key, value] : container) {
+        if (!first){
+        out << ", "s;}
+        
+        first = false;      
+        out << "("s;
+        out <<  key ;
+        out << " "s;
+        out << value;
+        out << ")"s;
+        
+    }
+} 
+
+// пишу отдельный оператор для словаря 
+template <typename Key,  typename Value>
+ostream& operator<<(ostream& out,  const map<Key, Value>& container) {
+    out << "<"s;
+    PrintMap(out, container);
+    out << ">"s;
+    return  out;
+}  
+*/
+
+    
+template <typename Term >
+ostream& operator<<(ostream& out,  const set<Term>& container) {
+    out << "{"s;
+    Print(out, container);
+    out << "}"s;
+    return  out;
 }  
 
 template <typename Term >
 ostream& operator<<(ostream& out,  const vector<Term>& container) {
-    bool first = true;
-    for ( const Term& element : container) {
-        if (!first){
-        out << ", "s;}
-        
-        first = false;
-        
-        out << element;
-    }
+    out << "["s;
+    Print(out, container);
+    out << "]"s;
     return out;
 }  
    
 
 int main() {
-    const vector<int> ages = {10, 5, 2, 12};
+    /*const vector<int> ages = {10, 5, 2, 12};
     cout << ages << endl;
     const  set<string> cats = {"Мурка"s, "Белка"s, "Георгий"s, "Рюрик"s};
     cout << cats << endl;
+    return 0;*/
+    
+    const set<string> cats = {"Мурка"s, "Белка"s, "Георгий"s, "Рюрик"s};
+    cout << cats << endl;
+    const vector<int> ages = {10, 5, 2, 12};
+    cout << ages << endl;
+    const map<string, int> cat_ages = {
+                                        {"Мурка"s, 10}, 
+                                        {"Белка"s, 5},
+                                        {"Георгий"s, 2}, 
+                                        {"Рюрик"s, 12}
+                                    };
+    cout << cat_ages<< endl;
     return 0;
 }
