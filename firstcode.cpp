22bd4eaf89a7a86cfa4b62e2d7be8ae428c6bcfd
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2022-12-20 16:45:08 +0300

    Update firstcode.cpp

--- firstcode.cpp
+++ firstcode.cpp
@@ -1,21 +1,33 @@
+Задание 2
+Мы ловко упорядочили животных по убыванию веса, отсортировав по возрастанию -weight. Но с именем так не выйдет. Добавьте в функцию необязательный третий параметр reverse. Если туда будет передано true, объекты нужно отсортировать в обратном порядке.
+Не используйте функцию reverse из библиотеки <algorithm>.
+Пример
+int main() {
+    vector<Animal> animals = {
+        {"Мурка"s,   10, 5},
+        {"Белка"s,   5,  1.5},
+        {"Георгий"s, 2,  4.5},
+        {"Рюрик"s,   12, 3.1},
+    };
+    PrintNames(animals);
+    SortBy(animals, [](const Animal& animal) { return animal.name; }, true);
+    PrintNames(animals);
+    SortBy(animals, [](const Animal& animal) { return animal.weight; });
+    PrintNames(animals);
+    return 0;
+} 
+Вывод
+Мурка Белка Георгий Рюрик 
+Рюрик Мурка Георгий Белка 
+Белка Рюрик Георгий Мурка 
+
+Здесь будет достаточно любого решения, даже с небольшим копированием кода. Например, можно начать функцию с if (reverse) и в каждой ветке отсортировать с нужным компаратором
+
 #include <algorithm>
 #include <iostream>
 #include <vector>
-
+#include <algorithm>
 using namespace std;
-/*
-Задание 1
-Реализуйте функцию SortBy в соответствии с указанными требованиями. В процессе у вас могут возникнуть проблемы. Постарайтесь решить их самостоятельно: читайте подсказки.
-Код должен выводить:
-Мурка Белка Георгий Рюрик 
-Белка Георгий Мурка Рюрик 
-Мурка Георгий Рюрик Белка 
-
-ПОДСКАЗКИ:
-Чтобы написать компаратор, укажите типы параметров lhs и rhs. С ходу это сделать не получается, потому что известен только тип контейнера. Здесь поможет auto. Задайте список параметров const auto& lhs, const auto& rhs, и тогда сам компаратор окажется по сути шаблонной функцией.
-Чтобы вызвать в компараторе-лямбде key_mapper, захватите его: [key_mapper](const auto& lhs, const auto& rhs).
-*/
-
 
 struct Animal {
     string name;
@@ -23,10 +35,13 @@ struct Animal {
     double weight;
 };
 
-template <typename Container, typename KeyMapper>
-void SortBy(Container& container, KeyMapper key_mapper) {
-    // теперь можно сортировать контейнер
-    // с компаратором key_mapper(lhs) < key_mapper(rhs)
+template<typename Container, typename KeyMapper>
+void SortBy(Container &container, KeyMapper key_mapper) {
+sort(container.begin(), container.end(), [key_mapper](const auto& lhs, const auto& rhs) {
+			return key_mapper(lhs) < key_mapper(rhs);
+		});
+// теперь можно сортировать контейнер
+// с компаратором key_mapper(lhs) < key_mapper(rhs)
 }
 
 void PrintNames(const vector<Animal>& animals) {
