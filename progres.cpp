466b6bcf3f849b023918d4473d6b2336d148b055
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2022-12-22 08:50:44 +0300

    Update progres.cpp

--- progres.cpp
+++ progres.cpp
@@ -1,280 +1,102 @@
+#include <cassert>
 #include <iostream>
 #include <map>
 #include <string>
-#include <tuple>
 #include <vector>
 
 using namespace std;
 
-// Перечислимый тип для статуса задачи
-enum class TaskStatus {
-	NEW,          // новая
-	IN_PROGRESS,  // в разработке
-	TESTING,      // на тестировании
-	DONE          // завершена
+/*На первом шаге реализуйте оператор чтения структуры Query, примените его и обновите исходный код функции main из теории. Убедитесь, что код по-прежнему работает.*/
+enum class QueryType {
+    NewBus,
+    BusesForStop,
+    StopsForBus,
+    AllBuses,
 };
 
-// Объявляем тип-синоним для map<TaskStatus, int>,
-// позволяющего хранить количество задач каждого статуса
-using TasksInfo = map<TaskStatus, int>;
+struct Query {
+    QueryType type;
+    string bus;
+    string stop;
+    vector<string> stops;
+};
 
-// cout << TaskStatus.NEW<< endl;
-
-class TeamTasks {
-public:
-
-	// Вернуть person_tasks
-	/*map<string, TasksInfo> GetPersonTask(const string &person){
-		return persons_tasks;
-	}*/
-
-	map<string, TasksInfo> GetPersonTask(){
-		return persons_tasks;
-	}
-
-
-	void AddManyTasks(){
-	    persons_tasks["Ivan"][TaskStatus::NEW] += 5;
-	    persons_tasks["Ivan"][TaskStatus::IN_PROGRESS] -= 2;
-	    persons_tasks["Ivan"][TaskStatus::TESTING] += 4;
-	    //persons_tasks["Ivan"][TaskStatus::DONE] += 7;
-	}
-
-
-	// Получить статистику по статусам задач конкретного разработчика
-	const TasksInfo& GetPersonTasksInfo(const string &person) const {
-		return persons_tasks.at(person);
-
-	}
-	;
-
-	// Добавить новую задачу (в статусе NEW) для конкретного разработчитка
-	void AddNewTask(const string &person) {
-		++persons_tasks[person][TaskStatus::NEW];
-	}
-	;
-
-
-	tuple<TasksInfo, TasksInfo> PerformPersonTasks(const string &person,
-			int task_count) {
-		int left_to_change_task = task_count;
-		int status_number = 0;
-		int next_status = status_number + 1;
-		map<string, TasksInfo> persons_tasks_copy = persons_tasks ;
-
-		updated_tasks[person].clear();
-		untached_tasks[person].clear();
-
-
-		//for (const auto [status, quantity] : persons_tasks.at(person)) {
-		for (const auto [status, quantity] : persons_tasks[person]) {
-			if (status_number < 3) {
-				if (left_to_change_task >= quantity) {
-					AddNewTaskUpdated(person, next_status, quantity);
-					left_to_change_task = left_to_change_task - quantity;
-					persons_tasks_copy[person][(TaskStatus) (status_number)] -=  // тут убрал at
-							quantity;
-					persons_tasks_copy[person][(TaskStatus) (next_status)] +=   // тут убрал at
-							quantity;
-				}
-
-				else {
-					if (left_to_change_task != 0) {
-						if (quantity != 0) {
-							AddNewTaskUpdated(person, next_status,
-									left_to_change_task);
-							int itg_q = quantity - left_to_change_task;
-							AddNewTaskUntached(person, status_number, itg_q);
-
-							persons_tasks_copy[person][(TaskStatus) (status_number)] -=  // тут убрал at
-									left_to_change_task;
-							persons_tasks_copy[person][(TaskStatus) (next_status)] +=  // тут убрал at
-									left_to_change_task;
-							left_to_change_task = 0;
-							int status_for_untached = next_status;
-							// добавляю в untached оставшиеся нетрунутые таски из persons_tasks
-						    while (status_for_untached <=3)
-						    {
-						    	AddNewTaskUntached(person, status_for_untached, static_cast<int>(persons_tasks[person][(TaskStatus) (status_for_untached)]));
-						        ++status_for_untached;
-						    }
-
-
-
-						} else {
-							continue;
-						}
-					} else
-						break;
-
-				}
-
-				++status_number;
-				++next_status;
-
-			} else {break;}
-		}
-
-		untached_tasks[person].erase(TaskStatus::DONE);
-		persons_tasks = persons_tasks_copy; // Отражаю все правки после изменения задач в изначальном словаре
-
-		return make_tuple(updated_tasks[person], untached_tasks[person]);
-
-	}
-	;
-
-private:
-// храним имя разработчика и статусы+количество его задач
-	map<string, TasksInfo> persons_tasks;
-	map<string, TasksInfo> updated_tasks;
-	map<string, TasksInfo> untached_tasks;
-
-
-
-	void AddNewTaskUpdated(const string &person, int status, int quantity) {
-		if (status == 0) {
-			updated_tasks[person][TaskStatus::NEW] += quantity;
-		} else if (status == 1) {
-			updated_tasks[person][TaskStatus::IN_PROGRESS] += quantity;
-		}
-
-		else if (status == 2) {
-			updated_tasks[person][TaskStatus::TESTING] += quantity;
-		}
-
-		else if (status == 3) {
-			updated_tasks[person][TaskStatus::DONE] += quantity;
-		}
-		else if (status == 4) {
-			return;
-		}
-
-	}
-
-	void AddNewTaskUntached(const string &person, int status, int quantity) {
-		if (status == 0) {
-			untached_tasks[person][TaskStatus::NEW] += quantity;
-		} else if (status == 1) {
-			untached_tasks[person][TaskStatus::IN_PROGRESS] += quantity;
-		}
-
-		else if (status == 2) {
-			untached_tasks[person][TaskStatus::TESTING] += quantity;
-		}
-
-		else if (status == 3) {
-			untached_tasks[person][TaskStatus::DONE] += quantity;
-		}
-	}
-	;
-
-
-	TaskStatus get_next_status(TaskStatus status) {
-		if (status == TaskStatus::NEW) {
-			return TaskStatus::IN_PROGRESS;
-		} else if (status == TaskStatus::IN_PROGRESS) {
-			return TaskStatus::TESTING;
-		} else if (status == TaskStatus::TESTING) {
-			return status;
-		}
-	}
-}
-;
-
-/////////////////////////////////////////////////////////////////////////НЕ ОТНОСИТСЯ ////////////////////////////////
-
-// Принимаем словарь по значению, чтобы иметь возможность
-// обращаться к отсутствующим ключам с помощью [] и получать 0,
-// не меняя при этом исходный словарь.
-void PrintTasksInfo(TasksInfo tasks_info) {
-	cout << tasks_info[TaskStatus::NEW] << " new tasks"s << ", "s
-			<< tasks_info[TaskStatus::IN_PROGRESS] << " tasks in progress"s
-			<< ", "s << tasks_info[TaskStatus::TESTING]
-			<< " tasks are being tested"s << ", "s
-			<< tasks_info[TaskStatus::DONE] << " tasks are done"s << endl;
+istream& operator>>(istream& is, Query& q) {
+    // Реализуйте эту функцию
+	is >> q;
+    return is;
 }
 
 
 int main() {
-    TeamTasks tasks;
-    tasks.AddNewTask("Ilia");
-    for (int i = 0; i < 3; ++i) {
-        tasks.AddNewTask("Ivan");
+    int q;
+    cin >> q;
+
+    map<string, vector<string>> buses_to_stops, stops_to_buses;
+
+    for (int i = 0; i < q; ++i) {
+        string operation_code;
+        cin >> operation_code;
+
+        if (operation_code == "NEW_BUS"s) {
+            string bus;
+            cin >> bus;
+            int stop_count;
+            cin >> stop_count;
+            vector<string>& stops = buses_to_stops[bus];
+            stops.resize(stop_count);
+            for (string& stop : stops) {
+                cin >> stop;
+                stops_to_buses[stop].push_back(bus);
+            }
+
+        } else if (operation_code == "BUSES_FOR_STOP"s) {
+            string stop;
+            cin >> stop;
+            if (stops_to_buses.count(stop) == 0) {
+                cout << "No stop"s << endl;
+            } else {
+                for (const string& bus : stops_to_buses[stop]) {
+                    cout << bus << " "s;
+                }
+                cout << endl;
+            }
+
+        } else if (operation_code == "STOPS_FOR_BUS"s) {
+            string bus;
+            cin >> bus;
+            if (buses_to_stops.count(bus) == 0) {
+                cout << "No bus"s << endl;
+            } else {
+                for (const string& stop : buses_to_stops[bus]) {
+                    cout << "Stop "s << stop << ": "s;
+                    if (stops_to_buses[stop].size() == 1) {
+                        cout << "no interchange"s;
+                    } else {
+                        for (const string& other_bus : stops_to_buses[stop]) {
+                            if (bus != other_bus) {
+                                cout << other_bus << " "s;
+                            }
+                        }
+                    }
+                    cout << endl;
+                }
+            }
+
+        } else if (operation_code == "ALL_BUSES"s) {
+            if (buses_to_stops.empty()) {
+                cout << "No buses"s << endl;
+            } else {
+                for (const auto& bus_item : buses_to_stops) {
+                    cout << "Bus "s << bus_item.first << ": "s;
+                    for (const string& stop : bus_item.second) {
+                        cout << stop << " "s;
+                    }
+                    cout << endl;
+                }
+            }
+        }
     }
-    cout << "Ilia's tasks: ";
-    PrintTasksInfo(tasks.GetPersonTasksInfo("Ilia"));
-    cout << "Ivan's tasks: ";
-    PrintTasksInfo(tasks.GetPersonTasksInfo("Ivan"));
 
-    TasksInfo updated_tasks, untouched_tasks;
-
-    tie(updated_tasks, untouched_tasks) = tasks.PerformPersonTasks("Ivan", 2);
-    cout << "Updated Ivan's tasks: ";
-    PrintTasksInfo(updated_tasks);
-    cout << "Untouched Ivan's tasks: ";
-    PrintTasksInfo(untouched_tasks);
-
-    tie(updated_tasks, untouched_tasks) = tasks.PerformPersonTasks("Ivan", 2);
-    cout << "Updated Ivan's tasks: ";
-    PrintTasksInfo(updated_tasks);
-    cout << "Untouched Ivan's tasks: ";
-    PrintTasksInfo(untouched_tasks);
-
-    /*
-     Tasks: 5/0/2/7
-		> PerformPersonTasks(100)
-		Updated: 0/5/0/2
-		Untouched: 0/0/0/0
-		Tasks: 0/5/0/2
-		А еще 7 где?
-     */
-
-    /*
-     Tasks: 5/0/5/0
-	> PerformPersonTasks(2)
-	Updated: 0/2/0/0
-	Untouched: 3/0/0/0
-      */
-
-
-
-    tasks.AddManyTasks();  // добавляю таски чтобы было как в примере выше
-    map<string, TasksInfo> persons_tasks = tasks.GetPersonTask();
-
-    for (const auto [status, quantity] : persons_tasks["Ivan"]){cout << "quantity after"s << quantity << endl;};
-
-
-    cout << "ADD 2 to IVAN : "s << endl;
-
-    tie(updated_tasks, untouched_tasks) = tasks.PerformPersonTasks("Ivan", 2);
-    cout << "Updated Ivan's tasks: ";
-    PrintTasksInfo(updated_tasks);
-    cout << "Untouched Ivan's tasks: ";
-    PrintTasksInfo(untouched_tasks);
-
-    map<string, TasksInfo> persons_tasks_all = tasks.GetPersonTask();
-    for (const auto [status, quantity] : persons_tasks_all["Ivan"]){cout << "quantity after all "s << quantity << endl;};
-
+    return 0;
 }
-
-// правильный вывод
-/*
- Ilia's tasks: 1 new tasks, 0 tasks in progress, 0 tasks are being tested, 0 tasks are done
-Ivan's tasks: 3 new tasks, 0 tasks in progress, 0 tasks are being tested, 0 tasks are done
-Updated Ivan's tasks: 0 new tasks, 2 tasks in progress, 0 tasks are being tested, 0 tasks are done
-Untouched Ivan's tasks: 1 new tasks, 0 tasks in progress, 0 tasks are being tested, 0 tasks are done
-Updated Ivan's tasks: 0 new tasks, 1 tasks in progress, 1 tasks are being tested, 0 tasks are done
-Untouched Ivan's tasks: 0 new tasks, 1 tasks in progress, 0 tasks are being tested, 0 tasks are done
- */
-
-
-// мой вывод
-/*
-Ilia's tasks: 1 new tasks, 0 tasks in progress, 0 tasks are being tested, 0 tasks are done
-Ivan's tasks: 3 new tasks, 0 tasks in progress, 0 tasks are being tested, 0 tasks are done
-Updated Ivan's tasks: 0 new tasks, 2 tasks in progress, 0 tasks are being tested, 0 tasks are done
-Untouched Ivan's tasks: 1 new tasks, 0 tasks in progress, 0 tasks are being tested, 0 tasks are done
-Updated Ivan's tasks: 0 new tasks, 1 tasks in progress, 1 tasks are being tested, 0 tasks are done
-Untouched Ivan's tasks: 0 new tasks, 1 tasks in progress, 0 tasks are being tested, 0 tasks are done
-
-  */
