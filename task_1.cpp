2910d6a4f75edc68fd6b931e2b0270a35c48041c
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2022-12-24 21:54:39 +0300

    Update task_1.cpp

--- task_1.cpp
+++ task_1.cpp
@@ -8,6 +8,21 @@
 
 using namespace std;
 
+// ВЫВОД 
+/*
+No buses
+No stop
+No bus
+32 32K
+Stop Vnukovo: 32 32K 950
+Stop Moskovsky: no interchange
+Stop Rumyantsevo: no interchange
+Stop Troparyovo: 950
+Bus 272: Vnukovo Moskovsky Rumyantsevo Troparyovo
+Bus 32: Tolstopaltsevo Marushkino Vnukovo
+Bus 32K: Tolstopaltsevo Marushkino Vnukovo Peredelkino Solntsevo Skolkovo
+Bus 950: Kokoshkino Marushkino Vnukovo Peredelkino Solntsevo Troparyovo
+*/
 
 vector<string> SplitIntoWords(const string& text) {
     vector<string> words;
