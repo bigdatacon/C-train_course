3283ebe1017b25a4ea6ea20a6b9f46dd1fbbb7ec
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2022-12-25 19:18:39 +0300

    Update firstcode.cpp

--- firstcode.cpp
+++ firstcode.cpp
@@ -1,100 +1,60 @@
+
+
+#include <algorithm>
 #include <cassert>
-#include <iostream>
+#include <cmath>
 #include <map>
 #include <set>
-#include <sstream>
 #include <string>
+#include <utility>
+#include <vector>
 
 using namespace std;
 
-class Synonyms {
-public:
-    void Add(const string& first_word, const string& second_word) {
-        synonyms_[first_word].insert(second_word);
-        synonyms_[second_word].insert(first_word);
+/* Подставьте вашу реализацию класса SearchServer сюда */
+
+// -------- Начало модульных тестов поисковой системы ----------
+
+// Тест проверяет, что поисковая система исключает стоп-слова при добавлении документов
+void TestExcludeStopWordsFromAddedDocumentContent() {
+    const int doc_id = 42;
+    const string content = "cat in the city"s;
+    const vector<int> ratings = {1, 2, 3};
+    // Сначала убеждаемся, что поиск слова, не входящего в список стоп-слов,
+    // находит нужный документ
+    {
+        SearchServer server;
+        server.AddDocument(doc_id, content, DocumentStatus::ACTUAL, ratings);
+        const auto found_docs = server.FindTopDocuments("in"s);
+        assert(found_docs.size() == 1);
+        const Document& doc0 = found_docs[0];
+        assert(doc0.id == doc_id);
     }
 
-    size_t GetSynonymCount(const string& word) const {
-        if (synonyms_.count(word) != 0) {
-            return synonyms_.at(word).size();
-        }
-        return 0;
+    // Затем убеждаемся, что поиск этого же слова, входящего в список стоп-слов,
+    // возвращает пустой результат
+    {
+        SearchServer server;
+        server.SetStopWords("in the"s);
+        server.AddDocument(doc_id, content, DocumentStatus::ACTUAL, ratings);
+        assert(server.FindTopDocuments("in"s).empty());
     }
-
-    bool AreSynonyms(const string& first_word, const string& second_word) const {
-        // Напишите недостающий код
-        set syn_1 = synonyms_[first_word];    // ВНИМАНИЕ ПОЧЕМУ ТУТ ВЫДАЕТ ОШИБКУ а с at. не выдает ? 
-        set syn_2 = synonyms_[second_word];
-        //set syn_1 = synonyms_.at(first_word);
-        //set syn_2 = synonyms_.at(second_word);
-        bool first_is_syn_two = syn_2.count(first_word);
-        bool second_is_syn_first = syn_1.count(second_word);
-        if (first_is_syn_two && second_is_syn_first){return true;}
-        
-        return false;
-    }
-
-private:
-    map<string, set<string>> synonyms_;
-};
-
-void TestAddingSynonymsIncreasesTheirCount() {
-    Synonyms synonyms;
-    assert(synonyms.GetSynonymCount("music"s) == 0);
-    assert(synonyms.GetSynonymCount("melody"s) == 0);
-
-    synonyms.Add("music"s, "melody"s);
-    assert(synonyms.GetSynonymCount("music"s) == 1);
-    assert(synonyms.GetSynonymCount("melody"s) == 1);
-
-    synonyms.Add("music"s, "tune"s);
-    assert(synonyms.GetSynonymCount("music"s) == 2);
-    assert(synonyms.GetSynonymCount("tune"s) == 1);
-    assert(synonyms.GetSynonymCount("melody"s) == 1);
-    
-    //Тесты на проверку что синонимы
-    assert(synonyms.AreSynonyms("music"s, "tune"s) == true);
-    //assert(synonyms.AreSynonyms("music"s, "tune"s) == 1); // на всякий случай с 1 проверю    
 }
 
-void TestAreSynonyms() {
-    // Напишите недостающий код
+/*
+Разместите код остальных тестов здесь
+*/
+
+// Функция TestSearchServer является точкой входа для запуска тестов
+void TestSearchServer() {
+    TestExcludeStopWordsFromAddedDocumentContent();
+    // Не забудьте вызывать остальные тесты здесь
 }
 
-void TestSynonyms() {
-    TestAddingSynonymsIncreasesTheirCount();
-    TestAreSynonyms();
-}
+// --------- Окончание модульных тестов поисковой системы -----------
 
 int main() {
-    TestSynonyms();
-
-    Synonyms synonyms;
-
-    string line;
-    while (getline(cin, line)) {
-        istringstream command(line);
-        string action;
-        command >> action;
-
-        if (action == "ADD"s) {
-            string first_word, second_word;
-            command >> first_word >> second_word;
-            synonyms.Add(first_word, second_word);
-        } else if (action == "COUNT"s) {
-            string word;
-            command >> word;
-            cout << synonyms.GetSynonymCount(word) << endl;
-        } else if (action == "CHECK"s) {
-            string first_word, second_word;
-            command >> first_word >> second_word;
-            if (synonyms.AreSynonyms(first_word, second_word)) {
-                cout << "YES"s << endl;
-            } else {
-                cout << "NO"s << endl;
-            }
-        } else if (action == "EXIT"s) {
-            break;
-        }
-    }
+    TestSearchServer();
+    // Если вы видите эту строку, значит все тесты прошли успешно
+    cout << "Search server testing finished"s << endl;
 }
