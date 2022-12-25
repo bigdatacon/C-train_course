0f8e1e0e398ef5a5f1ff3f1fcbf95f1d25493222
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2022-12-25 09:55:35 +0300

    Update task_1.cpp

--- task_1.cpp
+++ task_1.cpp
@@ -8,7 +8,26 @@
 
 using namespace std;
 
-// ВЫВОД 
+//Вывод мой
+/*
+No buses
+No stop
+No bus
+32 32K 
+Stop Vnukovo: 32 32K 950 
+Stop Moskovsky: no interchange
+Stop Rumyantsevo: no interchange
+Stop Troparyovo: 950 
+Bus 272: Vnukovo Moskovsky Rumyantsevo Troparyovo 
+Bus 32: Tolstopaltsevo Marushkino Vnukovo 
+Bus 32K: Tolstopaltsevo Marushkino Vnukovo Peredelkino Solntsevo Skolkovo 
+Bus 950: Kokoshkino Marushkino Vnukovo Peredelkino Solntsevo Troparyovo 
+
+*/
+
+
+
+// ВЫВОД по заданию
 /*
 No buses
 No stop
