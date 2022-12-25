c24d9dfd98dd3b7db973a16bc42163b134cd4304
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2022-12-25 17:14:52 +0300

    Update task_1.cpp

--- task_1.cpp
+++ task_1.cpp
@@ -8,69 +8,7 @@
 
 using namespace std;
 
-/*
- 10
-ALL_BUSES
-BUSES_FOR_STOP Marushkino
-STOPS_FOR_BUS 32K
-NEW_BUS 32 3 Tolstopaltsevo Marushkino Vnukovo
-NEW_BUS 32K 6 Tolstopaltsevo Marushkino Vnukovo Peredelkino Solntsevo Skolkovo
-BUSES_FOR_STOP Vnukovo
-NEW_BUS 950 6 Kokoshkino Marushkino Vnukovo Peredelkino Solntsevo Troparyovo
-NEW_BUS 272 4 Vnukovo Moskovsky Rumyantsevo Troparyovo
-STOPS_FOR_BUS 272
-ALL_BUSES
-ALL_BUSES
- * */
 
-
-//Вывод мой
-/*
-No buses
-No stop
-No bus
-32 32K
-Stop Vnukovo: 32 32K 950
-Stop Moskovsky: no interchange
-Stop Rumyantsevo: no interchange
-Stop Troparyovo: 950
-Bus 272: Vnukovo Moskovsky Rumyantsevo Troparyovo
-Bus 32: Tolstopaltsevo Marushkino Vnukovo
-Bus 32K: Tolstopaltsevo Marushkino Vnukovo Peredelkino Solntsevo Skolkovo
-Bus 950: Kokoshkino Marushkino Vnukovo Peredelkino Solntsevo Troparyovo
-
-*/
-
-/*
-No buses
-No stop
-No bus
-32 32K
-Stop Vnukovo: 32 32K 950
-Stop Moskovsky: no interchange
-Stop Rumyantsevo: no interchange
-Stop Troparyovo: 950
-Bus 272: Vnukovo Moskovsky Rumyantsevo Troparyovo
-Bus 32: Tolstopaltsevo Marushkino Vnukovo
-Bus 32K: Tolstopaltsevo Marushkino Vnukovo Peredelkino Solntsevo Skolkovo
-Bus 950: Kokoshkino Marushkino Vnukovo Peredelkino Solntsevo Troparyovo
- */
-
-// ВЫВОД по заданию
-/*
-No buses
-No stop
-No bus
-32 32K
-Stop Vnukovo: 32 32K 950
-Stop Moskovsky: no interchange
-Stop Rumyantsevo: no interchange
-Stop Troparyovo: 950
-Bus 272: Vnukovo Moskovsky Rumyantsevo Troparyovo
-Bus 32: Tolstopaltsevo Marushkino Vnukovo
-Bus 32K: Tolstopaltsevo Marushkino Vnukovo Peredelkino Solntsevo Skolkovo
-Bus 950: Kokoshkino Marushkino Vnukovo Peredelkino Solntsevo Troparyovo
-*/
 
 vector<string> SplitIntoWords(const string& text) {
     vector<string> words;
