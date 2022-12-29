5a7fa2227028b5d5d2a74739adcccbc69e4135e4
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2022-12-29 21:09:03 +0300

    Update firstcode.cpp

--- firstcode.cpp
+++ firstcode.cpp
@@ -1,61 +1,67 @@
-#include <algorithm>
 #include <cmath>
-#include <map>
-#include <set>
+#include <iostream>
 #include <string>
-#include <utility>
 #include <vector>
-
-#include "search_server.h"
+#include <numeric>
 
 using namespace std;
 
-/* Подставьте вашу реализацию класса SearchServer сюда */
-
 /*
-   Подставьте сюда вашу реализацию макросов
-   ASSERT, ASSERT_EQUAL, ASSERT_EQUAL_HINT, ASSERT_HINT и RUN_TEST
+Перед приведением дроби к несократимому виду измените знак числителя и знаменателя, если знаменатель изначально был отрицательным. Затем и числитель, и знаменатель разделите на их наибольший общий делитель. Для нахождения наибольшего общего делителя в C++ есть удобная функция gcd из заголовочного файла <numeric>. Вы можете познакомиться с ней в документации по С++ или разработать такую функцию самостоятельно.
 */
 
-// -------- Начало модульных тестов поисковой системы ----------
-
-// Тест проверяет, что поисковая система исключает стоп-слова при добавлении документов
-void TestExcludeStopWordsFromAddedDocumentContent() {
-    const int doc_id = 42;
-    const string content = "cat in the city"s;
-    const vector<int> ratings = {1, 2, 3};
-    {
-        SearchServer server;
-        server.AddDocument(doc_id, content, DocumentStatus::ACTUAL, ratings);
-        const auto found_docs = server.FindTopDocuments("in"s);
-        ASSERT_EQUAL(found_docs.size(), 1u);
-        const Document& doc0 = found_docs[0];
-        ASSERT_EQUAL(doc0.id, doc_id);
+class Rational {
+public:
+    
+            // Явно сообщаем компилятору, что в этом классе нужно создать конструктор по умолчанию
+    Rational() = default;
+    
+        // Параметризованный конструктор
+    Rational(int numerator) {
+        numerator_ = numerator;
+        denominator_ = 1;  
+    }
+    
+    // Параметризованный конструктор
+    Rational(int numerator, int denominator ) {
+        int num_input = numerator;
+        int denom_input = denominator;       
+        if (denom_input<0){num_input = -num_input;}
+        
+        int deviser = gcd(num_input, denom_input);
+        numerator_ = num_input/deviser;
+        denominator_ = denom_input/deviser;
+    }
+    
+    int Numerator() const {
+        return numerator_;
     }
 
-    {
-        SearchServer server;
-        server.SetStopWords("in the"s);
-        server.AddDocument(doc_id, content, DocumentStatus::ACTUAL, ratings);
-        ASSERT_HINT(server.FindTopDocuments("in"s).empty(),
-                    "Stop words must be excluded from documents"s);
+    int Denominator() const {
+        return denominator_;
     }
+
+private:
+    int numerator_ = 0;
+    int denominator_ = 1;
+};
+
+
+Rational Add(Rational r1, Rational r2) {
+    int numerator = r1.Numerator() * r2.Denominator() + r2.Numerator() * r1.Denominator();
+    int denominator = r1.Denominator() * r2.Denominator();
+    // Создаём и возвращаем дробь с заданным числителем и знаменателем
+    return Rational{numerator, denominator};
 }
-
-/*
-Разместите код остальных тестов здесь
-*/
-
-// Функция TestSearchServer является точкой входа для запуска тестов
-void TestSearchServer() {
-    RUN_TEST(TestExcludeStopWordsFromAddedDocumentContent);
-    // Не забудьте вызывать остальные тесты здесь
-}
-
-// --------- Окончание модульных тестов поисковой системы -----------
-
 int main() {
-    TestSearchServer();
-    // Если вы видите эту строку, значит все тесты прошли успешно
-    cout << "Search server testing finished"s << endl;
-}
+    Rational zero;     // Дробь 0/1 = 0
+    const Rational seven(7); // Дробь 7/1 = 7
+    const Rational one_third(1, 3); // Дробь 1/3
+    vector<Rational> numbers;
+    numbers.push_back(Rational{7, 8});
+    // Следующие 2 строки эквивалентны - добавляют в numbers дробь 3/1
+    numbers.push_back(Rational{3});
+    numbers.push_back(3);
+    Rational sum = Add(Rational{1,6}, one_third);
+    cout << "SUM : " << sum.Numerator() << endl;
+    // Выведет 1/2
