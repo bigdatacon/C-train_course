6c38aeab697cd63562835e184fa0f3917cf364ae
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2022-12-31 22:31:37 +0300

    Update firstcode.cpp

--- firstcode.cpp
+++ firstcode.cpp
@@ -1,18 +1,18 @@
-#include <numeric>
-#include <ostream>
-#include <istream>
-#include <cmath>
 #include <iostream>
-#include <string>
-#include <vector>
 #include <numeric>
 
 using namespace std;
-/*Задание
-Реализуйте для класса Rational операции ввода и вывода в формате числитель/знаменатель. Гарантируется, что данные во входном потоке будут корректными.
-Не забудьте в операциях ввода и вывода возвращать ссылку на поток, переданный в качестве первого аргумента. Это позволит объединить несколько операций вывода в цепочки:
-cout << rational1 << " "s << rational2; 
+
+/*
+Задание
+Реализуйте для класса Rational операции сложения и вычитания, а также операции унарного плюса и унарного минуса. Это позволит выполнять над обыкновенными дробями базовые арифметические операции, так же как с целыми числами и числами с плавающей запятой.
+
+Подсказка: 
+Возьмите код сложения дробей из теории. Операцию вычитания реализуйте аналогично. Операции унарного плюса и минуса реализуйте по аналогии со структурой Vector2D.
 */
+
+
+
 
 
 class Rational {
@@ -21,21 +21,51 @@ public:
 
     Rational(int numerator)
         : numerator_(numerator)
-        , denominator_(1) {
+        , denominator_(1)
+    {
     }
 
     Rational(int numerator, int denominator)
         : numerator_(numerator)
-        , denominator_(denominator) {
+        , denominator_(denominator)
+    {
         Normalize();
     }
-
+    
     int Numerator() const {
         return numerator_;
     }
 
     int Denominator() const {
         return denominator_;
+    }
+    
+    Rational operator+(Rational left, Rational right) {
+    const int numerator = left.Numerator() * right.Denominator() 
+                  + right.Numerator() * left.Denominator();
+    const int denominator = left.Denominator() * right.Denominator();
+
+    return {numerator, denominator};
+    }
+
+
+    Rational operator-(Rational left, Rational right) {
+        const int numerator = left.Numerator() * right.Denominator() 
+                      - right.Numerator() * left.Denominator();
+        const int denominator = left.Denominator() * right.Denominator();
+
+        return {numerator, denominator};
+    }
+
+
+    // Операция унарного плюса возвращает копию переданного экземпляра
+    Rational operator+(Rational r) {
+        return r;
+    }
+
+    // Операция унарного минуса возвращает вектор с противоположным направлением
+    Rational operator-(Rational r) {
+        return {-r.Numerator() , -r.Denominator()};
     }
 
 private:
@@ -44,29 +74,24 @@ private:
             numerator_ = -numerator_;
             denominator_ = -denominator_;
         }
-        const int divisor = gcd(numerator_, denominator_);
-        numerator_ /= divisor;
-        denominator_ /= divisor;
+        int n = gcd(numerator_, denominator_);
+        numerator_ /= n;
+        denominator_ /= n;
     }
 
     int numerator_ = 0;
     int denominator_ = 1;
 };
 
-// вывод
-ostream& operator<<(ostream& output, Rational& r) {
-    //char slash = "/"s;  - так не работает 
-    //output << r.Numerator() << slash << r.Denominator();
-    output << r.Numerator() << "/" << r.Denominator();  // так не проходит проверку 
-    
+ostream& operator<<(ostream& output, Rational rational) {
+    output << rational.Numerator() << '/' << rational.Denominator();
     return output;
 }
 
-// ввод
-istream& operator>>(istream& input, Rational& r) {
+istream& operator>>(istream& input, Rational& rational) {
     int numerator, denominator;
-    input >> numerator >> denominator;
-    r = Rational{numerator, denominator};
+    char slash;
+    input >> numerator >> slash >> denominator;
+    rational = Rational{numerator, denominator};
     return input;
-} 
-
+}
