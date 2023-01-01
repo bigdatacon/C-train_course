9c817eb286a9a753d79d0dc9c77864bedb131298
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-01-01 17:05:10 +0300

    Update firstcode.cpp

--- firstcode.cpp
+++ firstcode.cpp
@@ -4,13 +4,20 @@
 
 using namespace std;
 /*
-Задание 1
-Реализуйте для класса Rational операции присваивания +=, -=, *= и /=. После выполнения этих операций результирующая дробь должна приводиться к несократимому виду с положительным знаменателем. Входные данные программы исключают деление на ноль.
+Задание 2
+Перегрузите бинарные арифметические операции +, -, *, / для класса Rational на основе реализованных вами присваивающих выражений.
 
 ПОДСКАЗКА
-Объявите присваивающие выражения внутри класса Rational, как в примере с классом Vector2D. Убедитесь, что они модифицируют текущий объект и возвращают ссылку на него командой return *this;. Перед выходом из операции не забудьте нормализовать дробь. Для этого выделите код нормализации дроби в приватный метод Normalize(). Вызывайте его в этих операциях перед return *this;.
-*/
+В теории вы узнали, как это сделать на примере структуры Vector2D. Здесь сделайте по аналогии.
 
+Например, задав +=, реализуем операцию сложения буквально в одну строку кода: 
+
+Vector2D operator+(Vector2D left, Vector2D right) {
+    // аргумент left принят по значению; можно спокойно модифицировать
+    // это значение внутри операции и вернуть в виде результата
+    return left += right;
+} 
+*/
 
 class Rational {
 public:
@@ -36,60 +43,9 @@ public:
     int Denominator() const {
         return denominator_;
     }
-
-private:
-    void Normalize() {
-        if (denominator_ < 0) {
-            numerator_ = -numerator_;
-            denominator_ = -denominator_;
-        }
-        int n = gcd(numerator_, denominator_);
-        numerator_ /= n;
-        denominator_ /= n;
-    }
-
-    int numerator_ = 0;
-    int denominator_ = 1;
-};
-
-ostream& operator<<(ostream& output, Rational rational) {
-    return output << rational.Numerator() << '/' << rational.Denominator();
-}
-
-istream& operator>>(istream& input, Rational& rational) {
-    int numerator;
-    int denominator;
-    char slash;
-    if ((input >> numerator) && (input >> slash) && (slash == '/') && (input >> denominator)) {
-        rational = Rational{numerator, denominator};
-    }
-    return input;
-}
-
-Rational operator+(Rational left, Rational right) {
-    const int numerator = left.Numerator() * right.Denominator() + right.Numerator() * left.Denominator();
-    const int denominator = left.Denominator() * right.Denominator();
-
-    return {numerator, denominator};
-}
-
-Rational operator-(Rational left, Rational right) {
-    const int numerator = left.Numerator() * right.Denominator() - right.Numerator() * left.Denominator();
-    const int denominator = left.Denominator() * right.Denominator();
-
-    return {numerator, denominator};
-}
-
-Rational operator+(Rational value) {
-    return value;
-}
-
-Rational operator-(Rational value) {
-    return {-value.Numerator(), value.Denominator()};
-}
-
-// МОЙ КОД НАЧИНАЕТСЯ НИЖЕ 
-
+    
+    
+    // МОЙ КОД НАЧИНАЕТСЯ НИЖЕ 
 
 Rational operator+=(Rational right) {
     // Результат операции сохраняется в текущем экземпляре класса
@@ -111,7 +67,7 @@ Rational operator-=(Rational right) {
 
 Rational operator*=(Rational right) {
     // Результат операции сохраняется в текущем экземпляре класса
-    numerator_ = numerator_*right.Denominator() * right.Numerator() * denominator_;
+    numerator_ = numerator_* right.Numerator();
     denominator_ = denominator_*right.Denominator();  
     Normalize();
     // return *this позволяет вернуть ссылку на текущий объект
@@ -120,12 +76,71 @@ Rational operator*=(Rational right) {
 
 Rational operator/=(Rational right) {
     // Результат операции сохраняется в текущем экземпляре класса
-    numerator_ = numerator_*right.Denominator() / right.Numerator() * denominator_;
-    denominator_ = denominator_*right.Denominator();  
+    numerator_ = numerator_*right.Denominator();
+    denominator_ = denominator_*right.Numerator();  
     Normalize();
     // return *this позволяет вернуть ссылку на текущий объект
     return *this;
 }
+    
+// мой новый код тут 
+/*Rational operator+(Rational right) {
+    Rational left = { numerator_, denominator_};
+    return left += right;;
+}*/
+    
+
+private:
+    void Normalize() {
+        if (denominator_ < 0) {
+            numerator_ = -numerator_;
+            denominator_ = -denominator_;
+        }
+        int n = gcd(numerator_, denominator_);
+        numerator_ /= n;
+        denominator_ /= n;
+    }
+
+    int numerator_ = 0;
+    int denominator_ = 1;
+
+};
+
+ostream& operator<<(ostream& output, Rational rational) {
+    return output << rational.Numerator() << '/' << rational.Denominator();
+}
+
+istream& operator>>(istream& input, Rational& rational) {
+    int numerator;
+    int denominator;
+    char slash;
+    if ((input >> numerator) && (input >> slash) && (slash == '/') && (input >> denominator)) {
+        rational = Rational{numerator, denominator};
+    }
+    return input;
+}
+
+Rational operator+(Rational left, Rational right) {
+    const int numerator = left.Numerator() * right.Denominator() + right.Numerator() * left.Denominator();
+    const int denominator = left.Denominator() * right.Denominator();
+    return {numerator, denominator};
+}
+
+Rational operator-(Rational left, Rational right) {
+    const int numerator = left.Numerator() * right.Denominator() - right.Numerator() * left.Denominator();
+    const int denominator = left.Denominator() * right.Denominator();
+    return {numerator, denominator};
+}
+
+Rational operator+(Rational value) {
+    return value;
+}
+
+Rational operator-(Rational value) {
+    return {-value.Numerator(), value.Denominator()};
+}
+
+
 
 
 
