6406cadb75fda8804bcdfe314ef03bbc12109312
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-01-11 22:30:26 +0300

    Update task_current.cpp

--- task_current.cpp
+++ task_current.cpp
@@ -28,6 +28,15 @@ public:
 
 	}
 
+	ostream& operator << (ostream& os)
+	{	
+		for (auto el : disks_) { cout << el << " "; }
+		return os;
+	}
+
+	void PrintDisks() { for (auto el : disks_) { cout << el << " "; } }
+
+
 	// добавляем диск на верх собственной башни
 	// обратите внимание на исключение, которое выбрасывается этим методом
 	void AddToTop(int disk) {
@@ -41,31 +50,28 @@ public:
 		}
 	}
 
+	// 3:2:1  0   0
+	//3:2   0  1
+	//3   2   1  // выполняетяся 1 раз раскидываение 2 дисков 
+
+	//3  2:1  0  // точка при которой на buffer n-1
+
+	//0  2:1  3
+	// 1  2  3
+	//1 0  3:2
+	//0  0  3:2:1
+
+
+
+
+
+
+
 	// вы можете дописывать необходимые для вашего решения методы
 	void MoveDisks(int disks_num, Tower& destination, Tower& buffer) {
-		if (destination.GetDisksNum() < disks_num) {
-			/*if (GetDisksNum() == 1) {
-				if (buffer.GetDisksNum() > 0) {
-					destination.AddToTop(buffer.GetDisks().back());  // сначала перекидываю диск с buffer
-					buffer.PopBack();    // удаляю диск с 1 башни 
-					destination.AddToTop(GetDisks().back()); // перекидываю диск с первой башни 
-					PopBack();  // удаляю диск с 1 башни 
-				}
-				else {
-					destination.AddToTop(GetDisks().back());  // перекидываю диск с первой башни
-					PopBack();
-				}     // удаляю диск с 1 башни 
-			}
-			else if (GetDisksNum() == 0) {
-				if (buffer.GetDisksNum() > 0) {
-					destination.AddToTop(buffer.GetDisks().back());  // сначала перекидываю диск с buffer
-					buffer.PopBack();    // удаляю диск с 1 башни 
-					destination.AddToTop(GetDisks().back()); // перекидываю диск с первой башни 
-					PopBack();  // удаляю диск с 1 башни 
-				}
-				}
-			else {*/
+		if (disks_num > 1) {
 
+			while (buffer.GetDisksNum() < disks_num - 1) {
 				int current_disk = disks_.back(); // беру последний элемент 
 				PopBack(); // удаляю последний элемент
 
@@ -73,29 +79,46 @@ public:
 				PopBack(); // удаляю предпоследний элемент
 
 				// добавляю верхний диск на буффер а второй диск на destination 
+				destination.AddToTop(current_disk);
+				buffer.AddToTop(sec_current_disk);
+				//3   2   1 
+
+				destination.PopBack(); // удаляю диск с destination и переношу его на буффер 
 				buffer.AddToTop(current_disk);
-				destination.AddToTop(sec_current_disk);
+				///3  2:1  0
+			}
+			//тут условие когда на source остался 1 диск 
+			int current_disk = disks_.back(); // беру последний элемент 
+			PopBack(); // удаляю последний элемент
 
-				if (GetDisksNum() > 1) {
+			int buffer_disk = buffer.GetDisks().back(); // беру крайний элемент с buffer
+			buffer.PopBack(); // удаляю предпоследний элемент
 
-					buffer.PopBack(); // удаляю первый элемент из buffer и переношу его обратно на 1 башню 
-					AddToTop(current_disk);
+			destination.AddToTop(current_disk);
+			//0  2:1  3
 
-					destination.PopBack(); // удаляю предпоследний сверху диск с destination и переношу его на буффер 
-					buffer.AddToTop(sec_current_disk);
-				}
-				else {
-					destination.AddToTop(buffer.GetDisks().back());  // сначала перекидываю диск с buffer
-					buffer.PopBack();   // удаляю диск с 1 башни 
-					destination.AddToTop(GetDisks().back()); // перекидываю диск с первой башни 
-					PopBack();  // удаляю диск с 1 башни 
-				}
+			AddToTop(buffer_disk);
+			//1  2  3
 
-				// в целом тут цикл заканчивается так как в след итерации с 1 башни диск перенесется на буфер, а нижний на destination
-				//далее верхний с буфера  перенесется на 1 башню 
-			/* }*/
+			destination.AddToTop(buffer.GetDisks().back());  // сначала перекидываю диск с buffer
+			buffer.PopBack();    // удаляю диск с 1 башни 
+
+			//1  0 3:2
+
+			destination.AddToTop(GetDisks().back()); // перекидываю диск с первой башни 
+			PopBack();  // удаляю диск с 1 башни 
+			//0 0 3:2:1
+
+
+
 		}
-		MoveDisks(GetDisksNum(), destination, buffer);
+		else {
+			//1 0 0
+			destination.AddToTop(GetDisks().back()); // перекидываю диск с первой башни 
+			PopBack();  // удаляю диск с 1 башни }
+			//0 0 1
+		}
+		//MoveDisks(GetDisksNum(), destination, buffer);
 	}
 private:
 	vector<int> disks_;
@@ -123,6 +146,8 @@ void SolveHanoi(vector<Tower>& towers) {
 
 
 int main() {
+	//int towers_num = 5;
+	//int disks_num = 5;
 	int towers_num = 3;
 	int disks_num = 3;
 	vector<Tower> towers;
@@ -133,4 +158,10 @@ int main() {
 	// добавим на первую башню три кольца
 	towers[0].SetDisks(disks_num);
 	SolveHanoi(towers);
+
+	//cout << towers[0] << endl;  -- почему этот оператор не работает 
+	towers[0].PrintDisks();
+	towers[1].PrintDisks();
+	towers[2].PrintDisks();
+
 }
