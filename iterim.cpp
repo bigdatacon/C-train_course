7a250f20c57d965b7e09411aa568d9fa63c82905
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-01-13 18:51:27 +0300

    Update iterim.cpp

--- iterim.cpp
+++ iterim.cpp
@@ -1,48 +1,82 @@
-// разработайте сигнатуру фунции MakeVector по аналогии с функцией MakeSet из урока
 #include <algorithm>
 #include <iostream>
-#include <set>
-#include <string>
+#include <numeric>
+#include <sstream>
 #include <vector>
+#include <valarray>
+
 
 using namespace std;
+/*
+Задание 2
+Напишите функцию-шаблон MergeSort. Она должна принимать итераторы категории произвольного доступа, указывающие на начало и конец контейнера, который нужно отсортировать. Сортируйте по возрастанию. Считайте определённой операцию сравнения между элементами. 
+Есть много разных способов сортировать, но в этой задаче реализуйте сортировку методом слияния. Алгоритм такой сортировки:
+Разделить контейнер на две примерно равные части;
+Отсортировать каждую из этих частей;
+Объединить две части так, чтобы сортировка сохранилась.
+Пункт два в свою очередь решается теми же шагами. Этот алгоритм может быть реализован как рекурсивно, так и итеративно (с применением цикла).
+Не используйте стандартный алгоритм сортировки sort и всех его «родственников» — это будет слишком просто.
+
+Предположим, наш диапазон состоит из двух элементов.
+Разделите его пополам. В каждой части получилось по одному элементу.
+Каждая из половин уже отсортирована, ведь она состоит всего из одного элемента.
+Найдите подходящий алгоритм, который соединяет два отсортированных диапазона.
+*/
+
+/*template <typename RandomIt>
+void MergeSort(RandomIt range_begin, RandomIt range_end)*/
 
 template <typename It>
-void PrintRange(It range_begin, It range_end) {
-    for (auto it = range_begin; it != range_end; ++it) {
-        cout << *it << " "s;
+void PrintRange(It range_begin, It range_end){
+    for (auto it = range_begin; it != range_end; it++) {
+            cout << *it << " "s;
+            
+        }
+        cout << endl;
     }
-    cout << endl;
+
+template <typename It_Begin, typename It_End>
+vector<int> SliceVec(It_Begin it_begin, It_End it_end)
+{
+    vector<int> vec;
+    for (auto i = it_begin; i < it_end; ++i){
+        vec.push_back(*i);
+    }
+        
+	return vec;
 }
 
-template <typename It>
-auto MakeSet(It range_begin, It range_end) {
-    return set(range_begin, range_end);
+vector<int> SliceVec2(vector<int> vector_input, int it_begin, int it_end)
+{
+    vector<int> vec;
+    for (auto i = it_begin; i < it_end; ++i){
+        vec.push_back(vector_input[i]);
+    }
+        
+	return vec;
 }
-
-template <typename It>
-auto MakeVector(It range_begin, It range_end) {
-    return vector(range_begin, range_end);
-}
-
-
-
-template <typename Container, typename Iterator>
-void EraseAndPrint(Container& container, Iterator iter_beyond, Iterator p1,  Iterator p2) {
-
-    auto res = container.erase(container.begin() +iter_beyond);
-	PrintRange(container.begin(), res);
-    PrintRange(res, container.end());
-    
-    auto res_2 = container.erase(container.begin() +p1, container.begin() +p2);
-    PrintRange(container.begin(), res_2);
-    PrintRange(res_2, container.end());
-}
-
 
 
 int main() {
-    vector<string> langs = {"Python"s, "Java"s, "C#"s, "Ruby"s, "C++"s};
-    EraseAndPrint(langs, 0, 0, 2);
+    vector<int> test_vector(10);
+    // iota             -> http://ru.cppreference.com/w/cpp/algorithm/iota
+    // Заполняет диапазон последовательно возрастающими значениями
+    iota(test_vector.begin(), test_vector.end(), 1);
+    // random_shuffle   -> https://ru.cppreference.com/w/cpp/algorithm/random_shuffle
+    // Перемешивает элементы в случайном порядке
+    random_shuffle(test_vector.begin(), test_vector.end());
+    vector<int> new_vector = SliceVec(test_vector.begin(), test_vector.end());
+    vector<int> new_vector2 = SliceVec2(test_vector, 0, test_vector.size()/2);
+    vector<int> new_vector3 = SliceVec2(test_vector, test_vector.size()/2, test_vector.size());
+    // Выводим вектор до сортировки
+    PrintRange(test_vector.begin(), test_vector.end());
+    // Сортируем вектор с помощью сортировки слиянием
+    //MergeSort(test_vector.begin(), test_vector.end());
+    // Выводим результат
+    //PrintRange(test_vector.begin(), test_vector.end());
+    PrintRange(new_vector.begin(), new_vector.end());
+    PrintRange(new_vector2.begin(), new_vector2.end());
+    PrintRange(new_vector3.begin(), new_vector3.end());
+    
     return 0;
 }
