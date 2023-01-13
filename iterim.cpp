477bf0922099cec0055a27f9703a58f439a2f5eb
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-01-13 19:03:51 +0300

    Update iterim.cpp

--- iterim.cpp
+++ iterim.cpp
@@ -26,6 +26,58 @@ using namespace std;
 /*template <typename RandomIt>
 void MergeSort(RandomIt range_begin, RandomIt range_end)*/
 
+
+
+/*template<typename T>
+void merge(vector<T> &vec, vector<T> &v1, vector<T> &v2) {
+    auto siz1 = v1.size();
+    auto siz2 = v2.size();
+    size_t p1 = 0;
+    size_t p2 = 0;
+
+    while (p1 < siz1 && p2 < siz2) {
+        if (v1.at(p1) < v2.at(p2))
+            vec.push_back(v1.at(p1++));
+        else
+            vec.push_back(v2.at(p2++));
+    }
+
+    while (p1 < siz1) vec.push_back(v1.at(p1++));
+    while (p2 < siz2) vec.push_back(v2.at(p2++));
+}
+
+template<typename T>
+void MergeSort(vector<T> &vec) {
+    if (vec.size() <= 1)
+        return;
+
+    auto iter = vec.begin() + vec.size() / 2;
+    vector<T> v1(vec.begin(), iter);
+    vector<T> v2(iter, vec.end());
+
+    MergeSort(v1);
+    MergeSort(v2);
+
+    vec.clear();
+    merge(vec, v1, v2);
+}*/
+
+void bubbleSort(vector<int>& a)
+{
+      bool swapp = true;
+      while(swapp){
+        swapp = false;
+        for (size_t i = 0; i < a.size()-1; i++) {
+            if (a[i]>a[i+1] ){
+                a[i] += a[i+1];
+                a[i+1] = a[i] - a[i+1];
+                a[i] -=a[i+1];
+                swapp = true;
+            }
+        }
+    }
+}
+
 template <typename It>
 void PrintRange(It range_begin, It range_end){
     for (auto it = range_begin; it != range_end; it++) {
@@ -73,9 +125,12 @@ int main() {
     // Сортируем вектор с помощью сортировки слиянием
     //MergeSort(test_vector.begin(), test_vector.end());
     // Выводим результат
-    //PrintRange(test_vector.begin(), test_vector.end());
+    PrintRange(test_vector.begin(), test_vector.end());
     PrintRange(new_vector.begin(), new_vector.end());
     PrintRange(new_vector2.begin(), new_vector2.end());
+    PrintRange(new_vector3.begin(), new_vector3.end());
+    
+    bubbleSort(new_vector3);
     PrintRange(new_vector3.begin(), new_vector3.end());
     
     return 0;
