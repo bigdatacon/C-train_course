3cf8f4ba1eaa4be579859ee2056e5c4622db9b8c
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-01-18 00:03:23 +0300

    Update iterim.cpp

--- iterim.cpp
+++ iterim.cpp
@@ -1,213 +1,136 @@
-#include <iostream>
-#include <vector>
-#include <map>
+#include <stack>
 #include <algorithm>
+#include <iostream>
 #include <numeric>
-#include <iterator>
-#include <set>
+#include <vector>
 
+/*
+Задание 1
+Внутри контейнера queue в стандартной библиотеке находится deque. Класс queue даёт доступ только к определённым возможностям класса deque. Вам реализовывать класс queue с применением deque не нужно. Сделайте свою реализацию, используя два стека.
+Вы спросите, зачем заниматься такой ерундой. Вспомним задачу о нахождении минимума среди элементов стека без перебора. Структура стека позволяет сделать это достаточно легко. Но структура deque так не сможет. Поэтому, если для очереди понадобится поддержка поиска минимума или максимума без перебора элементов, реализация с применением стеков — самая подходящая.
+Напишите реализацию методов класса Queue, отвечающих принципу FIFO, если внутри контейнера два стека.
+Внимание, ваша очередь имеет лишь неконстантную версию метода Front. В этом задании для доступа к первому элементу понадобится переместить элементы из одного стека в другой, а в константном методе это сделать проблематично. Язык C++ в некоторых случаях разрешает константным методам изменять поля класса. Эту возможность рассмотрим в будущих уроках.
+
+ВЫВОД:
+5 2 4 3 1
+Заполняем очередь
+Вставленный элемент 5
+Первый элемент очереди 5
+Вставленный элемент 2
+Первый элемент очереди 5
+Вставленный элемент 4
+Первый элемент очереди 5
+Вставленный элемент 3
+Первый элемент очереди 5
+Вставленный элемент 1
+Первый элемент очереди 5
+Вынимаем элементы из очереди
+Будем вынимать элемент 5
+Будем вынимать элемент 2
+Будем вынимать элемент 4
+Будем вынимать элемент 3
+Будем вынимать элемент 1
+
+ПОДСКАЗКА:
+Посмотрите на вставку элемента. Сначала вариантов немного: просто складывайте элементы в первый стек. Когда накопится несколько элементов, вы хотите что-нибудь оттуда вытащить. По принципу FIFO первым должен выйти тот элемент, который сейчас находится на дне первого стека. Нужно поправить эту ситуацию. Переложите все элементы первого стека во второй.
+*/
 
 using namespace std;
-/*
-Задание 3
-Продолжите исследовать стек и то, как его можно реализовать.
-В этот раз напишите класс SortedStack, который бы всегда поддерживал свои элементы в отсортированном виде. При вставке сортировка должна сохраняться. На вершине стека всегда должен быть минимум. Не используйте в этой задаче алгоритмы сортировки — её можно решить без них.
-Методы стека перестанут быть такими простыми, эффективными и быстрыми, как раньше. В них могут появиться условия и циклы. Это цена, которую вы платите за то, что стек будет постоянно находиться в сортированном состоянии.
-Используйте заготовку и допишите необходимую реализацию методов Push и Pop:
-
-Подсказка
-Вам нужно вставить новый элемент. Ответьте на некие три вопроса, которые вы где-то уже видели.
-Какие параметры принимает функция? Функция Push принимает элемент для вставки. Это вы уже знаете.
-В каком случае мы можем остановить алгоритм поиска места для вставки и вставить элемент без дальнейших действий? Если стек пуст или если элемент больше или равен элементу на вершине стека.
-Что делать, если алгоритм не остановился? Это значит, что элемент сверху больше того, который вы хотите вставить. Вытащите его, но сохраните, чтобы потом вернуть. Теперь у вас есть стек без верхнего элемента. Попробуйте повторить для него весь алгоритм.
-В итоге вы найдёте место для вставки, вставите новый элемент, а дальше нужно будет вернуть все вытащенные элементы на место.
-Вывод:
-Вставляемый элемент = 5
-5
-Вставляемый элемент = 2
-5 2
-Вставляемый элемент = 4
-5 4 2
-Вставляемый элемент = 3
-5 4 3 2
-Вставляемый элемент = 1
-5 4 3 2 1 
-*/
 
 template <typename It>
 void PrintRange(It range_begin, It range_end) {
-	for (auto it = range_begin; it != range_end; ++it) {
-		cout << *it << " "s;
-	}
-	cout << endl;
+    for (auto it = range_begin; it != range_end; ++it) {
+        cout << *it << " "s;
+    }
+    cout << endl;
 }
 
 template <typename Type>
 class Stack {
 public:
-	void Push(const Type& element) {
-		elements_.push_back(element);
-	}
-	void Pop() {
-		// напишите реализацию
-		elements_.pop_back();
-	}
-	const Type& Peek() const {
-		//return elements_;
-		return elements_.back();
-		// напишите реализацию
-	}
-	Type& Peek() {
-		// напишите реализацию
-		//return elements_;
-		return elements_.back();
-	}
-	void Print() const {
-		// напишите реализацию
-		for (auto el : elements_) { cout << el << " "; }
-		cout << endl;
-	}
-	uint64_t Size() const {
-		// напишите реализацию
-		return elements_.size();
-	}
-	bool IsEmpty() const {
-		// напишите реализацию
-		return elements_.empty();
-	}
-
-private:
-	vector<Type> elements_;
-};
-
-template <typename Type>
-class StackMin {
-public:
-	void Push(const Type& element) {
-		// напишите реализацию метода
-		//elements_.push_back(element);
-		 // для первого добавления минимальный элемент это 1 элемент списка 
-		if (elements_.IsEmpty()) {
-			min_element_ = element;
-			elements_.Push(element);
-			elements_minimus_.push_back({element, min_element_});
-
-		}
-		else {
-			bool new_min = element < min_element_;
-			if (new_min) {
-                min_element_ = element;
-                elements_.Push(element);
-                elements_minimus_.push_back({element, min_element_});
-			}
-			else { 
-            elements_.Push(element);
-            elements_minimus_.push_back({element, min_element_}); }
-
-		}
-	}
-		void Pop() {
-			// напишите реализацию метода
-			//elements_.pop_back();
-			elements_.Pop();
-            elements_minimus_.pop_back();
-            
-		}
-		const Type& Peek() const {
-			return elements_.Peek();
-		}
-		Type& Peek() {
-			return elements_.Peek();
-		}
-		void Print() const {
-			// работу этого метода мы проверять не будем,
-			// но если он вам нужен, то можете его реализовать
-			elements_.Print();
-		}
-        
-        void Print_mins() const {
-			// работу этого метода мы проверять не будем,
-			// но если он вам нужен, то можете его реализовать
-			for (auto el : elements_minimus_) {cout << "first: " << el.first << " second :" << el.second << " ";}
-		}
-        
-		uint64_t Size() const {
-			return elements_.size();
-		}
-		bool IsEmpty() const {
-			return elements_.IsEmpty();
-		}
-		const Type& PeekMin() const {
-			// напишите реализацию метода
-			return elements_minimus_.back().second;
-		}
-		Type& PeekMin() {
-			// напишите реализацию метода
-			return elements_minimus_.back().second;
-		}
-private:
-	Stack<Type> elements_;
-	// возможно, здесь вам понадобится что-то изменить
-	Type min_element_;  // задаю начальный максимум нулем
-    //vector<map<Type, Type>> elements_minimus_;
-    vector<pair<Type, Type>> elements_minimus_;
-
-	};
-
-
-template <typename Type>
-class SortedStack {
-public:
     void Push(const Type& element) {
-    // напишите реализацию метода
-    	if (elements_.IsEmpty()) {
-			elements_.Push(element);
-
-		}
-		else {
-            // Запрашиваем итератор на первый элемент, не меньший border;
-            // если такого элемента нет, то мы получим numbers.end()
-            //auto first_not_less = elements_.lower_bound(element);  -- ни так ни так не работает 
-            auto first_not_less = elements_.lower_bound(elements_.begin(), elements_.end(), element);
-            elements_.insert(first_not_less, element);
-    }
+        elements_.push_back(element);
     }
     void Pop() {
-    // напишите реализацию метода
-        elements_.Pop();
+        elements_.pop_back();
     }
     const Type& Peek() const {
-    return elements_.Peek();
+        return elements_.back();
     }
     Type& Peek() {
-    return elements_.Peek();
+        return elements_.back();
     }
     void Print() const {
-    elements_.Print();
+        PrintRange(elements_.begin(), elements_.end());
     }
     uint64_t Size() const {
-    return elements_.Size();
+        return elements_.size();
     }
     bool IsEmpty() const {
-    return elements_.IsEmpty();
+        return elements_.empty();
     }
+
 private:
-    Stack<Type> elements_;
+    vector<Type> elements_;
 };
 
+template <typename Type>
+class Queue {
+public:
+    void Push(const Type& element) {
+        // напишите реализацию
+        //in_stack_.Push(element);   // ни так ни так не работает 
+        in_stack_.push_back(element);
+    }
+    void Pop() {
+        // напишите реализацию
+        if (!out_stack_.IsEmpty()) {
+            out_stack_.Pop();
+        }
+        else {
+            for (auto el : in_stack_) { out_stack_.Push(el); }  // добавляю элементы в outstack 
+            out_stack_.Pop(); // беру последний элемент из outstack 
+        }
+    }
+    Type& Front() {
+        // напишите реализацию
+        return Pop();
+    }
+    uint64_t Size() const {
+        // напишите реализацию
+        return in_stack_.Size();
+    }
+    bool IsEmpty() const {
+        return in_stack_.Empty();
+    }
 
+private:
+    stack<Type> in_stack_;
+    stack<Type> out_stack_;
+};
 
 int main() {
-    SortedStack<int> stack;
+    Queue<int> queue;
     vector<int> values(5);
-    // заполняем вектор для тестирования нашего стека
+    // заполняем вектор для тестирования очереди
     iota(values.begin(), values.end(), 1);
     // перемешиваем значения
     random_shuffle(values.begin(), values.end());
-    // заполняем стек и проверяем, что сортировка сохраняется после каждой вставки
+    PrintRange(values.begin(), values.end());
+    cout << "Заполняем очередь"s << endl;
+    // заполняем очередь и выводим элемент в начале очереди
     for (int i = 0; i < 5; ++i) {
-        cout << "Вставляемый элемент = "s << values[i] << endl;
-        stack.Push(values[i]);
-        stack.Print();
+        queue.Push(values[i]);
+        cout << "Вставленный элемент "s << values[i] << endl;
+        cout << "Первый элемент очереди "s << queue.Front() << endl;
     }
+    cout << "Вынимаем элементы из очереди"s << endl;
+    // выводим элемент в начале очереди и вытаскиваем элементы по одному
+    while (!queue.IsEmpty()) {
+        // сначала будем проверять начальный элемент, а потом вытаскивать,
+        // так как операция Front на пустой очереди не определена
+        cout << "Будем вынимать элемент "s << queue.Front() << endl;
+        queue.Pop();
+    }
+    return 0;
 }
