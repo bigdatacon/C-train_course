29c99767d6f09640beee11efcddbcedfeccc6725
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-01-17 16:08:16 +0300

    Update firstcode.cpp

--- firstcode.cpp
+++ firstcode.cpp
@@ -1,46 +1,84 @@
-#include <algorithm>
 #include <iostream>
-#include <string>
-#include <utility>
 #include <vector>
 
 using namespace std;
 /*
-Задание 3
-Обобщим предыдущую задачу. Теперь префикс будет не одним символом типа char, а строкой с любым количеством символов.
+Задание 1
+Чтобы работать со стеком по всем правилам LIFO, напишите класс, который поможет это сделать. Храните элементы стека в векторе. Напишите свою реализацию методов Push, Pop, Peek, Size и IsEmpty. Для удобства добавьте ещё метод Print. Элементы могут быть разных типов, поэтому нужен класс-шаблон. 
+Методов Peek два, потому что один из них будет вызываться в случае константного объекта, а другой — в остальных случаях. При этом возвращаемое значение будет разным. Если объект константный, нужно гарантировать, что пользователь не изменит тот объект, на который ему передаётся ссылка. Поэтому в константном методе возвращается const Type&. В другом случае можно спокойно вернуть ссылку без const, так как стек не будет неизменяемым объектом — пользователь может свободно его менять. Этот же принцип будет выполняться всегда, когда предоставляете пользователю доступ к элементам контейнера.
 
 
-Эта задача аналогична предыдущей, но с одной тонкостью. Как составить ближайшую строку, которая будет больше prefix? Просто увеличить на единицу последний символ.
+Подсказка 
+Добавление и удаление в вектор должны происходить с одного конца. Удобнее сделать это именно с конца, а не с начала. Примените стандартные методы контейнера vector.
 
-Пример вывода
-moscow motovilikha
-2 2
-3 3 
+Вывод: 
+0
+0 1
+0 1 2
+0 1 2 3
+0 1 2 3 4
+0 1 2 3 4 5
+0 1 2 3 4 5 6
+0 1 2 3 4 5 6 7
+0 1 2 3 4 5 6 7 8
+0 1 2 3 4 5 6 7 8 9
+0 1 2 3 4 5 6 7 8
+0 1 2 3 4 5 6 7
+0 1 2 3 4 5 6
+0 1 2 3 4 5
+0 1 2 3 4
+0 1 2 3
+0 1 2
+0 1
+0
 */
 
-
-template <typename RandomIt>
-pair<RandomIt, RandomIt> FindStartsWith(RandomIt range_begin, RandomIt range_end, string prefix) {
-    auto left = lower_bound(range_begin, range_end, prefix); // левый получинтерва
-    char next_prefix = static_cast<char>(prefix.back() + 1); // нахожу букву следуюшую за последней
-    string new_string = prefix.substr(0,-1)+next_prefix; // составляю новую строку в которой все неизменно кроме //последнй буквы
-    auto right = lower_bound(range_begin, range_end,  new_string);
-
-    return {left, right};
+template <typename It>
+void PrintRange(It range_begin, It range_end) {
+    for (auto it = range_begin; it != range_end; ++it) {
+        cout << *it << " "s;
+    }
+    cout << endl;
 }
 
+template <typename Type>
+class Stack {
+public:
+    void Push(const Type& element) {
+        // напишите реализацию
+    }
+    void Pop() {
+        // напишите реализацию
+    }
+    const Type& Peek() const {
+        // напишите реализацию
+    }
+    Type& Peek() {
+        // напишите реализацию
+    }
+    void Print() const {
+        // напишите реализацию
+    }
+    uint64_t Size() const {
+        // напишите реализацию
+    }
+    bool IsEmpty() const {
+        // напишите реализацию
+    }
+
+private:
+    vector<Type> elements_;
+};
 
 
 int main() {
-    const vector<string> sorted_strings = {"moscow", "motovilikha", "murmansk"};
-    const auto mo_result = FindStartsWith(begin(sorted_strings), end(sorted_strings), "mo");
-    for (auto it = mo_result.first; it != mo_result.second; ++it) {
-        cout << *it << " ";
+    Stack<int> stack;
+    for (uint32_t i = 0; i < 10; ++i) {
+        stack.Push(i);
+        stack.Print();
     }
-    cout << endl;
-    const auto mt_result = FindStartsWith(begin(sorted_strings), end(sorted_strings), "mt");
-    cout << (mt_result.first - begin(sorted_strings)) << " " << (mt_result.second - begin(sorted_strings)) << endl;
-    const auto na_result = FindStartsWith(begin(sorted_strings), end(sorted_strings), "na");
-    cout << (na_result.first - begin(sorted_strings)) << " " << (na_result.second - begin(sorted_strings)) << endl;
-    return 0;
-} 
+    while (!stack.IsEmpty()) {
+        stack.Pop();
+        stack.Print();
+    }
+}
