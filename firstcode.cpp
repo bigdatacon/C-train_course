42f954781e8168ffc80b2b210c0424b746f6866a
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-01-17 14:02:07 +0300

    Update firstcode.cpp

--- firstcode.cpp
+++ firstcode.cpp
@@ -1,37 +1,46 @@
-/*
-Задание 2
-Напишите функцию FindStartsWith, которая
-принимает отсортированный набор строк в виде итераторов range_begin, range_end и один символ prefix;
-возвращает полуинтервал [range_begin, range_end) строк в виде пары итераторов. Эти строки начинаются с символа prefix.
-Если итоговый диапазон пуст, его границы должны указывать на место в контейнере, куда можно без нарушения порядка сортировки вставить любую строку, начинающуюся с символа prefix — подобно алгоритму equal_range. Гарантируется, что строки состоят лишь из строчных латинских букв, а символ prefix — строчная латинская буква.
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <utility>
+#include <vector>
 
-ПОДСКАЗКА 
-Символы, которые относятся к типу данных char, можно складывать с числами. Вычитать из них числа тоже можно. Так вы получите следующие или предыдущие буквы в алфавитном порядке. Например, для строки s выражение -s[0] заменит её первую букву на предыдущую.
-Выражение 'a' + 1 имеет тип int. Поэтому может понадобиться привести его к типу char, применив static_cast.
+using namespace std;
+/*
+Задание 3
+Обобщим предыдущую задачу. Теперь префикс будет не одним символом типа char, а строкой с любым количеством символов.
+
+
+Эта задача аналогична предыдущей, но с одной тонкостью. Как составить ближайшую строку, которая будет больше prefix? Просто увеличить на единицу последний символ.
 
 Пример вывода
-
-moscow murmansk
+moscow motovilikha
 2 2
 3 3 
 */
 
+
 template <typename RandomIt>
-pair<RandomIt, RandomIt> FindStartsWith(RandomIt range_begin, RandomIt range_end, char prefix) {
-    // напишите реализацию
+pair<RandomIt, RandomIt> FindStartsWith(RandomIt range_begin, RandomIt range_end, string prefix) {
+    auto left = lower_bound(range_begin, range_end, prefix); // левый получинтерва
+    char next_prefix = static_cast<char>(prefix.back() + 1); // нахожу букву следуюшую за последней
+    string new_string = prefix.substr(0,-1)+next_prefix; // составляю новую строку в которой все неизменно кроме //последнй буквы
+    auto right = lower_bound(range_begin, range_end,  new_string);
+
+    return {left, right};
 }
+
 
 
 int main() {
-    const vector<string> sorted_strings = {"moscow", "murmansk", "vologda"};
-    const auto m_result = FindStartsWith(begin(sorted_strings), end(sorted_strings), 'm');
-    for (auto it = m_result.first; it != m_result.second; ++it) {
+    const vector<string> sorted_strings = {"moscow", "motovilikha", "murmansk"};
+    const auto mo_result = FindStartsWith(begin(sorted_strings), end(sorted_strings), "mo");
+    for (auto it = mo_result.first; it != mo_result.second; ++it) {
         cout << *it << " ";
     }
     cout << endl;
-    const auto p_result = FindStartsWith(begin(sorted_strings), end(sorted_strings), 'p');
-    cout << (p_result.first - begin(sorted_strings)) << " " << (p_result.second - begin(sorted_strings)) << endl;
-    const auto z_result = FindStartsWith(begin(sorted_strings), end(sorted_strings), 'z');
-    cout << (z_result.first - begin(sorted_strings)) << " " << (z_result.second - begin(sorted_strings)) << endl;
+    const auto mt_result = FindStartsWith(begin(sorted_strings), end(sorted_strings), "mt");
+    cout << (mt_result.first - begin(sorted_strings)) << " " << (mt_result.second - begin(sorted_strings)) << endl;
+    const auto na_result = FindStartsWith(begin(sorted_strings), end(sorted_strings), "na");
+    cout << (na_result.first - begin(sorted_strings)) << " " << (na_result.second - begin(sorted_strings)) << endl;
     return 0;
-}
+} 
