cd0c2cd10751c7d95031667fe8b9f6aa5c2b7237
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-01-18 22:54:12 +0300

    Update wall.h

--- folder/wall.h
+++ folder/wall.h
@@ -1,3 +1,19 @@
+class Wall {
+public:
+    enum class Color { BLUE, GREEN, RED, WHITE, YELLOW };
+
+    Wall(double width, double height);
+
+    double GetHeight() const;
+    double GetWidth() const;
+    void SetColor(Color color);
+    Color GetColor() const;
+
+private:
+    double width_;
+    double height_;
+    Color color_;
+};
 
 /* Так было раньше до разнесения на 2 файла
 class Wall {
