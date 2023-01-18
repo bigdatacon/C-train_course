ac0dd741842b0603560d462312342bcc3141bc28
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-01-18 13:36:09 +0300

    Update firstcode.cpp

--- firstcode.cpp
+++ firstcode.cpp
@@ -1,84 +1,439 @@
-#include <stack>
-
 /*
-Задание 1
-Внутри контейнера queue в стандартной библиотеке находится deque. Класс queue даёт доступ только к определённым возможностям класса deque. Вам реализовывать класс queue с применением deque не нужно. Сделайте свою реализацию, используя два стека.
-Вы спросите, зачем заниматься такой ерундой. Вспомним задачу о нахождении минимума среди элементов стека без перебора. Структура стека позволяет сделать это достаточно легко. Но структура deque так не сможет. Поэтому, если для очереди понадобится поддержка поиска минимума или максимума без перебора элементов, реализация с применением стеков — самая подходящая.
-Напишите реализацию методов класса Queue, отвечающих принципу FIFO, если внутри контейнера два стека.
-Внимание, ваша очередь имеет лишь неконстантную версию метода Front. В этом задании для доступа к первому элементу понадобится переместить элементы из одного стека в другой, а в константном методе это сделать проблематично. Язык C++ в некоторых случаях разрешает константным методам изменять поля класса. Эту возможность рассмотрим в будущих уроках.
+Задание 
+Сохраните решение этой задачи: локально или в git. Оно вам ещё понадобится.
+Напишите класс RequestQueue, принимающий запросы на поиск, — AddFindRequest. Класс RequestQueue должен уметь ответить на вопрос, сколько за последние сутки было запросов, на которые ничего не нашлось. Заготовка класса будет выглядеть так: class RequestQueue { внизу перед main
 
-ВЫВОД: 
-5 2 4 3 1
-Заполняем очередь
-Вставленный элемент 5
-Первый элемент очереди 5
-Вставленный элемент 2
-Первый элемент очереди 5
-Вставленный элемент 4
-Первый элемент очереди 5
-Вставленный элемент 3
-Первый элемент очереди 5
-Вставленный элемент 1
-Первый элемент очереди 5
-Вынимаем элементы из очереди
-Будем вынимать элемент 5
-Будем вынимать элемент 2
-Будем вынимать элемент 4
-Будем вынимать элемент 3
-Будем вынимать элемент 1 
+Вывод: 
+Total empty requests: 1437 
 
-ПОДСКАЗКА: 
-Посмотрите на вставку элемента. Сначала вариантов немного: просто складывайте элементы в первый стек. Когда накопится несколько элементов, вы хотите что-нибудь оттуда вытащить. По принципу FIFO первым должен выйти тот элемент, который сейчас находится на дне первого стека. Нужно поправить эту ситуацию. Переложите все элементы первого стека во второй.
+Подсказка 
+В момент появления нового запроса:
+Увеличьте время на одну секунду (запросы приходят раз в секунду);
+Удалите из дека все запросы, которые успели устареть;
+Не забудьте во время удаления уменьшить количество запросов с пустым вектором ответов, если нужно;
+Добавьте новый запрос в дек и обновите количество запросов без результатов поиска.
 */
 
+/* решение задачи "Выводим результаты поиска страницами" из темы "Итераторы" */
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <set>
+#include <stdexcept>
+#include <string>
+#include <utility>
+#include <vector>
+ 
 using namespace std;
+ 
+const int MAX_RESULT_DOCUMENT_COUNT = 5;
+ 
+string ReadLine() {
+    string s;
+    getline(cin, s);
+    return s;
+}
+ 
+int ReadLineWithNumber() {
+    int result;
+    cin >> result;
+    ReadLine();
+    return result;
+}
+ 
+vector<string> SplitIntoWords(const string& text) {
+    vector<string> words;
+    string word;
+    for (const char c : text) {
+        if (c == ' ') {
+            if (!word.empty()) {
+                words.push_back(word);
+                word.clear();
+            }
+        } else {
+            word += c;
+        }
+    }
+    if (!word.empty()) {
+        words.push_back(word);
+    }
+ 
+    return words;
+}
+ 
+struct Document {
+    Document() = default;
+ 
+    Document(int id, double relevance, int rating)
+        : id(id)
+        , relevance(relevance)
+        , rating(rating) {
+ 
+    }
+ 
+    int id = 0;
+    double relevance = 0.0;
+    int rating = 0;
+};
+ 
+ostream& operator << (ostream& out, const Document search){
+    return out << "{ document_id = " << search.id << ", relevance = " << search.relevance << ", rating = " << search.rating << " }";
+ }
 
-template <typename Type>
-class Queue {
+ /*ostream& operator << (ostream& out, const Document search){
+    return out << "{ " << search.id << ", " << search.relevance << ", " << search.rating << " }";
+ }*/
+
+
+
+
+ 
+template <typename StringContainer>
+set<string> MakeUniqueNonEmptyStrings(const StringContainer& strings) {
+    set<string> non_empty_strings;
+    for (const string& str : strings) {
+        if (!str.empty()) {
+            non_empty_strings.insert(str);
+        }
+    }
+    return non_empty_strings;
+}
+ 
+enum class DocumentStatus {
+    ACTUAL,
+    IRRELEVANT,
+    BANNED,
+    REMOVED,
+};
+ 
+class SearchServer {
 public:
-    void Push(const Type& element) {
-        // напишите реализацию
+ 
+ 
+ 
+    template <typename StringContainer>
+    explicit SearchServer(const StringContainer& stop_words)
+        : stop_words_(MakeUniqueNonEmptyStrings(stop_words))  // Extract non-empty stop words
+    {
+        if (!all_of(stop_words_.begin(), stop_words_.end(), IsValidWord)) {
+            throw invalid_argument("Some of stop words are invalid"s);
+        }
     }
-    void Pop() {
-        // напишите реализацию
+ 
+    explicit SearchServer(const string& stop_words_text)
+        : SearchServer(
+            SplitIntoWords(stop_words_text))  // Invoke delegating constructor from string container
+    {
     }
-    Type& Front() {
-        // напишите реализацию
+ 
+    void AddDocument(int document_id, const string& document, DocumentStatus status,
+                     const vector<int>& ratings) {
+        if ((document_id < 0) || (documents_.count(document_id) > 0)) {
+            throw invalid_argument("Invalid document_id"s);
+        }
+        const auto words = SplitIntoWordsNoStop(document);
+ 
+        const double inv_word_count = 1.0 / words.size();
+        for (const string& word : words) {
+            word_to_document_freqs_[word][document_id] += inv_word_count;
+        }
+        documents_.emplace(document_id, DocumentData{ComputeAverageRating(ratings), status});
+        document_ids_.push_back(document_id);
     }
-    uint64_t Size() const {
-        // напишите реализацию
+ 
+    template <typename DocumentPredicate>
+    vector<Document> FindTopDocuments(const string& raw_query,
+                                      DocumentPredicate document_predicate) const {
+        const auto query = ParseQuery(raw_query);
+ 
+        auto matched_documents = FindAllDocuments(query, document_predicate);
+ 
+        sort(matched_documents.begin(), matched_documents.end(),
+             [](const Document& lhs, const Document& rhs) {
+                 if (abs(lhs.relevance - rhs.relevance) < 1e-6) {
+                     return lhs.rating > rhs.rating;
+                 } else {
+                     return lhs.relevance > rhs.relevance;
+                 }
+             });
+        if (matched_documents.size() > MAX_RESULT_DOCUMENT_COUNT) {
+            matched_documents.resize(MAX_RESULT_DOCUMENT_COUNT);
+        }
+ 
+        return matched_documents;
     }
-    bool IsEmpty() const {
-        // напишите реализацию
+ 
+    vector<Document> FindTopDocuments(const string& raw_query, DocumentStatus status) const {
+        return FindTopDocuments(
+            raw_query, [status](int document_id, DocumentStatus document_status, int rating) {
+                return document_status == status;
+            });
     }
-
+ 
+    vector<Document> FindTopDocuments(const string& raw_query) const {
+        return FindTopDocuments(raw_query, DocumentStatus::ACTUAL);
+    }
+ 
+    int GetDocumentCount() const {
+        return documents_.size();
+    }
+ 
+    int GetDocumentId(int index) const {
+        return document_ids_.at(index);
+    }
+ 
+    tuple<vector<string>, DocumentStatus> MatchDocument(const string& raw_query,
+                                                        int document_id) const {
+        const auto query = ParseQuery(raw_query);
+ 
+        vector<string> matched_words;
+        for (const string& word : query.plus_words) {
+            if (word_to_document_freqs_.count(word) == 0) {
+                continue;
+            }
+            if (word_to_document_freqs_.at(word).count(document_id)) {
+                matched_words.push_back(word);
+            }
+        }
+        for (const string& word : query.minus_words) {
+            if (word_to_document_freqs_.count(word) == 0) {
+                continue;
+            }
+            if (word_to_document_freqs_.at(word).count(document_id)) {
+                matched_words.clear();
+                break;
+            }
+        }
+        return {matched_words, documents_.at(document_id).status};
+    }
+ 
 private:
-    stack<Type> stack1_;
-    stack<Type> stack2_;
+    struct DocumentData {
+        int rating;
+        DocumentStatus status;
+    };
+    const set<string> stop_words_;
+    map<string, map<int, double>> word_to_document_freqs_;
+    map<int, DocumentData> documents_;
+    vector<int> document_ids_;
+ 
+    bool IsStopWord(const string& word) const {
+        return stop_words_.count(word) > 0;
+    }
+ 
+    static bool IsValidWord(const string& word) {
+        // A valid word must not contain special characters
+        return none_of(word.begin(), word.end(), [](char c) {
+            return c >= '\0' && c < ' ';
+        });
+    }
+ 
+    vector<string> SplitIntoWordsNoStop(const string& text) const {
+        vector<string> words;
+        for (const string& word : SplitIntoWords(text)) {
+            if (!IsValidWord(word)) {
+                throw invalid_argument("Word "s + word + " is invalid"s);
+            }
+            if (!IsStopWord(word)) {
+                words.push_back(word);
+            }
+        }
+        return words;
+    }
+ 
+    static int ComputeAverageRating(const vector<int>& ratings) {
+        if (ratings.empty()) {
+            return 0;
+        }
+        int rating_sum = 0;
+        for (const int rating : ratings) {
+            rating_sum += rating;
+        }
+        return rating_sum / static_cast<int>(ratings.size());
+    }
+ 
+    struct QueryWord {
+        string data;
+        bool is_minus;
+        bool is_stop;
+    };
+ 
+    QueryWord ParseQueryWord(const string& text) const {
+        if (text.empty()) {
+            throw invalid_argument("Query word is empty"s);
+        }
+        string word = text;
+        bool is_minus = false;
+        if (word[0] == '-') {
+            is_minus = true;
+            word = word.substr(1);
+        }
+        if (word.empty() || word[0] == '-' || !IsValidWord(word)) {
+            throw invalid_argument("Query word "s + text + " is invalid");
+        }
+ 
+        return {word, is_minus, IsStopWord(word)};
+    }
+ 
+    struct Query {
+        set<string> plus_words;
+        set<string> minus_words;
+    };
+ 
+    Query ParseQuery(const string& text) const {
+        Query result;
+        for (const string& word : SplitIntoWords(text)) {
+            const auto query_word = ParseQueryWord(word);
+            if (!query_word.is_stop) {
+                if (query_word.is_minus) {
+                    result.minus_words.insert(query_word.data);
+                } else {
+                    result.plus_words.insert(query_word.data);
+                }
+            }
+        }
+        return result;
+    }
+ 
+    // Existence required
+    double ComputeWordInverseDocumentFreq(const string& word) const {
+        return log(GetDocumentCount() * 1.0 / word_to_document_freqs_.at(word).size());
+    }
+ 
+    template <typename DocumentPredicate>
+    vector<Document> FindAllDocuments(const Query& query,
+                                      DocumentPredicate document_predicate) const {
+        map<int, double> document_to_relevance;
+        for (const string& word : query.plus_words) {
+            if (word_to_document_freqs_.count(word) == 0) {
+                continue;
+            }
+            const double inverse_document_freq = ComputeWordInverseDocumentFreq(word);
+            for (const auto [document_id, term_freq] : word_to_document_freqs_.at(word)) {
+                const auto& document_data = documents_.at(document_id);
+                if (document_predicate(document_id, document_data.status, document_data.rating)) {
+                    document_to_relevance[document_id] += term_freq * inverse_document_freq;
+                }
+            }
+        }
+ 
+        for (const string& word : query.minus_words) {
+            if (word_to_document_freqs_.count(word) == 0) {
+                continue;
+            }
+            for (const auto [document_id, _] : word_to_document_freqs_.at(word)) {
+                document_to_relevance.erase(document_id);
+            }
+        }
+ 
+        vector<Document> matched_documents;
+        for (const auto [document_id, relevance] : document_to_relevance) {
+            matched_documents.push_back(
+                {document_id, relevance, documents_.at(document_id).rating});
+        }
+        return matched_documents;
+    }
+ 
+};
+ 
+template <typename Iterator>
+struct IteratorRange{
+    Iterator begin;
+    Iterator end;
+    IteratorRange(Iterator begin, Iterator end):begin(begin), end(end){}
+};
+ 
+template <typename Iterator>
+    class Paginator {
+    public:
+ 
+    Paginator(Iterator begin, Iterator end, int size)
+        :page_size_(size){
+            vector test(begin, end);
+            Iterator temp = begin; 
+            for (; temp + size < end; temp += size){
+                pages_.push_back(IteratorRange(temp, temp + size));
+            }
+            if (temp < end) {
+                pages_.push_back(IteratorRange(temp, end));
+            }
+        }
+ 
+    auto begin() const {
+        return pages_.begin();
+    }
+    auto end() const {
+        return pages_.end();
+    }    
+    int size() const {
+        return page_size_;
+    }
+ 
+    private:
+    int page_size_;
+    vector<IteratorRange<Iterator>> pages_;
+    }; 
+ 
+template <typename Container>
+auto Paginate(const Container& c, size_t page_size) {
+    return Paginator(begin(c), end(c), page_size);
+}
+
+template<typename Iterator>
+ostream& operator<< (ostream& out, IteratorRange<Iterator> p){
+    for (auto i = p.begin; i < p.end; i++) {
+        out << *i << " ";
+    }
+    return out;
+}
+
+class RequestQueue {
+public:
+    explicit RequestQueue(const SearchServer& search_server) {
+        // напишите реализацию
+    }
+    // сделаем "обёртки" для всех методов поиска, чтобы сохранять результаты для нашей статистики
+    template <typename DocumentPredicate>
+    vector<Document> AddFindRequest(const string& raw_query, DocumentPredicate document_predicate) {
+        // напишите реализацию
+    }
+    vector<Document> AddFindRequest(const string& raw_query, DocumentStatus status) {
+        // напишите реализацию
+    }
+    vector<Document> AddFindRequest(const string& raw_query) {
+        // напишите реализацию
+    }
+    int GetNoResultRequests() const {
+        // напишите реализацию
+    }
+private:
+    struct QueryResult {
+        // определите, что должно быть в структуре
+    };
+    deque<QueryResult> requests_;
+    const static int min_in_day_ = 1440;
+    // возможно, здесь вам понадобится что-то ещё
 };
 
 int main() {
-    Queue<int> queue;
-    vector<int> values(5);
-    // заполняем вектор для тестирования очереди
-    iota(values.begin(), values.end(), 1);
-    // перемешиваем значения
-    random_shuffle(values.begin(), values.end());
-    PrintRange(values.begin(), values.end());
-    cout << "Заполняем очередь"s << endl;
-    // заполняем очередь и выводим элемент в начале очереди
-    for (int i = 0; i < 5; ++i) {
-        queue.Push(values[i]);
-        cout << "Вставленный элемент "s << values[i] << endl;
-        cout << "Первый элемент очереди "s << queue.Front() << endl;
+    SearchServer search_server("and in at"s);
+    RequestQueue request_queue(search_server);
+    search_server.AddDocument(1, "curly cat curly tail"s, DocumentStatus::ACTUAL, {7, 2, 7});
+    search_server.AddDocument(2, "curly dog and fancy collar"s, DocumentStatus::ACTUAL, {1, 2, 3});
+    search_server.AddDocument(3, "big cat fancy collar "s, DocumentStatus::ACTUAL, {1, 2, 8});
+    search_server.AddDocument(4, "big dog sparrow Eugene"s, DocumentStatus::ACTUAL, {1, 3, 2});
+    search_server.AddDocument(5, "big dog sparrow Vasiliy"s, DocumentStatus::ACTUAL, {1, 1, 1});
+    // 1439 запросов с нулевым результатом
+    for (int i = 0; i < 1439; ++i) {
+        request_queue.AddFindRequest("empty request"s);
     }
-    cout << "Вынимаем элементы из очереди"s << endl;
-    // выводим элемент в начале очереди и вытаскиваем элементы по одному
-    while (!queue.IsEmpty()) {
-        // сначала будем проверять начальный элемент, а потом вытаскивать,
-        // так как операция Front на пустой очереди не определена
-        cout << "Будем вынимать элемент "s << queue.Front() << endl;
-        queue.Pop();
-    }
+    // все еще 1439 запросов с нулевым результатом
+    request_queue.AddFindRequest("curly dog"s);
+    // новые сутки, первый запрос удален, 1438 запросов с нулевым результатом
+    request_queue.AddFindRequest("big collar"s);
+    // первый запрос удален, 1437 запросов с нулевым результатом
+    request_queue.AddFindRequest("sparrow"s);
+    cout << "Total empty requests: "s << request_queue.GetNoResultRequests() << endl;
     return 0;
 }
