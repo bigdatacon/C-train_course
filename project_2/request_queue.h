5807fbae0e263752109c0987c0b76d2a34f6f795
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-01-19 22:35:52 +0300

    Update request_queue.h

--- project_2/request_queue.h
+++ project_2/request_queue.h
@@ -6,36 +6,64 @@
 #include <iostream>
 #include "search_server.h"
 
+
 class RequestQueue {
 public:
     explicit RequestQueue(const SearchServer& search_server);
-    // сделаем "обёртки" для всех методов поиска, чтобы сохранять результаты для нашей статистики
+
     template <typename DocumentPredicate>
     std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate) {
-        // Поиск документов по запросу
-        std::vector<Document> result = server_->FindTopDocuments(raw_query, document_predicate);
-        // Поместить в очередь запрос и найденные документы
-        enqueue(raw_query, result);
+        const auto result = search_server_.FindTopDocuments(raw_query, document_predicate);
+        AddRequest(result.size());
         return result;
     }
-    std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentStatus status);
-    std::vector<Document> AddFindRequest(const std::string& raw_query);
-    int GetNoResultRequests() const;
+    std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentStatus status) ;
+    std::vector<Document> AddFindRequest(const std::string& raw_query) ;
+    int GetNoResultRequests() const ;
+private:
+    struct QueryResult {
+        uint64_t timestamp;
+        int results;
+    };
+    std::deque<QueryResult> requests_;
+    const SearchServer& search_server_;
+    int no_results_requests_;
+    uint64_t current_time_;
+    const static int min_in_day_ = 1440;
+ 
+    void AddRequest(int results_num) ;
+};
+
+
+/*
+class RequestQueue {
+public:
+    explicit RequestQueue(const SearchServer& search_server);
+
+    // сделаем "обертки" для всех методов поиска, чтобы сохранять результаты для нашей статистики
+    template <typename DocumentPredicate>
+    std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate) {
+        const auto result = search_server_.FindTopDocuments(raw_query, document_predicate);
+        AddRequest(result.size());
+        return result;
+    }
+    std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentStatus status) ;
+    std::vector<Document> AddFindRequest(const std::string& raw_query) ;
+    int GetNoResultRequests() const ;
     
 private:
     struct QueryResult {
-        // Содержит строку запроса и документы, найденные по этому запросу
-        std::string raw_query;
-        std::vector<Document> documents_found;
-
-        bool isEmpty() {return documents_found.empty();}
+        uint64_t timestamp;
+        int results;
     };
     std::deque<QueryResult> requests_;
+    const SearchServer& search_server_;
+    int no_results_requests_;
+    uint64_t current_time_;
     const static int min_in_day_ = 1440;
-    // Количество пустых ответов в очереди
-    int empty_queries_;
-    // Сам объект поискового сервера - точнее ссылка на него
-    SearchServer * server_;
+ 
+    void AddRequest(int results_num) ;
 
-    void enqueue(std::string raw_query, std::vector<Document>result);
 };
+
+*/
