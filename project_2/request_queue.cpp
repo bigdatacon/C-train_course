d4123dceeae32eacc122d4e83647af66b0ba2629
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-01-19 22:35:35 +0300

    Update request_queue.cpp

--- project_2/request_queue.cpp
+++ project_2/request_queue.cpp
@@ -3,52 +3,81 @@
 #include <string>
 #include "request_queue.h"
 
+
+
     RequestQueue::RequestQueue(const SearchServer& search_server)
-    {
-        // Запоминаем ссылку на объект поискового движка
-        server_ = const_cast<SearchServer *> (&search_server);
-        empty_queries_ = 0;
+        : search_server_(search_server)
+        , no_results_requests_(0)
+        , current_time_(0) {
     }
 
     std::vector<Document> RequestQueue::AddFindRequest(const std::string& raw_query, DocumentStatus status) {
-        // Поиск документов по запросу
-        std::vector<Document> result = server_->FindTopDocuments(raw_query, status);
-        // Поместить в очередь запрос и найденные документы
-        enqueue(raw_query, result);
+        const auto result = search_server_.FindTopDocuments(raw_query, status);
+        AddRequest(result.size());
         return result;
     }
     std::vector<Document> RequestQueue::AddFindRequest(const std::string& raw_query) {
-        // Поиск документов по запросу
-        std::vector<Document> result = server_->FindTopDocuments(raw_query);
-        // Поместить в очередь запрос и найденные документы
-        enqueue(raw_query, result);
+        const auto result = search_server_.FindTopDocuments(raw_query);
+        AddRequest(result.size());
         return result;
     }
     int RequestQueue::GetNoResultRequests() const {
-        return empty_queries_;
+        return no_results_requests_;
     }
 
-
-
-    /*const static*/ /*int min_in_day_ = 1440;*/
-    // Количество пустых ответов в очереди
-
-    // Сам объект поискового сервера - точнее ссылка на него
-    SearchServer * server_;
-
-    void RequestQueue::enqueue(std::string raw_query, std::vector<Document>result) {
-        // Поместить в очередь запрос и найденные документы
-        requests_.push_back({raw_query, result});
-        if (result.empty()) {
-            empty_queries_++;
-        }
-        while (requests_.size() > min_in_day_) {
-            // Если очередь переполнена, то убрать элемент из начала
-            if (requests_.front().isEmpty()) {
-                // И если удаляемый элемент пуст то уменьшить общее количество пустых запросов
-                empty_queries_--;
+    void RequestQueue::AddRequest(int results_num) {
+        // новый запрос - новая секунда
+        ++current_time_;
+        // удаляем все результаты поиска, которые устарели
+        while (!requests_.empty() && min_in_day_ <= current_time_ - requests_.front().timestamp) {
+            if (0 == requests_.front().results) {
+                --no_results_requests_;
             }
             requests_.pop_front();
         }
+        // сохраняем новый результат поиска
+        requests_.push_back({current_time_, results_num});
+        if (0 == results_num) {
+            ++no_results_requests_;
+        }
     }
 
+/*
+    RequestQueue::RequestQueue(const SearchServer& search_server)
+        : search_server_(search_server)
+        , no_results_requests_(0)
+        , current_time_(0) {
+    }
+
+    std::vector<Document> RequestQueue::AddFindRequest(const std::string& raw_query, DocumentStatus status) {
+        const auto result = search_server_.FindTopDocuments(raw_query, status);
+        AddRequest(result.size());
+        return result;
+    }
+    std::vector<Document> RequestQueue::AddFindRequest(const std::string& raw_query) {
+        const auto result = search_server_.FindTopDocuments(raw_query);
+        AddRequest(result.size());
+        return result;
+    }
+    int RequestQueue::GetNoResultRequests() const {
+        return no_results_requests_;
+    }
+
+ 
+    void RequestQueue::AddRequest(int results_num) {
+        // новый запрос - новая секунда
+        ++current_time_;
+        // удаляем все результаты поиска, которые устарели
+        while (!requests_.empty() && min_in_day_ <= current_time_ - requests_.front().timestamp) {
+            if (0 == requests_.front().results) {
+                --no_results_requests_;
+            }
+            requests_.pop_front();
+        }
+        // сохраняем новый результат поиска
+        requests_.push_back({current_time_, results_num});
+        if (0 == results_num) {
+            ++no_results_requests_;
+        }
+    }
+*/
