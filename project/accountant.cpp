025aed909fc832a8f493c880d9d292dd20b2afa7
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-01-19 13:46:07 +0300

    Update accountant.cpp

--- project/accountant.cpp
+++ project/accountant.cpp
@@ -14,3 +14,4 @@ template <class BuildingObject>
 double Accountant::CalcBricksNeeded(const BuildingObject& building_object) const {
     pair<double, double> sizes = building_object.GetSizes();
     return CalcSquare(sizes.first, sizes.second) * 5; 
+    }
