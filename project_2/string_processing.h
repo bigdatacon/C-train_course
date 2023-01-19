6883ad5a52e86dad3103bec2405284fdc548c7b8
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-01-19 17:25:30 +0300

    Update string_processing.h

--- project_2/string_processing.h
+++ project_2/string_processing.h
@@ -1,15 +1,14 @@
-//Вставьте сюда своё решение из урока «Очередь запросов» темы «Стек, очередь, дек».‎
+//Вставьте сюда своё решение из урока «Очередь запросов» темы «Стек, очередь, дек».
 #pragma once
 #include <vector>
 #include <string>
 #include <set>
 
-vector<string> SplitIntoWords(const std::string& text) {
-}
+std::vector<std::string> SplitIntoWords(const std::string& text) ;
 
 template <typename StringContainer>
-std::set<string> MakeUniqueNonEmptyStrings(const StringContainer& strings) {
-    std::set<string> non_empty_strings;
+std::set<std::string> MakeUniqueNonEmptyStrings(const StringContainer& strings) {
+    std::set<std::string> non_empty_strings;
     for (const std::string& str : strings) {
         if (!str.empty()) {
             non_empty_strings.insert(str);
