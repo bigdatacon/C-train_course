e8c5cdef1410a89afb779134fe52660b39e8fd3e
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-01-20 17:51:04 +0300

    Update request_queue.h

--- project_3/request_queue.h
+++ project_3/request_queue.h
@@ -6,20 +6,22 @@
 #include <iostream>
 #include "search_server.h"
 
+template <typename DocumentPredicate>
+    std::vector<Document> AddFindRequest(const SearchServer& search_server_, const std::string& raw_query,           DocumentPredicate document_predicate) {
+    const auto result = search_server_.FindTopDocuments(raw_query, document_predicate);
+    AddRequest(result.size());
+    return result;
+    }
+
 
 class RequestQueue {
 public:
     explicit RequestQueue(const SearchServer& search_server);
-
-    template <typename DocumentPredicate>
-    std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate) {
-        const auto result = search_server_.FindTopDocuments(raw_query, document_predicate);
-        AddRequest(result.size());
-        return result;
-    }
     std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentStatus status) ;
     std::vector<Document> AddFindRequest(const std::string& raw_query) ;
     int GetNoResultRequests() const ;
+
+
 private:
     struct QueryResult {
         uint64_t timestamp;
@@ -33,4 +35,5 @@ private:
  
     void AddRequest(int results_num) ;
 };
+
 
