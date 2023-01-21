0c670aeb141f4cc513aa438fade48f395c24cd7f
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-01-22 02:36:16 +0300

    Update iterim.cpp

--- iterim.cpp
+++ iterim.cpp
@@ -1,135 +1,93 @@
-#include <stack>
-#include <algorithm>
+#include <cstdlib>
 #include <iostream>
-#include <numeric>
 #include <vector>
-
-/*
-Задание 1
-Внутри контейнера queue в стандартной библиотеке находится deque. Класс queue даёт доступ только к определённым возможностям класса deque. Вам реализовывать класс queue с применением deque не нужно. Сделайте свою реализацию, используя два стека.
-Вы спросите, зачем заниматься такой ерундой. Вспомним задачу о нахождении минимума среди элементов стека без перебора. Структура стека позволяет сделать это достаточно легко. Но структура deque так не сможет. Поэтому, если для очереди понадобится поддержка поиска минимума или максимума без перебора элементов, реализация с применением стеков — самая подходящая.
-Напишите реализацию методов класса Queue, отвечающих принципу FIFO, если внутри контейнера два стека.
-Внимание, ваша очередь имеет лишь неконстантную версию метода Front. В этом задании для доступа к первому элементу понадобится переместить элементы из одного стека в другой, а в константном методе это сделать проблематично. Язык C++ в некоторых случаях разрешает константным методам изменять поля класса. Эту возможность рассмотрим в будущих уроках.
-ВЫВОД:
-5 2 4 3 1
-Заполняем очередь
-Вставленный элемент 5
-Первый элемент очереди 5
-Вставленный элемент 2
-Первый элемент очереди 5
-Вставленный элемент 4
-Первый элемент очереди 5
-Вставленный элемент 3
-Первый элемент очереди 5
-Вставленный элемент 1
-Первый элемент очереди 5
-Вынимаем элементы из очереди
-Будем вынимать элемент 5
-Будем вынимать элемент 2
-Будем вынимать элемент 4
-Будем вынимать элемент 3
-Будем вынимать элемент 1
-ПОДСКАЗКА:
-Посмотрите на вставку элемента. Сначала вариантов немного: просто складывайте элементы в первый стек. Когда накопится несколько элементов, вы хотите что-нибудь оттуда вытащить. По принципу FIFO первым должен выйти тот элемент, который сейчас находится на дне первого стека. Нужно поправить эту ситуацию. Переложите все элементы первого стека во второй.
-*/
+#include <chrono>
+#include <thread>
 
 using namespace std;
+using namespace chrono;
+// хотите немного магии? тогда используйте namespace literals
+using namespace literals;
 
-template <typename It>
-void PrintRange(It range_begin, It range_end) {
-    for (auto it = range_begin; it != range_end; ++it) {
-        cout << *it << " "s;
+/*
+Добавьте в Operate переменные, чтобы измерять продолжительность каждого действия: начального момента, конечного момента и продолжительности. Не забудьте включить <chrono>.
+*/
+
+vector<int> ReverseVector(const vector<int>& source_vector) {
+    vector<int> res;
+    for (int i : source_vector) {
+        res.insert(res.begin(), i);
     }
-    cout << endl;
+
+    return res;
 }
 
-template <typename Type>
-class Stack {
-public:
-    void Push(const Type& element) {
-        elements_.push_back(element);
-    }
-    void Pop() {
-        elements_.pop_back();
-    }
-    const Type& Peek() const {
-        return elements_.back();
-    }
-    Type& Peek() {
-        return elements_.back();
-    }
-    void Print() const {
-        PrintRange(elements_.begin(), elements_.end());
-    }
-    uint64_t Size() const {
-        return elements_.size();
-    }
-    bool IsEmpty() const {
-        return elements_.empty();
-    }
+int CountPops(const vector<int>& source_vector, int begin, int end) {
+    int res = 0;
 
-private:
-    vector<Type> elements_;
-};
-
-template <typename Type>
-class Queue {
-public:
-    void Push(const Type& element) {
-        // напишите реализацию
-        in_stack_.Push(element);   // ни так ни так не работает 
-        //in_stack_.push_back(element);
-    }
-    void Pop() {
-        // напишите реализацию
-        in_stack_.Pop();
-    }
-    Type& Front() {
-        // напишите реализацию
-        if (!out_stack_.IsEmpty()) {
-            out_stack_.Pop();
+    for (int i = begin; i < end; ++i) {
+        if (source_vector[i]) {
+            ++res;
         }
-        else {
-            for (auto el : in_stack_) { out_stack_.Push(el); in_stack_.Pop(); }  // добавляю элементы в outstack 
-        }
-        // беру последний элемент из outstack 
-        return out_stack_.Pop();
-    }
-    uint64_t Size() const {
-        // напишите реализацию
-        return in_stack_.Size();
-    }
-    bool IsEmpty() const {
-        return in_stack_.Empty();
     }
 
-private:
-    Stack<Type> in_stack_;
-    Stack<Type> out_stack_;
-};
+    return res;
+}
+
+void AppendRandom(vector<int>& v, int n) {
+    for (int i = 0; i < n; ++i) {
+        // получаем случайное число с помощью функции rand.
+        // с помощью (rand() % 2) получим целое число в диапазоне 0..1.
+        // в C++ имеются более современные генераторы случайных чисел,
+        // но в данном уроке не будем их касаться
+        v.push_back(rand() % 2);
+    }
+}
+
+
+////////////////////////
+
+
+
+/////////////////////////
+void Operate() {  
+    ///////////----------------------------------
+    vector<int> random_bits;
+
+    // операция << для целых чисел это сдвиг всех бит в двоичной
+    // записи числа. Запишем с её помощью число 2 в степени 17 (131072)
+    static const int N = 1 << 17;
+
+    // заполним вектор случайными числами 0 и 1
+    const auto start_time1 = steady_clock::now();
+    AppendRandom(random_bits, N);
+    const auto end_time1 = steady_clock::now();
+    const auto dur = end_time1 - start_time1;
+    cerr << "AppendRandom: "s << chrono::duration_cast<chrono::milliseconds>(dur).count() << " ms"s << endl;
+
+    // перевернём вектор задом наперёд
+    const auto start_time2 = steady_clock::now();
+    vector<int> reversed_bits = ReverseVector(random_bits);
+    const auto end_time2 = steady_clock::now();
+    const auto dur2 = end_time2 - start_time2;
+    cerr << "Reverse: "s << chrono::duration_cast<chrono::milliseconds>(dur2).count() << " ms"s << endl;
+
+    // посчитаем процент единиц на начальных отрезках вектора
+    const auto start_time3 = steady_clock::now();
+    for (int i = 1, step = 1; i <= N; i += step, step *= 2) {
+        // чтобы вычислить проценты, мы умножаем на литерал 100. типа double;
+        // целочисленное значение функции CountPops при этом автоматически
+        // преобразуется к double, как и i
+        double rate = CountPops(reversed_bits, 0, i) * 100. / i;
+        cout << "After "s << i << " bits we found "s << rate << "% pops"s
+             << endl;
+    }
+    const auto end_time3 = steady_clock::now();
+    const auto dur3 = end_time3 - start_time3;
+    cerr << "Counting: "s << chrono::duration_cast<chrono::milliseconds>(dur3).count() << " ms"s << endl;
+}
 
 int main() {
-    Queue<int> queue;
-    vector<int> values(5);
-    // заполняем вектор для тестирования очереди
-    iota(values.begin(), values.end(), 1);
-    // перемешиваем значения
-    random_shuffle(values.begin(), values.end());
-    PrintRange(values.begin(), values.end());
-    cout << "Заполняем очередь"s << endl;
-    // заполняем очередь и выводим элемент в начале очереди
-    for (int i = 0; i < 5; ++i) {
-        queue.Push(values[i]);
-        cout << "Вставленный элемент "s << values[i] << endl;
-        cout << "Первый элемент очереди "s << queue.Front() << endl;
-    }
-    cout << "Вынимаем элементы из очереди"s << endl;
-    // выводим элемент в начале очереди и вытаскиваем элементы по одному
-    while (!queue.IsEmpty()) {
-        // сначала будем проверять начальный элемент, а потом вытаскивать,
-        // так как операция Front на пустой очереди не определена
-        cout << "Будем вынимать элемент "s << queue.Front() << endl;
-        queue.Pop();
-    }
+    Operate();
     return 0;
 }
