77046faa0e8d334717769dddd860b8a43ac6232f
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-01-23 17:11:31 +0300

    Update and rename maincpp to main.cpp

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 using namespace std;
 
 vector<int> ReverseVector(const vector<int>& source_vector) {
+    LogDuration reverse("Reverse"s);
     vector<int> res;
     for (int i : source_vector) {
         res.insert(res.begin(), i);
@@ -17,7 +18,7 @@ vector<int> ReverseVector(const vector<int>& source_vector) {
 
 int CountPops(const vector<int>& source_vector, int begin, int end) {
     int res = 0;
-
+    
     for (int i = begin; i < end; ++i) {
         if (source_vector[i]) {
             ++res;
@@ -45,27 +46,31 @@ void Operate() {
     static const int N = 1 << 17;
 
     // заполним вектор случайными числами 0 и 1
-    const auto append_start = chrono::steady_clock::now();
+    {LogDuration reverse("Append random"s);
     AppendRandom(random_bits, N);
-    const auto append_end = chrono::steady_clock::now();
-    cerr << "Append random: "s << chrono::duration_cast<chrono::milliseconds>(append_end - append_start).count() << " ms"s << endl;
+    }
 
     // перевернём вектор задом наперёд
-    const auto rev_start = chrono::steady_clock::now();
+    //{LogDuration reverse("Reverse"s);
     vector<int> reversed_bits = ReverseVector(random_bits);
-    const auto rev_end = chrono::steady_clock::now();
-    cerr << "Reverse: "s << chrono::duration_cast<chrono::milliseconds>(rev_end - rev_start).count() << " ms"s << endl;
+    //}
+
 
     // посчитаем процент единиц на начальных отрезках вектора
-    const auto count_start = chrono::steady_clock::now();
-    for (int i = 1, step = 1; i <= N; i += step, step *= 2) {
+       {
+        LogDuration sleep_guard("Counting"s);
+               for (int i = 1, step = 1; i <= N; i += step, step *= 2) {
         double rate = CountPops(reversed_bits, 0, i) * 100. / i;
         cout << "After "s << i << " bits we found "s << rate << "% pops"s << endl;
     }
-    const auto count_end = chrono::steady_clock::now();
-    cerr << "Counting: "s << chrono::duration_cast<chrono::milliseconds>(count_end - count_start).count() << " ms"s << endl;
+
+    }
+
+
 }
 
 int main() {
+    {LogDuration sleep_guard("Total"s);
     Operate();
+    }
 }
