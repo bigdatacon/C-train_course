6697323661b1814677f891d5309021ee73759faa
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-01-26 02:16:14 +0300

    Update solvation.cpp

--- solvation.cpp
+++ solvation.cpp
@@ -1,124 +1,51 @@
-
-
 #include <algorithm>
 #include <iostream>
-#include <numeric>
-#include <sstream>
 #include <vector>
-#include <set>
+#include <random>
 
 using namespace std;
-/*
-Задание 1
-Напишите функцию GetPermutations, которая получает в качестве аргумента итератор на начало контейнера и итератор на его конец и возвращает вектор строк со всеми возможными перестановками элементов этого контейнера. Элементы перестановок разделяйте пробелами.
-Мы немного обновили для вашего удобства функцию PrintRange. Теперь это функция  PrintRangeToString, и она печатает не на экран, а в строку. Используйте в решении эту обновлённую версию.
-Порядок, в котором функция возвратит перестановки не важен.
-Пример вывода
-3 2 1
-3 1 2
-2 3 1
-2 1 3
-1 3 2
-1 2 3
-*/
 
-
-// функция, записывающая элементы диапазона в строку
-template <typename It>
-string PrintRangeToString(It range_begin, It range_end) {
-    // удобный тип ostringstream -> https://ru.cppreference.com/w/cpp/io/basic_ostringstream
-    ostringstream out;
-    for (auto it = range_begin; it != range_end; ++it) {
-        out << *it << " "s;
+// функция возвращает true, если векторы из одинаковых элементов
+// перепишите эту функцию, улучшив её асимптотическую сложность
+bool TestPermut(const vector<int>& v1, const vector<int>& v2) {
+    // если они разной длины, элементы заведомо разные
+    if (v1.size() != v2.size()) {
+        return false;
     }
-    out << endl;
-    // получаем доступ к строке с помощью метода str для ostringstream
-    return out.str();
+    vector<int> v3 = v1;
+    vector<int> v4 = v2;
+    
+    std::sort(v3.begin(), v3.end(),
+          [] (int a, int b) { return a < b; });
+    
+    std::sort(v4.begin(), v4.end(),
+          [] (int a, int b) { return a < b; });
+    
+
+    return v3 == v4 /*true*/;
 }
-
-
-string w(string s) {
-    string q = "";
-
-        for (char t : s) {
-
-            if (t != ' ') {
-                q += t;
-                q += ' ';
-            }
-            else {
-                continue;
-            }
-        }
-        //std::cout << "eto string: out: "s << q << '\n';
-    q.pop_back(); // удаляю пробел в конце 
-    q.pop_back(); // удаляю  2 пробел в конце 
-    return q;
-}
-
-string ww(string s) {
-    string q = "";
-
-        for (char t : s) {
-
-            if (t != ' ') {
-                q += t;
-            }
-            else {
-                continue;
-            }
-        }
-        //std::cout << "eto string: out: "s << q << '\n';
-    q.pop_back(); // удаляю пробел в конце 
-    return q;
-}
-
-vector<string> ToNormal(set<string> iterim_res){
-    vector<string> result;
-    for (auto e : iterim_res) { 
-    string q = ""s;
-    for (auto ch : e) {q+=ch; q+=' ';};
-    q.pop_back(); // удаляю пробел в конце
-    result.push_back(q); /*result.push_back("\n"s);*/  /*cout << "here to vector : " << e << endl;*/ }
-    //result.pop_back(); // удаляю символ переноса строкаи в конце 
-    cout <<  result.size()<< endl;
-    for (auto e : result) { cout << "here in vector : " << e << "  size : " << e.length() <<  endl; };
-    return result;
-}
-
-
-
-template <typename It>
-vector<string> GetPermutations(It range_begin, It range_end) {
-    vector<string> result;
-    set<string> iterim_res;
-    string s = PrintRangeToString(range_begin, range_end);
-    s = ww(s);
-    //s = w(s);
-    std::sort(s.begin(), s.end());
-    do {
-        iterim_res.insert(s);
-        //cout << "here s : " << s << endl;
-    } while (std::next_permutation(s.begin(), s.end()));
-    for (auto e : iterim_res) { result.push_back(e); result.push_back("\n"s); /*cout << "here to vector : " << e << endl;*/ };
-    //for (auto e : result) { cout << "here in vector : " << e << endl; };
-    vector<string> result_new = ToNormal(iterim_res) ; // добавляю пробел между цифрами в строке
-    //result.pop_back(); 
-    return /*result*/ result_new;
-
-}
-
-
 
 int main() {
-    vector<int> permutation(3);
-    // iota             -> http://ru.cppreference.com/w/cpp/algorithm/iota
-    // Заполняет диапазон последовательно возрастающими значениями
-    iota(permutation.begin(), permutation.end(), 1);
-    auto result = GetPermutations(permutation.begin(), permutation.end());
-    for (const auto& s : result) {
-        cout << s /*<< endl*/;
-    }
-    return 0;
-}
+    std::mt19937 g;
 
+    int n;
+    cin >> n;
+    vector<int> v1, v2;
+    v1.reserve(n);
+    v2.reserve(n);
+
+    for (int i = 0; i < n; ++i) {
+        v1.push_back(rand());
+        v2.push_back(rand());
+    }
+
+    // оба вектора случайны, вряд ли они совпадут
+    cout << "Random vectors match? "s << flush;
+    cout << (TestPermut(v1, v2) ? "Yes"s : "No"s) << endl;
+
+    // делаем один перестановкой другого явным образом
+    v2 = v1;
+    shuffle(v2.begin(), v2.end(), g);
+    cout << "Permuted vectors match? "s << flush;
+    cout << (TestPermut(v1, v2) ? "Yes"s : "No"s) << endl;
+}
