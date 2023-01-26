82efc2ab52746618d39d889b454781b6849a4ecd
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-01-26 20:04:31 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,54 +1,88 @@
 #include <iostream>
 #include <string>
 #include <cstdlib>
+#include <vector>
 
 using namespace std;
-/*
-Иногда экспонента всё-таки необходима. Реализуйте брутфорс для подбора пароля к секретному объекту Неверляндии. К счастью, контрразведка Неверляндии не очень продвинута — Неверландский системный администратор Артур установил пароль, содержащий всего пять латинских букв верхнего регистра A-Z. Вам не составит труда подобрать его. Функция проверки пароля будет передана в качестве аргумента.
-Ограничения
-Не меняйте сигнатуру функции BruteForce. 
-Пример
-Если пароль ABCDE, то check("ABCDE"s) == true, check(str) == false, где str — любая другая строка.
-Что отправлять на проверку
-Включите в решение реализацию функции BruteForce. Функция main при проверке будет игнорироваться.
-Как будет тестироваться ваш код
-Функция BruteForce будет вызываться с различными предикатами check. Для каждого гарантируется, что есть ровно одна строка из пяти символов A-Z, на которой он истинен.
-Вы можете выбрать один из двух подходов к решению.
-Простой. Реализуйте 5 вложенных циклов по char от 'A' до 'Z'.
-Универсальный. Годится, если длина пароля заранее неизвестна. Напишите рекурсивную функцию string BruteForceInternal(F check, const string& begin, int n). При n == 5 она будет вызывать check(begin) и возвращать результат вызова в случае успеха, пустую строку в случае неудачи. При n < 5 она будет вызывать себя, приписывая к begin всевозможные символы и увеличивая n на 1. Если рекурсивный вызов BruteForceInternal вернул непустую строку, то это и есть правильный ответ, дальнейший перебор не нужен.
-*/
+/*template <typename F>
+string BruteForceInternal(F check, string prefix, int length) {
+    // Поиск паролей длины на более length начинающихся со строки prefix
+    static string symbols("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+    if (check(prefix)) {
+        return prefix;
+    }
+    if (prefix.length() < length) {
+        for (auto symbol: symbols) {
+            string found = BruteForceInternal(check, prefix + symbol, length);
+            if (!found.empty()) {
+                return found;
+            }
+        }
+    }
+    return string();
+}
+
+
+// -- реализация через рекурсию
+template <typename F>
+string BruteForce(F check) {
+    for(int length = 1; length < 1000; length++) {
+        string found = BruteForceInternal(check, ""s, length);
+        if (!found.empty()) {
+            return found;
+        }
+    }
+    return string();
+}*/
+
+
+
+
+
 
 // -- реализация через цикл 
 template <typename F>
 string BruteForce(F check) {
-    // верните строку str, для которой check(str) будет true
-    for (char one =  'A'; one <= 'Z'; ++one){
-        for (char two =  'A'; one <= 'Z'; ++one){
-            for (char three =  'A'; one <= 'Z'; ++one){
-                for (char four =  'A'; one <= 'Z'; ++one){
-                    for (char five =  'A'; one <= 'Z'; ++one){
-                            string s{one, two, three, four, five};
-                            if (!check(s)){continue;}
-                            else {return s;}
-
+    static string symbols("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+    vector<string> one;
+    vector<string> two;
+    vector<string> three;
+    vector<string> four;
+    vector<string> five;
     
-                        
-                    }
-                    
-                }
-                
-            }
-            
-            
-        }
+    for (auto symbol: symbols) {
+        string s {symbol};
+        one.push_back(s);
+        if (check(s)){return s;}
     }
-    return ""s;
+    for (auto symbol: symbols) {
+        for (auto el : one) {string s = el + symbol; two.push_back(s); }
+    }
+    for (auto symbol: symbols) {
+        for (auto el : two) {string s = el + symbol; three.push_back(s); /*cout << " s : " << s<<  endl; */ }
+    }
+    
+    for (auto el : three) 
+    {if (check(el)) { return el;}}*/
+    
+    /*for (auto symbol: symbols) {
+        for (auto el : three) {string s = el + symbol; four.push_back(s); }
+    }
+    
+    for (auto symbol: symbols) {
+        for (auto el : four) {string s = el + symbol; five.push_back(s); }
+    }
+    
+    for (auto el : five) 
+    {if (check(el)) { return el;}}*/
+        
+    return string();
 }
 
 
 
 // -- реализация через рекурсию
-template <typename F>
+/*template <typename F>
 string BruteForce(F check) {
     // верните строку str, для которой check(str) будет true
     char one =  'A' + rand() % 26;
@@ -60,7 +94,7 @@ string BruteForce(F check) {
 
     if (!check(s)){BruteForce(check);}
     return s;
-}
+}*/
 
 
 // - тут заготовка которую я не правил 
