c1b5404204bc427dccdb9a06e4d260d914091eb3
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-01-27 16:43:10 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,21 +1,10 @@
-#include <algorithm>
-#include <cmath>
-#include <cstdint>
-#include <iostream>
-#include <random>
-#include <vector>
-#include <cmath>
-
-using namespace std;
-
-
 int EffectiveCount(const vector<int>& v, int n, int i) {
     // место для вашего решения
     //1. нахожу номер позиции числа большего i
     auto pos_if = find_if( v.begin(), v.end(),  [i](int el) {return i < el;}) - v.begin();
     //auto pos = find( v.begin(), v.end(),  i);
     //cout << pos<< end;
-    cout << pos_if<< end;
+    cout << pos_if<< endl;
     
     
     //2 определяю какой алгоритм выбрать  - для худшего или хорошего случая 
@@ -28,27 +17,4 @@ int EffectiveCount(const vector<int>& v, int n, int i) {
         return upper_bound( v.begin(), v.end(),  i) - v.begin();
     }
     ///return *pos;     
-}
-
-
-
-
-int main() {
-    static const int NUMBERS = 1'000'000;
-    static const int MAX = 1'000'000'000;
-
-    mt19937 r;
-    uniform_int_distribution<int> uniform_dist(0, MAX);
-
-    vector<int> nums;
-    for (int i = 0; i < NUMBERS; ++i) {
-        int random_number = uniform_dist(r);
-        nums.push_back(random_number);
-    }
-    sort(nums.begin(), nums.end());
-
-    int i;
-    cin >> i;
-    int result = EffectiveCount(nums, MAX, i);
-    cout << "Total numbers before "s << i << ": "s << result << endl;
 }
