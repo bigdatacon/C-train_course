ecfcf7baadfd12628cc6776a9e723d1955e84850
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-01-29 18:08:23 +0300

    Update search_server.cpp

--- project_280123/search_server.cpp
+++ project_280123/search_server.cpp
@@ -171,8 +171,8 @@
 
     //2.Разработайте метод получения частот слов по id документа: 
     //Если документа не существует, возвратите ссылку на пустой map
-    const map<string, double>& SearchServer::GetWordFrequencies(int document_id) const {
-        map<string, double> word_freqs;
+    const std::map<std::string, double>& SearchServer::GetWordFrequencies(int document_id) const {
+        std::map<std::string, double> word_freqs;
         for (auto el : word_to_document_freqs_) {
             if (el.second.first == document_id) {
                 word_freqs[el.first] += el.second.second;
@@ -193,6 +193,39 @@
         }
         documents_.erase(document_id);
         document_ids_.erase(document_id);
+        
+        
+        
+    //4. Вне класса сервера разработайте функцию поиска и удаления дубликатов :
+void RemoveDuplicates() {
+    std::map<std::string, double> m; // то что приходит из GetWordFrequencies(document_id)
+    std::vector<std::string> key_words;  // только слова документа без id 
+    //pair<int, vector<string>> document_struct; // структура слов для 1 документа по id 
+    std::vector <std::pair<int, std::vector<std::string>>>  all_document_struct; // структура слов для всех документов по id 
+
+    for (const int document_id :search_server) {
+        std::map<std::string, double> id_freq = GetWordFrequencies(document_id); // получаю частоту слов для id
+        // записываю только слова для id В вектор
+        for (std::map<std::string, double>::iterator it = id_freq.begin(); it != id_freq.end(); ++it) {
+            key_words.push_back(it->first);
+        }
+        // добавляю id : vector (слов) в итоговую пару document_struct
+        all_document_struct.push_back(std::pair(document_id, key_words);  // добавляю в вектора для каждого id его струткуру документов 
+
+    }
+    // иду по вектору all_document_struct и нахожу равные key_words
+    for (auto i = 0; i < all_document_struct.size() - 1; ++i) {
+        for (auto j = 1; j < all_document_struct.size(); ++j)
+            if (all_document_struct[i].second == all_document_struct[j].second) {
+                cout << "Found duplicate document id " << all_document_struct[j].first << endl;
+                RemoveDocument(all_document_struct[j].first);
+
+            }
+
+    }
+
+
+}
 
     };
 
