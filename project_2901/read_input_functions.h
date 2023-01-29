ee65c67d8717acb9df5d16c3c1344e690ef23b86
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-01-29 23:03:59 +0300

    Add files via upload

---
+++ project_2901/read_input_functions.h
@@ -0,0 +1,9 @@
+// в качестве заготовки кода используйте последнюю версию своей поисковой системы
+#pragma once
+#include <string>
+
+
+
+std::string ReadLine();
+
+int ReadLineWithNumber() ;
---
+++ project_2901/remove_duplicates.cpp
@@ -0,0 +1,27 @@
+// в качестве заготовки кода используйте последнюю версию своей поисковой системы
+// в качестве заготовки кода используйте последнюю версию своей поисковой системы
+#include "search_server.h"
+#include "remove_duplicates.h"
+#include "document.h"
+#include "string_processing.h"
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <set>
+#include <stdexcept>
+#include <string>
+#include <utility>
+#include <vector>
+#include <deque>
+#include <numeric>
+
+
+//4. Вне класса сервера разработайте функцию поиска и удаления дубликатов :
+void RemoveDuplicates(SearchServer& search_server) {
+    search_server.RemoveDuplicates();
+}
+
+
+
+
