9f09d5660ab09ab0a6d493171f1cd2d1e3f818c0
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-01-30 19:42:35 +0300

    Add files via upload

---
+++ project_6_after_review/remove_duplicates.h
@@ -0,0 +1,20 @@
+
+#pragma once
+#include "search_server.h"
+#include "document.h"
+#include "string_processing.h"
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <map>
+#include <set>
+#include <stdexcept>
+#include <string>
+#include <utility>
+#include <vector>
+#include <deque>
+#include <numeric>
+
+
+void RemoveDuplicates(SearchServer& search_server);
+
