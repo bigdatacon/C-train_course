8fb913941e65b2ec3e12c1283b0f7fa00714c005
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-01-31 17:58:46 +0300

    Update remove_duplicates.cpp

--- cpp-search-server-main_review_2/cpp-search-server-main/remove_duplicates.cpp
+++ cpp-search-server-main_review_2/cpp-search-server-main/remove_duplicates.cpp
@@ -14,15 +14,29 @@
 #include <numeric>
 
 
-void RemoveDuplicates(SearchServer& search_server) {
-    for (auto i = search_server.begin(); i != search_server.end(); i++) {
-        for (auto j = i + 1; j != search_server.end(); j++) {
-            if (search_server.GetId_words()[*i] == search_server.GetId_words()[*j]) {
+/*void RemoveDuplicates(SearchServer& search_server) {
+    for (auto i = search_server.begin(); i != search_server.end(); ++i) {
+        for (auto j = i + 1; j != search_server.end(); ++j) {
+            if (search_server.GetDoc_ids()[*i] == search_server.GetDoc_ids()[*j]) {
                 std::cout << "Found duplicate document id " << *j << std::endl;
                 search_server.RemoveDocument(*j);
                 j--;
             }
         }
     }
-}
+}*/
 
+void RemoveDuplicates(SearchServer& search_server) {
+    std::set<int> to_delete;
+    for (auto i: search_server) {
+        for (auto j: search_server) {
+            if (j > i && !to_delete.count(j) && search_server.GetId_words()[i] == search_server.GetId_words()[j]) {
+                std::cout << "Found duplicate document id " << j << std::endl;
+                to_delete.insert(j);
+            }
+        }
+    }
+    for (auto j: to_delete) {
+        search_server.RemoveDocument(j);
+    }
+}
