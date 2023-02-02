0b8a9cd099585ed77142fc267d6d8aa8f75b5b9a
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-02-02 09:11:47 +0300

    Update iterim.cpp

--- iterim.cpp
+++ iterim.cpp
@@ -1,93 +1,140 @@
-#include <cstdlib>
+#include <algorithm>
+#include <cassert>
 #include <iostream>
+#include <string>
 #include <vector>
-#include <chrono>
-#include <thread>
 
 using namespace std;
-using namespace chrono;
-// хотите немного магии? тогда используйте namespace literals
-using namespace literals;
 
-/*
-Добавьте в Operate переменные, чтобы измерять продолжительность каждого действия: начального момента, конечного момента и продолжительности. Не забудьте включить <chrono>.
-*/
+// Породы кошек
+enum class CatBreed {
+    Bengal,  
+    Balinese,
+    Persian,
+    Siamese,
+    Siberian,
+    Sphynx,SuccessSuccess
+};
 
-vector<int> ReverseVector(const vector<int>& source_vector) {
-    vector<int> res;
-    for (int i : source_vector) {
-        res.insert(res.begin(), i);
+// Пол
+enum class Gender {
+    Male,
+    Female,
+};
+
+struct Cat {
+    string name;
+    Gender gender;
+    CatBreed breed;
+    int age;
+};
+
+string CatBreedToString(CatBreed breed) {
+    switch (breed) {
+        case CatBreed::Bengal:
+            return "Bengal"s;
+        case CatBreed::Balinese:
+            return "Balinese"s;
+        case CatBreed::Persian:
+            return "Persian"s;
+        case CatBreed::Siamese:
+            return "Siamese"s;
+        case CatBreed::Siberian:
+            return "Siberian";
+        case CatBreed::Sphynx:
+            return "Sphynx"s;
+        default:
+            throw invalid_argument("Invalid cat breed"s);
     }
-
-    return res;
 }
 
-int CountPops(const vector<int>& source_vector, int begin, int end) {
-    int res = 0;
-
-    for (int i = begin; i < end; ++i) {
-        if (source_vector[i]) {
-            ++res;
-        }
-    }
-
-    return res;
+ostream& operator<<(ostream& out, CatBreed breed) {
+    out << CatBreedToString(breed);
+    return out;
 }
 
-void AppendRandom(vector<int>& v, int n) {
-    for (int i = 0; i < n; ++i) {
-        // получаем случайное число с помощью функции rand.
-        // с помощью (rand() % 2) получим целое число в диапазоне 0..1.
-        // в C++ имеются более современные генераторы случайных чисел,
-        // но в данном уроке не будем их касаться
-        v.push_back(rand() % 2);
-    }
+ostream& operator<<(ostream& out, Gender gender) {
+    out << (gender == Gender::Male ? "male"s : "female"s);
+    return out;
 }
 
+ostream& operator<<(ostream& out, const Cat& cat) {
+    out << '{' << cat.name << ", "s << cat.gender;
+    out << ", breed: "s << cat.breed << ", age:"s << cat.age << '}';
+    return out;
+}
 
-////////////////////////
-
-
-
-/////////////////////////
-void Operate() {  
-    ///////////----------------------------------
-    vector<int> random_bits;
-
-    // операция << для целых чисел это сдвиг всех бит в двоичной
-    // записи числа. Запишем с её помощью число 2 в степени 17 (131072)
-    static const int N = 1 << 17;
-
-    // заполним вектор случайными числами 0 и 1
-    const auto start_time1 = steady_clock::now();
-    AppendRandom(random_bits, N);
-    const auto end_time1 = steady_clock::now();
-    const auto dur = end_time1 - start_time1;
-    cerr << "AppendRandom: "s << chrono::duration_cast<chrono::milliseconds>(dur).count() << " ms"s << endl;
-
-    // перевернём вектор задом наперёд
-    const auto start_time2 = steady_clock::now();
-    vector<int> reversed_bits = ReverseVector(random_bits);
-    const auto end_time2 = steady_clock::now();
-    const auto dur2 = end_time2 - start_time2;
-    cerr << "Reverse: "s << chrono::duration_cast<chrono::milliseconds>(dur2).count() << " ms"s << endl;
-
-    // посчитаем процент единиц на начальных отрезках вектора
-    const auto start_time3 = steady_clock::now();
-    for (int i = 1, step = 1; i <= N; i += step, step *= 2) {
-        // чтобы вычислить проценты, мы умножаем на литерал 100. типа double;
-        // целочисленное значение функции CountPops при этом автоматически
-        // преобразуется к double, как и i
-        double rate = CountPops(reversed_bits, 0, i) * 100. / i;
-        cout << "After "s << i << " bits we found "s << rate << "% pops"s
-             << endl;
+// Возвращает массив указателей на элементы вектора cats, отсортированные с использованием
+// компаратора comp. Компаратор comp - функция, принимающая два аргумента типа const Cat&
+// и возвращающая true, если значения упорядочены, и false в ином случае
+template <typename Comparator>
+vector<const Cat*> GetSortedCats(const vector<Cat>& cats, const Comparator& comp) {
+    vector<const Cat*> sorted_cat_pointers;
+    /*
+    Напишите тело функции самостоятельно. Подсказка:
+    1) Поместите в массив sorted_cat_pointers адреса объектов из массива cats.
+    2) Отсортируйте массив sorted_cat_pointers с помощью переданного компаратора comp.
+       Так как comp сравнивает ссылки на объекты, а отсортировать нужно указатели,
+       передайте в sort лямбда функцию, принимающую указатели и сравнивающую объекты
+       при помощи компаратора comp:
+       [comp](const Cat* lhs, const Cat* rhs) {
+           return comp(*lhs, *rhs);
+       }
+    */
+    //1) Поместите в массив sorted_cat_pointers адреса объектов из массива cats.
+    for (const Cat& item : cats) {
+        sorted_cat_pointers.push_back(&item);
     }
-    const auto end_time3 = steady_clock::now();
-    const auto dur3 = end_time3 - start_time3;
-    cerr << "Counting: "s << chrono::duration_cast<chrono::milliseconds>(dur3).count() << " ms"s << endl;
+
+    // сортируем указатели.
+    sort(sorted_cat_pointers.begin(), sorted_cat_pointers.end(), [comp](const Cat* lhs, const Cat* rhs) {
+        // Чтобы сравнить значения, надо разыменовать указатели
+        return comp(*lhs, *rhs);
+    });
+    
+    return sorted_cat_pointers;
+}
+
+// Выводит в поток out значения объектов, на который ссылаются указатели вектора cat_pointers.
+// Пример вывода элементов vector<const Cat*>:
+// {{Tom, male, breed: Bengal, age:2}, {Charlie, male, breed: Balinese, age:7}}
+void PrintCatPointerValues(const vector<const Cat*>& cat_pointers, ostream& out) {
+    // Напишите функцию самостоятельно
+    for (const Cat* item_ptr : cat_pointers) {
+            out << *item_ptr << endl;
+    }
+
+    //return out;
 }
 
 int main() {
-    Operate();
+    const vector<Cat> cats = {
+        {"Tom"s, Gender::Male, CatBreed::Bengal, 2},
+        {"Leo"s, Gender::Male, CatBreed::Siberian, 3},
+        {"Luna"s, Gender::Female, CatBreed::Siamese, 1},
+        {"Charlie"s, Gender::Male, CatBreed::Balinese, 7},
+        {"Ginger"s, Gender::Female, CatBreed::Sphynx, 5},
+        {"Tom"s, Gender::Male, CatBreed::Siamese, 2},
+    };
+
+    {
+        auto sorted_cats = GetSortedCats(cats, [](const Cat& lhs, const Cat& rhs) {
+            return tie(lhs.breed, lhs.name) < tie(rhs.breed, rhs.name);
+        });
+
+        cout << "Cats sorted by breed and name:"s << endl;
+        PrintCatPointerValues(sorted_cats, cout);
+        cout << endl;
+    }
+
+    {
+        auto sorted_cats = GetSortedCats(cats, [](const Cat& lhs, const Cat& rhs) {
+            return tie(lhs.gender, lhs.breed) < tie(rhs.gender, rhs.breed);
+        });
+
+        cout << "Cats sorted by gender and breed:"s << endl;
+        PrintCatPointerValues(sorted_cats, cout);
+        cout << endl;
+    }
     return 0;
 }
