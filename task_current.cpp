ded7370af2f0bb367649a6012fed66aaac3e6c68
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-02-05 10:03:40 +0300

    Update task_current.cpp

--- task_current.cpp
+++ task_current.cpp
@@ -1,87 +1,235 @@
 #include <cassert>
-#include <iostream>
+#include <cstddef>
 #include <string>
-#include <vector>
+#include <utility>
 
-using namespace std;
+/*
+Реализуйте операцию PushFront, которая делает вставки элемента в начало односвязного списка, и операцию Clear, очищающую список.
+Метод PushFront должен предоставлять строгую гарантию безопасности исключений: если в процессе работы метода будет выброшено исключение, состояние списка должно быть таким же, как до вызова метода.
+Метод Clear очищает список и не должен выбрасывать исключений. Считается, что деструктор типа Type не выбрасывает исключений.
+При разрушении списка должны быть удалены все его элементы. 
 
-// Щупальце
-class Tentacle {
-public:
-    explicit Tentacle(int id)
-        : id_(id)
-    {
+Подсказка 
+Если вам сложно реализовать методы PushFront и Clear, вернитесь к теории урока. Тесты и пример использования из условия помогут реализовать их корректно.
+При разрушении списка вызовите в деструкторе метод Clear, чтобы освободить память, занимаемую элементами списка.
+В методе Clear удаляйте узлы, начиная с первого элемента, до тех пор, пока список не опустеет. Не забудьте обнулить поле size_ по окончании зачистки.
+*/
+
+template <typename Type>
+class SingleLinkedList {
+    // Узел списка
+    struct Node {
+        Node() = default;
+        Node(const Type& val, Node* next)
+            : value(val)
+            , next_node(next) {
+        }
+        Type value;
+        Node* next_node = nullptr;
+    };
+    
+public:  
+    // создаю конструктор по умолчанию 
+    //SingleLinkedList() {} - так не работа
+    Node* first;
+    Node* last;
+    SingleLinkedList() : first(nullptr), last(nullptr) {}
+
+// Возвращает количество элементов в списке за время O(1)
+    [[nodiscard]] size_t GetSize() const noexcept {
+         int size = 0;
+        Node* p = first;
+        while (p) {
+            size += 1;
+            p = p->next_node;
+        }
+        return size;
+
     }
 
-    int GetId() const {
-        return id_;
+    // Сообщает, пустой ли список за время O(1)
+    [[nodiscard]] bool IsEmpty() const noexcept {
+        // Заглушка. Реализуйте метод самостоятельно
+        return first == nullptr;  // так нашел в интернете 
     }
+    
+        // Вставляет элемент value в начало списка за время O(1)
+    void PushFront(const Type& value) {
+        // Реализуйте метод самостоятельно
+         head_.next_node = new Node(value, head_.next_node);
+        ++size_;
+    }
+
+    // Очищает список за время O(N)
+    void Clear() noexcept {
+        // Реализуйте метод самостоятельно
+        if (IsEmpty()) return;
+        while (!IsEmpty()){
+        Node* p = first;
+        first = p->next_node;
+        delete p;}
+
+        /*if (is_empty()) return;
+        if (first == last) {
+            remove_first();
+            return;
+        }
+        Node* p = first;
+        while (p->next != last) p = p->next;
+        p->next = nullptr;
+        delete last;
+        last = p;*/
+    }
+
 
 private:
-    int id_ = 0;
+    // Фиктивный узел, используется для вставки "перед первым элементом"
+    Node head_;
+    size_t size_;
 };
 
-// Осьминог
-class Octopus {
-public:
-    Octopus() {
-        Tentacle* t = nullptr;
-        try {
-            for (int i = 1; i <= 8; ++i) {
-                t = new Tentacle(i);      // Может выбросить исключение bad_alloc
-                tentacles_.push_back(t);  // Может выбросить исключение bad_alloc
+#include <cassert>
+#include <cstddef>
+#include <string>
+#include <utility>
 
-                // Обнуляем указатель на щупальце, которое уже добавили в tentacles_,
-                // чтобы не удалить его в обработчике catch повторно
-                t = nullptr;
+
+// Эта функция тестирует работу SingleLinkedList
+void Test1() {
+    // Шпион, следящий за своим удалением
+    struct DeletionSpy {
+        DeletionSpy() = default;
+        explicit DeletionSpy(int& instance_counter) noexcept
+            : instance_counter_ptr_(&instance_counter)  //
+        {
+            OnAddInstance();
+        }
+        DeletionSpy(const DeletionSpy& other) noexcept
+            : instance_counter_ptr_(other.instance_counter_ptr_)  //
+        {
+            OnAddInstance();
+        }
+        DeletionSpy& operator=(const DeletionSpy& rhs) noexcept {
+            if (this != &rhs) {
+                auto rhs_copy(rhs);
+                std::swap(instance_counter_ptr_, rhs_copy.instance_counter_ptr_);
             }
-        } catch (const bad_alloc&) {
-            // Удаляем щупальца, которые успели попасть в контейнер tentacles_
-            Cleanup();
-            // Удаляем щупальце, которое создали, но не добавили в tentacles_
-            delete t;
-            // Конструктор не смог создать осьминога с восемью щупальцами,
-            // поэтому выбрасываем исключение, чтобы сообщить вызывающему коду об ошибке
-            // throw без параметров внутри catch выполняет ПЕРЕВЫБРОС пойманного исключения
-            throw;
+            return *this;
         }
-    }
-
-    const Tentacle& GetTentacle(int index) const {
-        if (index < 0 || static_cast<size_t>(index) >= tentacles_.size()) {
-            throw out_of_range("Invalid tentacle index"s);
+        ~DeletionSpy() {
+            OnDeleteInstance();
         }
-        // Чтобы превратить указатель в ссылку, разыменовываем его
-        return *tentacles_[index];
-    }
 
-    ~Octopus() {
-        // Осьминог владеет объектами в динамической памяти (щупальца),
-        // которые должны быть удалены при его разрушении.
-        // Деструктор - лучшее место, чтобы прибраться за собой.
-        Cleanup();
-    }
-
-private:
-    void Cleanup() {
-        // Удаляем щупальца осьминога из динамической памяти
-        for (Tentacle* t : tentacles_) {
-            delete t;
+    private:
+        void OnAddInstance() noexcept {
+            if (instance_counter_ptr_) {
+                ++(*instance_counter_ptr_);
+            }
         }
-        // Очищаем массив указателей на щупальца
-        tentacles_.clear();
+        void OnDeleteInstance() noexcept {
+            if (instance_counter_ptr_) {
+                assert(*instance_counter_ptr_ != 0);
+                --(*instance_counter_ptr_);
+            }
+        }
+
+        int* instance_counter_ptr_ = nullptr;
+    };
+
+    // Проверка вставки в начало
+    {
+        SingleLinkedList<int> l;
+        assert(l.IsEmpty());
+        assert(l.GetSize() == 0u);
+
+        l.PushFront(0);
+        l.PushFront(1);
+        assert(l.GetSize() == 2);
+        assert(!l.IsEmpty());
+
+        l.Clear();
+        assert(l.GetSize() == 0);
+        assert(l.IsEmpty());
     }
 
-    // Вектор хранит указатели на щупальца. Сами объекты щупалец находятся в куче
-    vector<Tentacle*> tentacles_;
-};
+    // Проверка фактического удаления элементов
+    {
+        int item0_counter = 0;
+        int item1_counter = 0;
+        int item2_counter = 0;
+        {
+            SingleLinkedList<DeletionSpy> list;
+            list.PushFront(DeletionSpy{item0_counter});
+            list.PushFront(DeletionSpy{item1_counter});
+            list.PushFront(DeletionSpy{item2_counter});
 
-// Напишите функцию main, в ней создайте осьминога и несколько его копий
+            assert(item0_counter == 1);
+            assert(item1_counter == 1);
+            assert(item2_counter == 1);
+            list.Clear();
+            assert(item0_counter == 0);
+            assert(item1_counter == 0);
+            assert(item2_counter == 0);
+
+            list.PushFront(DeletionSpy{item0_counter});
+            list.PushFront(DeletionSpy{item1_counter});
+            list.PushFront(DeletionSpy{item2_counter});
+            assert(item0_counter == 1);
+            assert(item1_counter == 1);
+            assert(item2_counter == 1);
+        }
+        assert(item0_counter == 0);
+        assert(item1_counter == 0);
+        assert(item2_counter == 0);
+    }
+
+    // Вспомогательный класс, бросающий исключение после создания N-копии
+    struct ThrowOnCopy {
+        ThrowOnCopy() = default;
+        explicit ThrowOnCopy(int& copy_counter) noexcept
+            : countdown_ptr(&copy_counter) {
+        }
+        ThrowOnCopy(const ThrowOnCopy& other)
+            : countdown_ptr(other.countdown_ptr)  //
+        {
+            if (countdown_ptr) {
+                if (*countdown_ptr == 0) {
+                    throw std::bad_alloc();
+                } else {
+                    --(*countdown_ptr);
+                }
+            }
+        }
+        // Присваивание элементов этого типа не требуется
+        ThrowOnCopy& operator=(const ThrowOnCopy& rhs) = delete;
+        // Адрес счётчика обратного отсчёта. Если не равен nullptr, то уменьшается при каждом копировании.
+        // Как только обнулится, конструктор копирования выбросит исключение
+        int* countdown_ptr = nullptr;
+    };
+
+    {
+        bool exception_was_thrown = false;
+        // Последовательно уменьшаем счётчик копирований до нуля, пока не будет выброшено исключение
+        for (int max_copy_counter = 5; max_copy_counter >= 0; --max_copy_counter) {
+            // Создаём непустой список
+            SingleLinkedList<ThrowOnCopy> list;
+            list.PushFront(ThrowOnCopy{});
+            try {
+                int copy_counter = max_copy_counter;
+                list.PushFront(ThrowOnCopy(copy_counter));
+                // Если метод не выбросил исключение, список должен перейти в новое состояние
+                assert(list.GetSize() == 2);
+            } catch (const std::bad_alloc&) {
+                exception_was_thrown = true;
+                // После выбрасывания исключения состояние списка должно остаться прежним
+                assert(list.GetSize() == 1);
+                break;
+            }
+        }
+        assert(exception_was_thrown);
+    }
+}
+
 int main() {
-    // Оригинальный осьминог
-    Octopus octopus;
-    // Копия первого осьминога
-    Octopus octopus1 = octopus;
-    cout << "Congratulations. Everything is OK!"s << endl;
-    cout << octopus1.GetTentacle(1) << endl;
+    Test1();
 }
