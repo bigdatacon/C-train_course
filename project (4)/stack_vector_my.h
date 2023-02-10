b34fc1f82835f0730df4865f1ecbf0c1c713d795
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-02-10 21:58:14 +0300

    Update stack_vector_my.h

--- project (4)/stack_vector_my.h
+++ project (4)/stack_vector_my.h
@@ -10,44 +10,52 @@ class StackVector {
 public:
     //Конструктор принимает размер вектора — по аналогии со стандартным вектором. Если аргумент конструктора больше вместимости вектора, конструктор должен выбрасывать исключение invalid_argument
     explicit StackVector(size_t a_size = 0) {
-      if (a_size > Capacity()){
-          throw invalid_argument("invalid_argument");}
-      else {
-           //array<T, a_size>  array_;
-          T array_[a_size] = {};   // вот так нашел в интернете 
-       }
-       
-        
-    } ;
+        if (a_size > N) {
+            throw invalid_argument("invalid_argument");
+        }
+        array_size_ = a_size;
+    }
 
-    T& operator[](size_t index){
+    T& operator[](size_t index)
+    {
         return array_[index];
-    };
-    const T& operator[](size_t index) const{
-        return array_[index];} 
+    }
+    const T& operator[](size_t index) const
+    {
+        return array_[index];
+    }
+    
+    typename array<T, N>::iterator begin(){return array_.begin();}
+    typename array<T, N>::iterator end(){return array_.end();}
+    typename array<T, N>::const_iterator begin() const {return array_.cbegin();}
+    typename array<T, N>::const_iterator end() const {return array_.cend();}
 
-    /*std::array<T, size>::iterator*/ auto  begin(){return array_.begin();}
-    /*std::array<T, size>::iterator*/ auto end(){return array_.end();}
-    /*std::array<T, size>::iterator*/ auto begin() const {return array_.cbegin();}
-    /*std::array<T, size>::iterator*/ auto end() const {return array_.cend();}
 
     size_t Size() const {
-        return array_.size();
-    };
+        return array_size_;
+    }
     size_t Capacity() const {
-        return array_.capacity();
-    };
+        return N;
+    }
 
     void PushBack(const T& value){
-        
-        
-    };
-    T PopBack();
-        auto el_to_del = *end();
-        delete array_[el_to_del]
-    
+        if (array_size_ >= N) {
+            throw overflow_error("push to a full stack");
+        }
+        array_[array_size_] = value;
+        array_size_++;
+    }
+
+    T PopBack() {
+        if (!array_size_) {
+            throw underflow_error("pop from an empty stack");
+        }
+        T value = array_[array_size_ - 1];
+        array_size_--;
+        return value;
+    }
+
 private:
     size_t array_size_;
-    array<T, size_t>  array_; // vec.capacity()
-    
+    std::array<T, N>  array_; // vec.capacity()
 };
