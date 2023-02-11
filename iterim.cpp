c9cab717a4418373f6c4ed9f0f031a44347db337
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-02-11 22:41:40 +0300

    Update iterim.cpp

--- iterim.cpp
+++ iterim.cpp
@@ -1,68 +1,158 @@
+#pragma once
+
 #include <cassert>
-#include <iostream>
-#include <string_view>
-#include <vector>
-#include <algorithm>
-#include <map>
-
-
-using namespace std;
+#include <initializer_list>
+#include "array_ptr.h"
 
 /*
-Самый простой способ реализации словаря — это, конечно, контейнер-словарь. В данном случае удобно держать два контейнера, чтобы обеспечить поиск слов и для прямого перевода, и для обратного. Хранить в этих словарях строки — расточительство. Но строки вам всё равно нужны, так как строки могут быть уничтожены до уничтожения объекта Translator. Сохраните их себе в удобном контейнере.
+В методе Resize отдельно обработайте три ситуации: новый размер меньше или равен текущему, новый размер не превышает его вместимости, новый размер превышает текущую вместимость вектора.
+Если при изменении размера массива новый размер вектора превышает его текущую вместимость, создайте новый массив с нужной вместимостью, скопируйте в него прежнее содержимое и заполните остальные элементы значением по умолчанию. Затем старый массив можно удалить и использовать копию. После этого не забудьте обновить размер и вместимость вектора.
+Если при увеличении размера массива новый размер вектора не превышает его вместимость, заполните добавленные элементы значением по умолчанию для типа Type.
+При уменьшении размера вектора просто уменьшите его размер.
+Примените алгоритмы std::copy и std::fill для копирования элементов массива и заполнения их некоторым значением.
+В методе Clear достаточно обнулить размер массива.
 */
 
-
-class Translator {
+template <typename Type>
+class SimpleVector {
 public:
-    Translator() {} // констуруктор по умолчани.
-    
-    void Add(string_view source, string_view target){
-            forward_.emplace(std::make_pair(source, target) );
-            backward_.emplace(std::make_pair(target, source) );
-    }
-    
-    string_view TranslateForward(string_view source) const {
-        string_view res{};
-        //map::const_iterator pos = forward_.find("source");
-        auto pos = forward_.find(source);
-        if (pos != forward_.end()) {
-           return pos->second;
-            }
-        else {return res;}
-    }
-    
-    string_view TranslateBackward(string_view target) const {
-        string_view res{};
-        //map::const_iterator pos = backward_.find(target);
-        auto pos = backward_.find(target);
-        if (pos != backward_.end()) {
-           return pos->second;
-            }
-        else {return res;}
-    }
-        
+    using Iterator = Type*;
+    using ConstIterator = const Type*;
 
+    SimpleVector() noexcept = default;
+
+    // Создаёт вектор из size элементов, инициализированных значением по умолчанию
+    explicit SimpleVector(size_t size):
+        array_ptr_(size) {
+        size_ = size;
+        capacity_ = size;
+    }
+
+    // Создаёт вектор из size элементов, инициализированных значением value
+    SimpleVector(size_t size, const Type& value):
+        array_ptr_(size) {
+        size_ = size;
+        capacity_ = size;
+        for (size_t i = 0; i < size; i++) {
+            array_ptr_[i] = value;
+        }
+    }
+
+    // Создаёт вектор из std::initializer_list
+    SimpleVector(std::initializer_list<Type> init) {
+        // Напишите тело конструктора самостоятельно
+        //https://runebook.dev/en/docs/cpp/utility/initializer_list
+        array_ptr_(init); 
+        // если не сработает то так:
+        //array_ptr_.insert(array_ptr_.end(), init.begin(), init.end());
+    }
+
+    // Возвращает количество элементов в массиве
+    size_t GetSize() const noexcept {
+        return size_;
+    }
+
+    // Возвращает вместимость массива
+    size_t GetCapacity() const noexcept {
+        return capacity_;
+    }
+
+    // Сообщает, пустой ли массив
+    bool IsEmpty() const noexcept {
+        return size_ == 0;
+    }
+
+    // Возвращает ссылку на элемент с индексом index
+    Type& operator[](size_t index) noexcept {
+        return array_ptr_[index];
+    }
+
+    // Возвращает константную ссылку на элемент с индексом index
+    const Type& operator[](size_t index) const noexcept {
+        return array_ptr_[index];
+    }
+
+    // Возвращает константную ссылку на элемент с индексом index
+    // Выбрасывает исключение std::out_of_range, если index >= size
+    Type& At(size_t index) {
+        if (index >= size_) {
+            throw std::out_of_range("index out of range");
+        }
+        return array_ptr_[index];
+    }
+
+    // Возвращает константную ссылку на элемент с индексом index
+    // Выбрасывает исключение std::out_of_range, если index >= size
+    const Type& At(size_t index) const {
+        // Напишите тело самостоятельно
+        if (index >= size_) {
+            throw std::out_of_range("index out of range");
+        }
+        return array_ptr_[index];
+    }
+
+    // Обнуляет размер массива, не изменяя его вместимость
+    void Clear() noexcept {
+        // Напишите тело самостоятельно
+        Resize(0);
+    }
+
+    // Изменяет размер массива.
+    // При увеличении размера новые элементы получают значение по умолчанию для типа Type
+    void Resize(size_t new_size) {
+        // Напишите тело самостоятельно
+        if (new_size <= size_){size = new_size; return;}
+        if (new_size >  size_ && new_size <=capacity_) {
+            
+        
+        
+        } 
+    }
+
+    // Возвращает итератор на начало массива
+    // Для пустого массива может быть равен (или не равен) nullptr
+    Iterator begin() noexcept {
+        // Напишите тело самостоятельно
+        return array_ptr_.begin();
+    }
+
+    // Возвращает итератор на элемент, следующий за последним
+    // Для пустого массива может быть равен (или не равен) nullptr
+    Iterator end() noexcept {
+        // Напишите тело самостоятельно
+        return array_ptr_.end();
+    }
+
+    // Возвращает константный итератор на начало массива
+    // Для пустого массива может быть равен (или не равен) nullptr
+    ConstIterator begin() const noexcept {
+        // Напишите тело самостоятельно
+        return array_ptr_.cbegin();
+    }
+
+    // Возвращает итератор на элемент, следующий за последним
+    // Для пустого массива может быть равен (или не равен) nullptr
+    ConstIterator end() const noexcept {
+        // Напишите тело самостоятельно
+        return array_ptr_.cend();
+    }
+
+    // Возвращает константный итератор на начало массива
+    // Для пустого массива может быть равен (или не равен) nullptr
+    ConstIterator cbegin() const noexcept {
+        // Напишите тело самостоятельно
+        return array_ptr_.cbegin();
+    }
+
+    // Возвращает итератор на элемент, следующий за последним
+    // Для пустого массива может быть равен (или не равен) nullptr
+    ConstIterator cend() const noexcept {
+        // Напишите тело самостоятельно
+        return array_ptr_.cend();
+    }
 
 private:
-    // ...
-    map<string_view, string_view> forward_;
-    map<string_view, string_view> backward_;
+    ArrayPtr<Type> array_ptr_;
+    size_t size_ = 0;
+    size_t capacity_ = 0;
 };
-
-
-
-void TestSimple() {
-    Translator translator;
-    translator.Add(string("okno"s), string("window"s));
-    translator.Add(string("stol"s), string("table"s));
-
-    assert(translator.TranslateForward("okno"s) == "window"s);
-    assert(translator.TranslateBackward("table"s) == "stol"s);
-    assert(translator.TranslateForward("table"s) == ""s);
-}
-
-int main() {
-    TestSimple();
-    return 0;
-}
