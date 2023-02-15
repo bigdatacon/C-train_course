9c3e1ef6de129a8af1bf19adf87cc0ec17f9bdef
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-02-15 11:26:26 +0300

    Update firstcode.cpp

--- firstcode.cpp
+++ firstcode.cpp
@@ -1,127 +1,45 @@
-#pragma once
+#include <algorithm>
+#include <iostream>
+#include <numeric>
+#include <vector>
+#include <iterator>
 
-#include <cassert>
-#include <initializer_list>
-#include "array_ptr.h"
+using namespace std;
+
+int main() {
+    std::vector<int> first = { 7, 5, 16, 8 };
+    std::vector<int> sec = { 8, 6, 17, 9,10,11,12 };
+    std::vector<int> for_coopy{1};
+
+    // Проверяю копирование
+    std::copy(first.begin(), first.end() , std::back_inserter(for_coopy)  /*for_coopy.begin()*/ );
+    for (auto el : for_coopy) { cout << " " << el << ' '; }
+
+    auto pos = sec.begin() + 5;
+    std::copy_backward(first.begin(), first.end(), pos);
+
+    std::cout << "destination contains: ";
+    for (auto i : sec)
+        std::cout << i << ' ';
+    std::cout << '\n';
 
 
-/*
-В методе Resize отдельно обработайте три ситуации: новый размер меньше или равен текущему, новый размер не превышает его вместимости, новый размер превышает текущую вместимость вектора.
-Если при изменении размера массива новый размер вектора превышает его текущую вместимость, создайте новый массив с нужной вместимостью, скопируйте в него прежнее содержимое и заполните остальные элементы значением по умолчанию. Затем старый массив можно удалить и использовать копию. После этого не забудьте обновить размер и вместимость вектора.
-Если при увеличении размера массива новый размер вектора не превышает его вместимость, заполните добавленные элементы значением по умолчанию для типа Type.
-При уменьшении размера вектора просто уменьшите его размер.
-Примените алгоритмы std::copy и std::fill для копирования элементов массива и заполнения их некоторым значением.
-В методе Clear достаточно обнулить размер массива.
-*/
+    sec.resize(sec.size() + 1);
+    std::cout << "destination 2  contains: ";
+    for (auto i : sec)
+        std::cout << i << ' ';
+    std::cout << '\n';
+
+
+    // Пробую скопировать полный фрагмент 
+    auto post = sec.begin() + 5;
+    std::copy_backward(post-2, post+3, sec.end());
+
+    std::cout << "destination 3  contains: ";
+    for (auto i : sec)
+        std::cout << i << ' ';
+    std::cout << '\n';
 
 
 
-template <typename Type>
-class SimpleVector {
-public:
-    using Iterator = Type*;
-    using ConstIterator = const Type*;
-
-    SimpleVector() noexcept = default;
-
-    // Создаёт вектор из size элементов, инициализированных значением по умолчанию
-    explicit SimpleVector(size_t size) {
-        // Напишите тело конструктора самостоятельно
-    }
-
-    // Создаёт вектор из size элементов, инициализированных значением value
-    SimpleVector(size_t size, const Type& value) {
-        // Напишите тело конструктора самостоятельно
-    }
-
-    // Создаёт вектор из std::initializer_list
-    SimpleVector(std::initializer_list<Type> init) {
-        // Напишите тело конструктора самостоятельно
-    }
-
-    // Возвращает количество элементов в массиве
-    size_t GetSize() const noexcept {
-        // Напишите тело самостоятельно
-        return 0;
-    }
-
-    // Возвращает вместимость массива
-    size_t GetCapacity() const noexcept {
-        // Напишите тело самостоятельно
-        return 0;
-    }
-
-    // Сообщает, пустой ли массив
-    bool IsEmpty() const noexcept {
-        return true;
-    }
-
-    // Возвращает ссылку на элемент с индексом index
-    Type& operator[](size_t index) noexcept {
-        // Напишите тело самостоятельно
-    }
-
-    // Возвращает константную ссылку на элемент с индексом index
-    const Type& operator[](size_t index) const noexcept {
-        // Напишите тело самостоятельно
-    }
-
-    // Возвращает константную ссылку на элемент с индексом index
-    // Выбрасывает исключение std::out_of_range, если index >= size
-    Type& At(size_t index) {
-        // Напишите тело самостоятельно
-    }
-
-    // Возвращает константную ссылку на элемент с индексом index
-    // Выбрасывает исключение std::out_of_range, если index >= size
-    const Type& At(size_t index) const {
-        // Напишите тело самостоятельно
-    }
-
-    // Обнуляет размер массива, не изменяя его вместимость
-    void Clear() noexcept {
-        // Напишите тело самостоятельно
-    }
-
-    // Изменяет размер массива.
-    // При увеличении размера новые элементы получают значение по умолчанию для типа Type
-    void Resize(size_t new_size) {
-        // Напишите тело самостоятельно
-    }
-
-    // Возвращает итератор на начало массива
-    // Для пустого массива может быть равен (или не равен) nullptr
-    Iterator begin() noexcept {
-        // Напишите тело самостоятельно
-    }
-
-    // Возвращает итератор на элемент, следующий за последним
-    // Для пустого массива может быть равен (или не равен) nullptr
-    Iterator end() noexcept {
-        // Напишите тело самостоятельно
-    }
-
-    // Возвращает константный итератор на начало массива
-    // Для пустого массива может быть равен (или не равен) nullptr
-    ConstIterator begin() const noexcept {
-        // Напишите тело самостоятельно
-    }
-
-    // Возвращает итератор на элемент, следующий за последним
-    // Для пустого массива может быть равен (или не равен) nullptr
-    ConstIterator end() const noexcept {
-        // Напишите тело самостоятельно
-    }
-
-    // Возвращает константный итератор на начало массива
-    // Для пустого массива может быть равен (или не равен) nullptr
-    ConstIterator cbegin() const noexcept {
-        // Напишите тело самостоятельно
-    }
-
-    // Возвращает итератор на элемент, следующий за последним
-    // Для пустого массива может быть равен (или не равен) nullptr
-    ConstIterator cend() const noexcept {
-        // Напишите тело самостоятельно
-    }
-};
+}
