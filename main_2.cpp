fc49bf2a236be3f36f53da4eded6808acc64ca5f
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-02-18 22:23:21 +0300

    Update main_2.cpp

--- main_2.cpp
+++ main_2.cpp
@@ -1,111 +1,102 @@
+#include <algorithm>
 #include <cassert>
-#include <iostream>
-#include <numeric>
+#include <string>
 #include <vector>
 
 using namespace std;
 
-template <typename RandomIt>
-void MakeJosephusPermutation(RandomIt first, RandomIt last, uint32_t step_size) {
-    vector<typename RandomIt::value_type> pool(first, last);
-    size_t cur_pos = 0;
-    while (!pool.empty()) {
-        
-        *(first++) = std::move(pool[cur_pos]) ;
+/*
+ПОдсказка
+Среди стандартных алгоритмов есть удобная функция adjacent_find. Она позволяет искать последовательность из двух элементов, удовлетворяющих условию, которое задано пользователем. Вы можете применить эту функцию, чтобы определить конец предложения.
+Найдите конец предложения, потом пройдите по лексемам от начала до конца и переместите значения лексем в предложение. Само предложение после этого можно переместить в вектор результатов.
 
-        /*(first++) = pool[cur_pos];
-        pool.erase(pool.begin() + cur_pos);*/
-        if (pool.empty()) {
-            break;
-        }
-        cur_pos = (cur_pos + step_size - 1) % pool.size();
-    }
+Задание
+Дан вектор лексем. Напишите функцию, разбивающую лексемы на предложения:
+template <typename Token>
+vector<Sentence<Token>> SplitIntoSentences(vector<Token> tokens); 
+Token — шаблонный тип. О нём известно лишь, что он имеет константный метод IsEndSentencePunctuation. Этот метод возвращает true, если лексема — знак пунктуации, заканчивающий предложение. В противном случае он возвращает false. Объекты такого типа могут быть копируемыми, а могут и не быть. Реализуйте функцию так, чтобы при передаче некопируемых объектов не возникало ошибок компиляции.
+Sentence — синоним для типа vector, объявленный следующим образом:
+// Объявляем Sentence<Token> для произвольного типа Token
+// синонимом vector<Token>.
+// Благодаря этому в качестве возвращаемого значения
+// функции можно указать не малопонятный вектор векторов,
+// а вектор предложений — vector<Sentence<Token>>.
+template <typename Token>
+using Sentence = vector<Token>; 
+Предложением считается последовательность лексем, которая заканчивается одной или несколькими идущими подряд лексемами — знаками пунктуации в конце предложения. Иными словами, любое предложение должно состоять из двух частей:
+лексемы, для которых IsEndSentencePunctuation возвращает false. Такие лексемы должны быть в каждом предложении кроме, возможно, первого;
+лексемы, для которых IsEndSentencePunctuation возвращает true. Такие лексемы должны быть в каждом предложении кроме, возможно, последнего.
+Сохраните себе решение этой задачи, оно вам ещё понадобится.
+Ограничения
+Максимальное количество лексем — 10^6. Время выполнения одного вызова функции ограничено одной секундой.
+Пример использования
+Допустим, вектор лексем задан так:
+{Красиво}{ты}{вошла}{в}{мою}{грешную}{жизнь}{!}{Красиво}{ты}{ушла}{из}{неё}{.} 
+При этом ! и . — символы конца предложения, и функция IsEndSentencePunctuation возвращает true. Значит, результирующий вектор предложений будет состоять из двух элементов:
+{{Красиво}{ты}{вошла}{в}{мою}{грешную}{жизнь}{!}}
+{{Красиво}{ты}{ушла}{из}{неё}{.}} 
+В прекоде уже есть тест, где используется копируемый тип TestToken. Напишите тест для класса, объекты которого нельзя копировать, и проверьте, что ваша функция работает корректно.
+*/
+
+
+
+// Объявляем Sentence<Token> для произвольного типа Token
+// синонимом vector<Token>.
+// Благодаря этому в качестве возвращаемого значения
+// функции можно указать не малопонятный вектор векторов,
+// а вектор предложений — vector<Sentence<Token>>.
+template <typename Token>
+using Sentence = vector<Token>;
+
+template <typename TokenForwardIt>
+TokenForwardIt FindSentenceEnd(TokenForwardIt tokens_begin, TokenForwardIt tokens_end) {
+    const TokenForwardIt before_sentence_end
+        = adjacent_find(tokens_begin, tokens_end, [](const auto& left_token, const auto& right_token) {
+              return left_token.IsEndSentencePunctuation() && !right_token.IsEndSentencePunctuation();
+          });
+    return before_sentence_end == tokens_end ? tokens_end : next(before_sentence_end);
 }
 
-vector<int> MakeTestVector() {
-    vector<int> numbers(10);
-    iota(begin(numbers), end(numbers), 0);
-    return numbers;
+// Класс Token имеет метод bool IsEndSentencePunctuation() const
+template <typename Token>
+vector<Sentence<Token>> SplitIntoSentences(vector<Token> tokens) {
+    // Напишите реализацию функции, не копируя объекты типа Token
 }
 
-void TestIntVector() {
-    const vector<int> numbers = MakeTestVector();
-    {
-        vector<int> numbers_copy = numbers;
-        MakeJosephusPermutation(begin(numbers_copy), end(numbers_copy), 1);
-        assert(numbers_copy == numbers);
+struct TestToken {
+    string data;
+    bool is_end_sentence_punctuation = false;
+
+    bool IsEndSentencePunctuation() const {
+        return is_end_sentence_punctuation;
     }
-    {
-        vector<int> numbers_copy = numbers;
-        MakeJosephusPermutation(begin(numbers_copy), end(numbers_copy), 3);
-        assert(numbers_copy == vector<int>({0, 3, 6, 9, 4, 8, 5, 2, 7, 1}));
+    bool operator==(const TestToken& other) const {
+        return data == other.data && is_end_sentence_punctuation == other.is_end_sentence_punctuation;
     }
-}
-
-// Это специальный тип, который поможет вам убедиться, что ваша реализация
-// функции MakeJosephusPermutation не выполняет копирование объектов.
-// Сейчас вы, возможно, не понимаете как он устроен, однако мы расскажем
-// об этом далее в нашем курсе
-
-struct NoncopyableInt {
-    int value;
-
-    NoncopyableInt(const NoncopyableInt&) = delete;
-    NoncopyableInt& operator=(const NoncopyableInt&) = delete;
-
-    NoncopyableInt(NoncopyableInt&&) = default;
-    NoncopyableInt& operator=(NoncopyableInt&&) = default;
 };
 
-bool operator==(const NoncopyableInt& lhs, const NoncopyableInt& rhs) {
-    return lhs.value == rhs.value;
+ostream& operator<<(ostream& stream, const TestToken& token) {
+    return stream << token.data;
 }
 
-ostream& operator<<(ostream& os, const NoncopyableInt& v) {
-    return os << v.value;
+// Тест содержит копирования объектов класса TestToken.
+// Для проверки отсутствия копирований в функции SplitIntoSentences
+// необходимо написать отдельный тест.
+void TestSplitting() {
+    assert(SplitIntoSentences(vector<TestToken>({{"Split"s}, {"into"s}, {"sentences"s}, {"!"s}}))
+           == vector<Sentence<TestToken>>({{{"Split"s}, {"into"s}, {"sentences"s}, {"!"s}}}));
+
+    assert(SplitIntoSentences(vector<TestToken>({{"Split"s}, {"into"s}, {"sentences"s}, {"!"s, true}}))
+           == vector<Sentence<TestToken>>({{{"Split"s}, {"into"s}, {"sentences"s}, {"!"s, true}}}));
+
+    assert(SplitIntoSentences(vector<TestToken>(
+               {{"Split"s}, {"into"s}, {"sentences"s}, {"!"s, true}, {"!"s, true}, {"Without"s}, {"copies"s}, {"."s, true}}))
+           == vector<Sentence<TestToken>>({
+               {{"Split"s}, {"into"s}, {"sentences"s}, {"!"s, true}, {"!"s, true}},
+               {{"Without"s}, {"copies"s}, {"."s, true}},
+           }));
 }
-
-void TestAvoidsCopying() {
-    vector<int> numbers;
-    numbers.push_back({1});
-    numbers.push_back({2});
-    numbers.push_back({3});
-    numbers.push_back({4});
-    numbers.push_back({5});
-
-    MakeJosephusPermutation(begin(numbers), end(numbers), 2);
-
-    vector<int> expected;
-    expected.push_back({1});
-    expected.push_back({3});
-    expected.push_back({5});
-    expected.push_back({4});
-    expected.push_back({2});
-
-    assert(numbers == expected);
-}
-
-/*void TestAvoidsCopying() {
-    vector<NoncopyableInt> numbers;
-    numbers.push_back({1});
-    numbers.push_back({2});
-    numbers.push_back({3});
-    numbers.push_back({4});
-    numbers.push_back({5});
-
-    MakeJosephusPermutation(begin(numbers), end(numbers), 2);
-
-    vector<NoncopyableInt> expected;
-    expected.push_back({1});
-    expected.push_back({3});
-    expected.push_back({5});
-    expected.push_back({4});
-    expected.push_back({2});
-
-    assert(numbers == expected);
-}*/
 
 int main() {
-    TestIntVector();
-    TestAvoidsCopying();
+    TestSplitting();
 }
