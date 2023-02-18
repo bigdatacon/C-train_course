da82a2c49f2fa6a052c484f1ac271d68e415ef3a
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-02-18 23:40:32 +0300

    Update main_2.cpp

--- main_2.cpp
+++ main_2.cpp
@@ -58,11 +58,96 @@ TokenForwardIt FindSentenceEnd(TokenForwardIt tokens_begin, TokenForwardIt token
 }
 
 // Класс Token имеет метод bool IsEndSentencePunctuation() const
+
+#include <algorithm>
+#include <cassert>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+/*
+ПОдсказка
+Среди стандартных алгоритмов есть удобная функция adjacent_find. Она позволяет искать последовательность из двух элементов, удовлетворяющих условию, которое задано пользователем. Вы можете применить эту функцию, чтобы определить конец предложения.
+Найдите конец предложения, потом пройдите по лексемам от начала до конца и переместите значения лексем в предложение. Само предложение после этого можно переместить в вектор результатов.
+*/
+
+// Объявляем Sentence<Token> для произвольного типа Token
+// синонимом vector<Token>.
+// Благодаря этому в качестве возвращаемого значения
+// функции можно указать не малопонятный вектор векторов,
+// а вектор предложений — vector<Sentence<Token>>.
+template <typename Token>
+using Sentence = vector<Token>;
+
+template <typename TokenForwardIt>
+TokenForwardIt FindSentenceEnd(TokenForwardIt tokens_begin, TokenForwardIt tokens_end) {
+    const TokenForwardIt before_sentence_end
+        = adjacent_find(tokens_begin, tokens_end, [](const auto& left_token, const auto& right_token) {
+              return left_token.IsEndSentencePunctuation() && !right_token.IsEndSentencePunctuation();
+          });
+    return before_sentence_end == tokens_end ? tokens_end : next(before_sentence_end);
+}
+
+// Класс Token имеет метод bool IsEndSentencePunctuation() const
 template <typename Token>
 vector<Sentence<Token>> SplitIntoSentences(vector<Token> tokens) {
     // Напишите реализацию функции, не копируя объекты типа Token
+    //Найдите конец предложения, потом пройдите по лексемам от начала до конца и переместите значения лексем в предложение. Само предложение после этого можно переместить в вектор результатов.
+    vector<Sentence<Token>> res; 
+    Sentence<Token> sent_res;
+    for (Token  Tok: tokens){
+    if (!Tok.IsEndSentencePunctuation()){
+        sent_res.push_back(Tok);
+    }
+    else {
+        sent_res.push_back(Tok);
+        res.push_back(sent_res);
+        sent_res.clear(); 
+    }
+    
+    } 
+   
+    return res;
 }
 
+struct TestToken { 
+    string data;
+    bool is_end_sentence_punctuation = false;
+
+    bool IsEndSentencePunctuation() const {
+        return is_end_sentence_punctuation;
+    }
+    bool operator==(const TestToken& other) const {
+        return data == other.data && is_end_sentence_punctuation == other.is_end_sentence_punctuation;
+    }
+};
+
+ostream& operator<<(ostream& stream, const TestToken& token) {
+    return stream << token.data;
+}
+
+// Тест содержит копирования объектов класса TestToken.
+// Для проверки отсутствия копирований в функции SplitIntoSentences
+// необходимо написать отдельный тест.
+void TestSplitting() {
+    assert(SplitIntoSentences(vector<TestToken>({{"Split"s}, {"into"s}, {"sentences"s}, {"!"s}}))
+           == vector<Sentence<TestToken>>({{{"Split"s}, {"into"s}, {"sentences"s}, {"!"s}}}));
+
+    assert(SplitIntoSentences(vector<TestToken>({{"Split"s}, {"into"s}, {"sentences"s}, {"!"s, true}}))
+           == vector<Sentence<TestToken>>({{{"Split"s}, {"into"s}, {"sentences"s}, {"!"s, true}}}));
+
+    assert(SplitIntoSentences(vector<TestToken>(
+               {{"Split"s}, {"into"s}, {"sentences"s}, {"!"s, true}, {"!"s, true}, {"Without"s}, {"copies"s}, {"."s, true}}))
+           == vector<Sentence<TestToken>>({
+               {{"Split"s}, {"into"s}, {"sentences"s}, {"!"s, true}, {"!"s, true}},
+               {{"Without"s}, {"copies"s}, {"."s, true}},
+           }));
+}
+
+int main() {
+    TestSplitting();
+}
 struct TestToken {
     string data;
     bool is_end_sentence_punctuation = false;
