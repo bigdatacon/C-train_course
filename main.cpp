4d1c2e3806227e6923d69dc61ebc876c23e83ade
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-02-19 15:06:48 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,21 +1,107 @@
+// Вставьте сюда своё решение задачи о считалке Иосифа.
+
+#include <cassert>
 #include <iostream>
+#include <numeric>
 #include <vector>
 
 using namespace std;
 
-template <class T>
-double mean(const std::vector<T>& v) {
-    T acc;
-    const T* v_ptr = v.data();
+/*
+Вернитесь к решению задачи о считалке Иосифа и перепишите его с использованием итераторов перемещения. В тестах будет проверяться наличие make_move_iterator в коде функции.
 
-    for (size_t i = 0; i <= v.size(); ++i) {
-        acc += v_ptr[i];
+Подсказка 
+Вместо цикла для создания pool можно использовать конструктор с аргументами-итераторами.
+
+
+*/
+
+template <typename RandomIt>
+void MakeJosephusPermutation(RandomIt first, RandomIt last, uint32_t step_size) {
+    //auto original = first;
+    size_t cur_pos = 0;
+    size_t cur_size = last - first;
+    while (cur_size > 0) {
+        auto cur_item = std::move(* (first + cur_pos));
+        for (auto it = first + cur_pos; it != first; it--) {
+            *it = std::move(* (it - 1));
+        }
+        * (first++) = std::move(cur_item);
+        cur_size--;
+        if (cur_size) {
+            cur_pos = (cur_pos + step_size - 1) % cur_size;
+        } else {
+            break;
+        }
     }
+}
 
-    return static_cast<double>(acc) / v.size();
+
+vector<int> MakeTestVector() {
+    vector<int> numbers(10);
+    iota(begin(numbers), end(numbers), 0);
+    return numbers;
+}
+
+void TestIntVector() {
+    const vector<int> numbers = MakeTestVector();
+    {
+        vector<int> numbers_copy = numbers;
+        MakeJosephusPermutation(begin(numbers_copy), end(numbers_copy), 1);
+        assert(numbers_copy == numbers);
+    }
+    {
+        vector<int> numbers_copy = numbers;
+        MakeJosephusPermutation(begin(numbers_copy), end(numbers_copy), 3);
+        assert(numbers_copy == vector<int>({0, 3, 6, 9, 4, 8, 5, 2, 7, 1}));
+    }
+}
+
+// Это специальный тип, который поможет вам убедиться, что ваша реализация
+// функции MakeJosephusPermutation не выполняет копирование объектов.
+// Сейчас вы, возможно, не понимаете как он устроен, однако мы расскажем
+// об этом далее в нашем курсе
+
+struct NoncopyableInt {
+    int value;
+
+    NoncopyableInt(const NoncopyableInt&) = delete;
+    NoncopyableInt& operator=(const NoncopyableInt&) = delete;
+
+    NoncopyableInt(NoncopyableInt&&) = default;
+    NoncopyableInt& operator=(NoncopyableInt&&) = default;
+};
+
+bool operator==(const NoncopyableInt& lhs, const NoncopyableInt& rhs) {
+    return lhs.value == rhs.value;
+}
+
+ostream& operator<<(ostream& os, const NoncopyableInt& v) {
+    return os << v.value;
+}
+
+
+void TestAvoidsCopying() {
+    vector<NoncopyableInt> numbers;
+    numbers.push_back({1});
+    numbers.push_back({2});
+    numbers.push_back({3});
+    numbers.push_back({4});
+    numbers.push_back({5});
+
+    MakeJosephusPermutation(begin(numbers), end(numbers), 2);
+
+    vector<NoncopyableInt> expected;
+    expected.push_back({1});
+    expected.push_back({3});
+    expected.push_back({5});
+    expected.push_back({4});
+    expected.push_back({2});
+
+    assert(numbers == expected);
 }
 
 int main() {
-    vector<int> v = {1, 2, 3, 4, 5};
-    cout << "Mean: "s << mean(v) << endl;
+    TestIntVector();
+    TestAvoidsCopying();
 }
