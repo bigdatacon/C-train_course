6e4bae258a0d925f0fe5c6fd6c985fe99a225fdb
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-03-04 18:40:25 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,16 +8,19 @@ using namespace std;
 
 template <typename RandomIt>
 pair<RandomIt, RandomIt> FindStartsWith(RandomIt range_begin, RandomIt range_end, char prefix) {
-    auto pairs =make_pair(lower_bound(range_begin, range_end, string(1, prefix)),
-                     lower_bound(range_begin, range_end, string(1, char(prefix + 1)))); 
-    
+    auto pairs = make_pair(lower_bound(range_begin, range_end, string(1, prefix)),
+        lower_bound(range_begin, range_end, string(1, char(prefix + 1))));
+
     //cout << "first : " << pairs.first;
-    
+
     return pairs;
 }
 
 int main() {
-    const vector<string> sorted_strings = {"moscow", "murmansk", "vologda"};
+    const vector<string> sorted_strings = { "moscow", "murmansk", "vologda" };
+    /*auto m_result_2 = (sorted_strings.begin(), sorted_strings.end());
+    for (auto el : m_result_2) { cout << el<< endl; };*/
+
     const auto m_result = FindStartsWith(begin(sorted_strings), end(sorted_strings), 'm');
     for (auto it = m_result.first; it != m_result.second; ++it) {
         cout << *it << " ";
