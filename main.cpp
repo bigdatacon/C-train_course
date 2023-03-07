843e5ec9bf02dae1dd8b80fcbf07358b2ccc18e0
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-03-07 22:22:52 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,142 +1,79 @@
-#include <algorithm>
-#include <cassert>
+#pragma once
+
+// Тут можно подключить scopedptr.h и ptrvector.h, 
+// если они вам понадобятся.
+#include "ptrvector.h"
+#include "scopedptr.h"
+
+#include <new> // Для исключения bad_alloc
 #include <vector>
-#include <iostream>
 
-using namespace std;
-
-/*
-В деструкторе PtrVector примените операцию delete к каждому указателю внутри items_.
-Обработку исключений в коде копирующего конструктора можно упростить: используйте метод reserve и зарезервируйте в векторе место для хранения нужного количества щупалец. Тогда метод push_back не будет выбрасывать исключений.
-*/
-
-
-template <typename T>
-class PtrVector {
+// Щупальце
+class Tentacle {
 public:
-    PtrVector() = default;
-
-    // Создаёт вектор указателей на копии объектов из other
-    PtrVector(const PtrVector& other) {
-        // Реализуйте копирующий конструктор самостоятельно
-        items_.reserve(other.size());
-        for (const auto item : other.items_) {
-            if(item) {
-                items_.push_back(*item );
-            } else {
-                items_.push_back(nullptr);
-            }
-        }
-    }
-    
-    /*PtrVector(const PtrVector& other) {
-        // Реализуйте копирующий конструктор самостоятельно
-        items_.reserve(other.size());
-        for (const auto item : other) {
-        items_.push_back(&item);
-    }
-    }*/
-
-    // Деструктор удаляет объекты в куче, на которые ссылаются указатели,
-    // в векторе items_
-    ~PtrVector() {
-        // Реализуйте тело деструктора самостоятельно
-        for (const auto item : items_) {
-        delete item;
-    }
+    explicit Tentacle(int id) noexcept
+        : id_(id) {
     }
 
-    // Возвращает ссылку на вектор указателей
-    vector<T*>& GetItems() noexcept {
-        // Реализуйте метод самостоятельно
-       return items_;
+    int GetId() const noexcept {
+        return id_;
     }
 
-    // Возвращает константную ссылку на вектор указателей
-    vector<T*> const& GetItems() const noexcept {
-        // Реализуйте метод самостоятельно
-        return items_;
+    Tentacle* GetLinkedTentacle() const noexcept {
+        return linked_tentacle_;
+    }
+    void LinkTo(Tentacle& tentacle) noexcept {
+        linked_tentacle_ = &tentacle;
+    }
+    void Unlink() noexcept {
+        linked_tentacle_ = nullptr;
     }
 
 private:
-    vector<T*> items_;
+    int id_ = 0;
+    Tentacle* linked_tentacle_ = nullptr;
+};
 
-
-int size() const {
-        return items_.size();
-    }
-    };
-
-// Эта функция main тестирует шаблон класса PtrVector
-int main() {
-    // Вспомогательный "шпион", позволяющий узнать о своём удалении
-    struct DeletionSpy {
-        explicit DeletionSpy(bool& is_deleted)
-            : is_deleted_(is_deleted) {
-        }
-        ~DeletionSpy() {
-            is_deleted_ = true;
-        }
-        bool& is_deleted_;
-    };
-
-    // Проверяем удаление элементов
-    {
-        bool spy1_is_deleted = false;
-        DeletionSpy* ptr1 = new DeletionSpy(spy1_is_deleted);
-        {
-            PtrVector<DeletionSpy> ptr_vector;
-            ptr_vector.GetItems().push_back(ptr1);
-            assert(!spy1_is_deleted);
-
-            // Константная ссылка на ptr_vector
-            const auto& const_ptr_vector_ref(ptr_vector);
-            // И константная, и неконстантная версия GetItems
-            // должны вернуть ссылку на один и тот же вектор
-            assert(&const_ptr_vector_ref.GetItems() == &ptr_vector.GetItems());
-        }
-        // При разрушении ptr_vector должен удалить все объекты, на которые
-        // ссылаются находящиеся внутри него указателели
-        assert(spy1_is_deleted);
+// Осьминог
+class Octopus {
+public:
+    Octopus()
+        : Octopus(8) {
     }
 
-    // Вспомогательный «шпион», позволяющий узнать о своём копировании
-    struct CopyingSpy {
-        explicit CopyingSpy(int& copy_count)
-            : copy_count_(copy_count) {
+    explicit Octopus(int num_tentacles) {
+        Tentacle* t = nullptr;
+        try {
+            for (int i = 1; i <= num_tentacles; ++i) {
+                t = new Tentacle(i);  // Может выбросить исключение bad_alloc
+                tentacles_.GetItems().push_back(t);  // Может выбросить исключение bad_alloc
+
+                t = nullptr;
+            }
+        } catch (const std::bad_alloc&) {
+            delete t;
+            throw;
         }
-        CopyingSpy(const CopyingSpy& rhs)
-            : copy_count_(rhs.copy_count_)  //
-        {
-            ++copy_count_;
-        }
-        int& copy_count_;
-    };
-
-    // Проверяем копирование элементов при копировании массива указателей
-    {
-        // 10 элементов
-        vector<int> copy_counters(10);
-
-        PtrVector<CopyingSpy> ptr_vector;
-        // Подготавливаем оригинальный массив указателей
-        for (auto& counter : copy_counters) {
-            ptr_vector.GetItems().push_back(new CopyingSpy(counter));
-        }
-        // Последний элемент содержит нулевой указатель
-        ptr_vector.GetItems().push_back(nullptr);
-
-        auto ptr_vector_copy(ptr_vector);
-        // Количество элементов в копии равно количеству элементов оригинального вектора
-        assert(ptr_vector_copy.GetItems().size() == ptr_vector.GetItems().size());
-
-        // копия должна хранить указатели на новые объекты
-        assert(ptr_vector_copy.GetItems() != ptr_vector.GetItems());
-        // Последний элемент исходного массива и его копии - нулевой указатель
-        assert(ptr_vector_copy.GetItems().back() == nullptr);
-        // Проверяем, что элементы были скопированы (копирующие шпионы увеличивают счётчики копий).
-        assert(all_of(copy_counters.begin(), copy_counters.end(), [](int counter) {
-            return counter == 1;
-        }));
     }
-} 
+
+    Tentacle& AddTentacle() {
+        ScopedPtr<Tentacle> t(new Tentacle(GetTentacleCount()+1));
+        tentacles_.GetItems().push_back(t.GetRawPtr());
+        t.Release();
+        return *tentacles_.GetItems().back(); // возвращаю разименованный послдений 
+    }
+
+    int GetTentacleCount() const noexcept {
+        return static_cast<int>(tentacles_.GetItems().size());
+    }
+
+    const Tentacle& GetTentacle(size_t index) const {
+        return *tentacles_.GetItems().at(index);
+    }
+    Tentacle& GetTentacle(size_t index) {
+        return *tentacles_.GetItems().at(index);
+    }
+
+private:
+    PtrVector<Tentacle> tentacles_;
+};
