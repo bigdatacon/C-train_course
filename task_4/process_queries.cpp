286d42f404ed3fca6d80521db1077995b61482af
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-03-13 18:46:26 +0300

    Update process_queries.cpp

--- task_4/process_queries.cpp
+++ task_4/process_queries.cpp
@@ -17,10 +17,13 @@ std::vector<std::vector<Document>> ProcessQueries(
     const SearchServer& search_server,
     const std::vector<std::string>& queries)
 {
-    std::vector<Document> results(queries.size());
+    std::vector<Document> results;
+    std::vector<std::vector<Document>> itg(queries.size());
     transform(std::execution::par, queries.begin(), queries.end(), results.begin(), [&search_server](std::string string_for_find) { return search_server.FindTopDocuments(string_for_find, [](int document_id, DocumentStatus status, int rating) {
         return status == DocumentStatus::ACTUAL; }); });  // записываю результат работы поискадокументов в вектор
-return results;    
+itg.push_back(results);
+results.clear();
+return itg;    
     
     
-};
+}
