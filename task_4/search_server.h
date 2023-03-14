b3b80d457b24be4280c34eada6635140d6c04a38
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-03-14 20:01:28 +0300

    Update search_server.h

--- task_4/search_server.h
+++ task_4/search_server.h
@@ -49,6 +49,7 @@ public:
 
         //3. Разработайте метод удаления документов из поискового сервера
     void RemoveDocument(int document_id);
+    void RemoveDocument(const std::execution::parallel_policy& policy, int document_id); 
     
     std::set<int> GetDoc_ids( ){return document_ids_;} ;
     
@@ -155,4 +156,4 @@ std::vector<Document> SearchServer::FindAllDocuments(const Query& query, Documen
     return matched_documents;
 }
 
-void RemoveDocument(const std::execution::parallel_policy&, int document_id); 
+
