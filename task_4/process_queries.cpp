e4ecfea3ddf7d42cf4642363aba34892c1aeaed7
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-03-14 16:26:19 +0300

    Update process_queries.cpp

--- task_4/process_queries.cpp
+++ task_4/process_queries.cpp
@@ -6,10 +6,6 @@
 #include <iostream>
 #include <functional>
 
-// Подсказка 
-/*
-Достаточно применить функцию transform, которая запишет результат в вектор, заранее созданный с нужной длиной.
-*/
 
 std::vector<std::vector<Document>> ProcessQueries(
     const SearchServer& search_server,
@@ -23,47 +19,19 @@ return itg;
     
 }
 
-//1 так не работает
-/*std::vector<Document> ProcessQueriesJoined(
-    const SearchServer& search_server,
-    const std::vector<std::string>& queries)
+std::vector<Document> ProcessQueriesJoined(const SearchServer& search_server, const std::vector<std::string>& queries)
 {
-    std::vector<Document> results;
-    transform(std::execution::par, queries.begin(), queries.end(), results.begin(),     [&search_server](std::string string_for_find) {return search_server.FindTopDocuments(string_for_find); } );
-
-return results;    
-    
-}*/
-
-//2 переиспользую функцию выше - так не работает прямым проходом 
-std::vector<Document> ProcessQueriesJoined(
-    const SearchServer& search_server,
-    const std::vector<std::string>& queries)
-{
-    std::vector<Document> results;
-
-        auto documents = ProcessQueries(search_server, queries);
-        for (auto el : documents){ 
-            transform(std::execution::par, el.begin(), el.end(), results.begin(),     [results](Document doc) {return results.push_back(doc); } );
-        }
-
-return results;    
-    
-}
-
-//3 переиспользую функцию выше -через лябмду но иначе - тоже не работает
-/*std::vector<Document> ProcessQueriesJoined(
-    const SearchServer& search_server,
-    const std::vector<std::string>& queries)
-{
-    std::vector<Document> results;
-
-    auto documents = ProcessQueries(search_server, queries);
-
-    transform(std::execution::par, documents.begin(), documents.end(), results.begin(),     [results](Document doc) {return results.push_back(doc); } );
+    //size_t total_size = 0u;
+    auto matched_docs = ProcessQueries(search_server, queries);
+    /*for (auto& query_res : matched_docs) {
+        total_size += query_res.size();
+    }*/
+    std::vector<Document> result;
+    //result.reserve(total_size);
+    for (auto& query_res : matched_docs) {
+        result.insert(result.end(), query_res.begin(), query_res.end());
     }
-
-return results;    
     
-}*/
+    return result;
+}
 
