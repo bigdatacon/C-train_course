6276e51510f873220d1f23ef3b87cf7187c291e8
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-03-15 22:49:39 +0300

    Update search_server.cpp

--- task_5_/search_server.cpp
+++ task_5_/search_server.cpp
@@ -211,7 +211,20 @@ SearchServer::Query SearchServer::ParseQuery(const std::execution::sequenced_pol
 // параллельная версия 
 SearchServer::Query SearchServer::ParseQuery(const std::execution::parallel_policy& policy, const std::string& text) const {
 	Query result;
-	for ( const std::string& word : SplitIntoWords(text)) {
+    auto p=[this , result](const std::string& word){
+        const auto query_word = ParseQueryWord(word);
+		if (!query_word.is_stop) {
+			if (query_word.is_minus) {
+				result.minus_words.insert(query_word.data);
+			}
+			else {
+				result.plus_words.insert(query_word.data);
+			}
+		}
+      }
+    for_each(policy, SplitIntoWords(text).begin(), SplitIntoWords(text).end(),p);
+    
+	/*for ( const std::string& word : SplitIntoWords(text)) {
 		const auto query_word = ParseQueryWord(word);
 		if (!query_word.is_stop) {
 			if (query_word.is_minus) {
@@ -221,7 +234,7 @@ SearchServer::Query SearchServer::ParseQuery(const std::execution::parallel_poli
 				result.plus_words.insert(query_word.data);
 			}
 		}
-	}
+	}*/
 	return result;
 }
 
