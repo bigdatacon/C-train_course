2d7ca863d208b8ce7d75fb8db88f0551fe3f8d0e
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-03-15 12:36:26 +0300

    Update search_server.cpp

--- task_4/search_server.cpp
+++ task_4/search_server.cpp
@@ -213,15 +213,10 @@ void SearchServer::RemoveDocument(const std::execution::parallel_policy& policy,
         word_freqs_.at(document_id).end(),
         words_for_erase.begin(),
         [this](const auto & temp) {
-                return const_cast<std::string *> (&temp.first); /*&temp.first;*/
+                return const_cast<std::string *> (&temp.first); 
             }
     );
-    
-    /*auto p = [this, word_to_document_freqs_](std::string* t){return word_to_document_freqs_.erase(*t);}*/
-    
     auto p = [this](const std::string* t){return word_to_document_freqs_.erase(*t);};
-    
-    // удаляю слова из word_to_document_freqs_ если указатели на них есть в векторе указателей words_for_erase
     std::for_each(
         policy,
         words_for_erase.begin(),
