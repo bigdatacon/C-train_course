9ab5f924a292bfa7a059856e2a08c1fd1c94383b
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-03-16 12:32:02 +0300

    Update search_server.cpp

--- task_5_/search_server.cpp
+++ task_5_/search_server.cpp
@@ -265,7 +265,7 @@ SearchServer::Query SearchServer::ParseQuery(const std::execution::sequenced_pol
 SearchServer::Query SearchServer::ParseQuery(const std::execution::parallel_policy& policy, const std::string& text) const {
 	Query result;
     // заготовка под for_each
-    auto p=[this , result](const std::string& word){
+    auto p=[this , &result](const std::string& word){
         const auto query_word = ParseQueryWord(word);
 		if (!query_word.is_stop) {
 			if (query_word.is_minus) {
@@ -275,8 +275,9 @@ SearchServer::Query SearchServer::ParseQuery(const std::execution::parallel_poli
 				result.plus_words.push_back(query_word.data);
 			}
 		}
-      }
-    for_each(policy, SplitIntoWords(text).begin(), SplitIntoWords(text).end(),p);
+      };
+    
+    std::for_each(policy, SplitIntoWords(text).begin(), SplitIntoWords(text).end(),p);
     
 	/*for ( const std::string& word : SplitIntoWords(text)) {
 		const auto query_word = ParseQueryWord(word);
