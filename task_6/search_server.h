7a3239dde162b8b05a1d2fad90ebf6dbd069a4ea
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-03-17 20:29:25 +0300

    Update search_server.h

--- task_6/search_server.h
+++ task_6/search_server.h
@@ -26,7 +26,7 @@ public:
     explicit SearchServer(const std::string_view& stop_words_text);
     explicit SearchServer(const std::string& stop_words_text);
     
-    void SearchServer::AddDocument(int document_id, const std::string_view& document, DocumentStatus status, const std::vector<int>& ratings);
+    void AddDocument(int document_id, const std::string_view& document, DocumentStatus status, const std::vector<int>& ratings);
     void AddDocument(int document_id, const std::string& document, DocumentStatus status, const std::vector<int>& ratings); 
 
     template <typename DocumentPredicate>
