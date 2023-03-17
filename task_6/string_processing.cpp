7a89ffed65ca55d5bcbabd573e42da900f8c37fe
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-03-17 20:28:25 +0300

    Update string_processing.cpp

--- task_6/string_processing.cpp
+++ task_6/string_processing.cpp
@@ -35,14 +35,6 @@ std::vector<std::string_view> SplitIntoWords(const std::string_view& text) {
                 word= {};
             }
         } else {
-            /*букву к string_view, нужно создать новый string_view, состоящий из старого + новая буква. Сделать это можно так: 
-```
-string_view old_str_view = "Это старый string_view";
-char new_char = '!';
-string new_str(old_str_view.data(), old_str_view.size() + 1); // создаём новый string более большого размера
-new_str.back() = new_char; // добавляем новую букву в конец строки
-string_view new_str_view(new_str); // создаём новый string_view на основе нового string
-old_str_view = new_str_view; // присваиваем старому string_view значение нового*/
             std::string new_str(word.data(), word.size() + 1);
             new_str.back() = c;
             std::string_view new_str_view(new_str);
@@ -61,7 +53,7 @@ old_str_view = new_str_view; // присваиваем старому string_vie
 std::set<std::string_view> MakeUniqueNonEmptyStrings(const std::vector<std::string_view>& strings) {
     std::set<std::string_view> non_empty_strings;
     for (const auto& str : strings) {
-        if (!str.empty()) {
+        if (str.size()==0) {
             non_empty_strings.insert(str);
         }
     }
