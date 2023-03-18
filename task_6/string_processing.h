accac05dd93299c1e2ca0f685ea2019cb92b9bef
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-03-18 15:12:30 +0300

    Update string_processing.h

--- task_6/string_processing.h
+++ task_6/string_processing.h
@@ -6,11 +6,11 @@
 std::vector<std::string> SplitIntoWords(const std::string& text) ;
 std::vector<std::string_view> SplitIntoWords(const std::string_view& text);
 
-std::set<std::string_view> MakeUniqueNonEmptyStrings(const std::vector<std::string_view>& strings);
+/*std::set<std::string_view> MakeUniqueNonEmptyStrings(const std::vector<std::string_view>& strings);*/
 
 template <typename StringContainer>
-std::set<std::string> MakeUniqueNonEmptyStrings(const StringContainer& strings) {
-    std::set<std::string> non_empty_strings;
+std::set<std::string, std::less> MakeUniqueNonEmptyStrings(const StringContainer& strings) {
+    std::set<std::string, std::less> non_empty_strings;
     for (const std::string& str : strings) {
         if (!str.empty()) {
             non_empty_strings.insert(str);
