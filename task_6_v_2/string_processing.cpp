1b568e6badf94617821b51637719ecd723989b52
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-03-19 13:49:15 +0300

    Update string_processing.cpp

--- task_6_v_2/string_processing.cpp
+++ task_6_v_2/string_processing.cpp
@@ -3,29 +3,24 @@
 #include <string>
 #include <string_view>
 
-
-// SplitIntoWords для Stringview
-std::vector<std::string_view> SplitIntoWords(const std::string_view& text) {
-    std::vector<std::string_view> words;
-    std::string_view word;
-    for (const char c : text) {
-        if (c == ' ') {
-            if (!word.empty()) {
-                words.push_back(word);
-                //word.clear();
-                word= {};
-            }
-        } else {
-            std::string new_str(word.data(), word.size() + 1);
-            new_str.back() = c;
-            std::string_view new_str_view(new_str);
-            word = new_str_view;
-        }
-    }
-    if (!word.empty()) {
-        words.push_back(word);
+std::vector<std::string_view> SplitIntoWords(const std::string_view& str) {
+    std::vector<std::string_view> result;
+    // 1
+    int64_t pos = str.find_first_not_of(" ");
+    // 2
+    const int64_t pos_end = str.npos;
+    // 3
+    while (pos != pos_end) {
+        // 4
+        int64_t space = str.find(' ', pos);
+        // 5
+        result.push_back(space == pos_end ? str.substr(pos) : str.substr(pos, space - pos));
+        // 6
+        pos = str.find_first_not_of(" ", space);
     }
 
-    return words;
-}
+    return result;
+} 
+
+
 
