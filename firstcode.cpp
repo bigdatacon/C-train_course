0a60ac4eb36a866f9f2b2aeb4f9ef670f14b72a7
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-03-21 19:17:25 +0300

    Update firstcode.cpp

--- firstcode.cpp
+++ firstcode.cpp
@@ -1,11 +1,81 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <iostream>
+#include <sstream>
+
+using std::istringstream;
+using std::string;
+using std::cout;
+
+using namespace std;
+// тут пример разбиения строк на слова через issstream 
+/*int main() {
+    std::vector<std::string> my_vector{ "Hello, world!", "How are you?", "Nice to meet you." };
+
+    std::vector<std::vector<std::string>> words_vector;
+
+    std::transform(my_vector.begin(), my_vector.end(), std::back_inserter(words_vector), [](std::string s) {
+    std::vector<std::string> words;
+    std::string word;
+    std::istringstream stream(s); // превращаем строку s в поток
+    while (stream >> word) { // парсим слова из потока
+        words.push_back(word);
+    }
+    return words;
+        });
+    
+
+    for (auto el : words_vector) { for (auto e : el) { cout << e << endl; } }
+    // выводим каждое слово вместе с номером строки
+    for (int i = 0; i < words_vector.size(); ++i) {
+        for (int j = 0; j < words_vector[i].size(); ++j) {
+            std::cout << "Строка " << i << ", слово " << j << ": " << words_vector[i][j] << std::endl;
+        }
+    }
+
+    return 0;
+}*/
+
 #include <functional>
 #include <iostream>
 #include <map>
 #include <set>
 #include <sstream>
 #include <string>
+#include <execution>
+#include <future>
+#include <algorithm>
 
 using namespace std;
+
+/* Подсказка Разные строки текста или наборы этих строк можно обрабатывать параллельно, а затем складывать полученные словари.
+Если собираетесь добавлять future в вектор, имейте в виду: это некопируемый тип.*/
+
+vector<string> SplitIntoWords(const string& text) {
+    vector<string> words;
+    string word;
+    for (const char c : text) {
+        if (c == ' ') {
+            if (!word.empty()) {
+                words.push_back(word);
+                word.clear();
+            }
+        }
+        else {
+            if (isprint(c) ) {
+                word += c;
+            }
+        }
+    }
+    if (!word.empty()) {
+        words.push_back(word);
+    }
+
+    return words;
+}
+
 
 struct Stats {
     map<string, int> word_frequences;
@@ -17,12 +87,56 @@ struct Stats {
 
 using KeyWords = set<string, less<>>;
 
+std::string safe_getline(istream& input) {
+    std::string line;
+    getline(input, line);
+    return move(line);
+}
+
+/* Подсказка Разные строки текста или наборы этих строк можно обрабатывать параллельно, а затем складывать полученные словари.
+Если собираетесь добавлять future в вектор, имейте в виду: это некопируемый тип.*/
 Stats ExploreKeyWords(const KeyWords& key_words, istream& input) {
-    // ...
+
+    Stats stat;
+    vector<string> all_words;
+    std::string line;
+    //std::istream& input = std::cin; // можно использовать любой 
+    while (safe_getline(input).size()!=0)
+    
+    { // читаем строку из входного потока, пока не достигнем конца
+        std::string line = safe_getline(input);
+        cout << "line before : " << line << endl;
+        //line.erase(std::remove(line.begin(), line.end(), '"'));
+        //cout << "line after : " << line << endl;
+        
+        /*for_each(line.begin(), line.end(), [&all_words](auto& s) {
+            auto async_ = async([&s] { return SplitIntoWords(*s); });
+        all_words.insert(all_words.end(), async_.get().begin(), async_.get().end() );
+            });*/
+
+
+
+        vector<string> words = SplitIntoWords(line);
+        cout << "words.size() : " << words.size() << endl;
+        //all_words.insert(all_words.end(), words.begin(), words.begin() );
+        all_words.insert(all_words.end(), words.begin(), words.end());
+
+        //words.clear();
+    }
+
+    for (auto word : all_words) {
+        if (key_words.count(word)) {
+            int freq = count(all_words.begin(), all_words.end(), word);
+            stat.word_frequences[word] = freq;
+        }
+    }
+
+    return stat;
+
 }
 
 int main() {
-    const KeyWords key_words = {"yangle", "rocks", "sucks", "all"};
+    const KeyWords key_words = { "yangle", "rocks", "sucks", "all" };
 
     stringstream ss;
     ss << "this new yangle service really rocks\n";
