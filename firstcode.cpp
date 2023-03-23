35ee516017d9b135284f8174b68f8c7978c89672
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-03-23 21:36:47 +0300

    Update firstcode.cpp

--- firstcode.cpp
+++ firstcode.cpp
@@ -9,11 +9,11 @@
 #include <vector>
 #include <thread>
 #include <future>
- 
+
 #include "log_duration.h"
- 
+
 using namespace std;
- 
+
 template <typename Strings>
 void PrintStrings(const Strings& strings) {
     for (string_view s : strings) {
@@ -21,7 +21,7 @@ void PrintStrings(const Strings& strings) {
     }
     cout << endl;
 }
- 
+
 string GenerateWord(mt19937& generator, int max_length) {
     const int length = uniform_int_distribution(1, max_length)(generator);
     string word;
@@ -31,7 +31,7 @@ string GenerateWord(mt19937& generator, int max_length) {
     }
     return word;
 }
- 
+
 template <template <typename> typename Container>
 Container<string> GenerateDictionary(mt19937& generator, int word_count, int max_length) {
     vector<string> words;
@@ -41,28 +41,28 @@ Container<string> GenerateDictionary(mt19937& generator, int word_count, int max
     }
     return Container(words.begin(), words.end());
 }
- 
+
 struct Reverser {
     void operator()(string& value) const {
         reverse(value.begin(), value.end());
     }
 };
- 
+
 template <typename Container, typename Function>
 void Test(string_view mark, Container keys, Function function) {
     LOG_DURATION(mark);
     function(keys, Reverser{});
 }
- 
+
 #define TEST(function) Test(#function, keys, function<remove_const_t<decltype(keys)>, Reverser>)
- 
+
 /*template <typename RandomAccessIterator, typename Value>
 RandomAccessIterator LowerBound(const execution::sequenced_policy&,
   RandomAccessIterator range_begin, RandomAccessIterator range_end,
   const Value& value) {
   return std::lower_bound(range_begin, range_end, value);
 }*/
- 
+
 template <typename ForwardRange, typename Function>
 void ForEach(const ExecutionPolicy::parallel_policy &, ForwardRange& range, Function function) {
     // ускорьте эту реализацию
@@ -96,54 +96,58 @@ void ForEach(const ExecutionPolicy::parallel_policy &, ForwardRange& range, Func
     }
     for (auto& task : asyncs) { task.get(); }
 }
- 
+
 template <typename ForwardRange, typename Function>
 void ForEach(const ExecutionPolicy&,  ForwardRange& range, Function function) {
   if constexpr (is_same_v<decay_t<ExecutionPolicy>, ExecutionPolicy::sequenced_policy> ) {
        return for_each(execution::par, range.begin(), range.end(), function); // если параллельная то базовую функцию 
   }
   if constexpr (is_same_v<decay_t<random_access_iterator_tag>, typename ForwardRange::iterator> && ! (is_same_v<decay_t<ExecutionPolicy>, ExecutionPolicy::sequenced_policy>)  { 
- 
+  
   ForEach(execution::par, range, range_end, function);
- 
+  
   }
- 
- 
 }
- 
- 
+
+template <typename ForwardRange, typename Function>
+void ForEach( ForwardRange& range, Function function) {
+       return for_each(execution::par, range.begin(), range.end(), function); // если параллельная то базовую функцию 
+  }
+
+
+
 int main() {
     auto reverser = [](string& s) { reverse(s.begin(), s.end()); };
- 
+
     list<string> strings_list = {"cat", "dog", "code"};
- 
+
     ForEach(strings_list, reverser);
     PrintStrings(strings_list);
     // tac god edoc
- 
+
     ForEach(execution::seq, strings_list, reverser);
     PrintStrings(strings_list);
     // cat dog code
- 
+
     // единственный из вызовов, где должна работать ваша версия
     // из предыдущего задания
     ForEach(execution::par, strings_list, reverser);
     PrintStrings(strings_list);
     // tac god edoc
- 
+
     vector<string> strings_vector = {"cat", "dog", "code"};
- 
+
     ForEach(strings_vector, reverser);
     PrintStrings(strings_vector);
     // tac god edoc
- 
+
     ForEach(execution::seq, strings_vector, reverser);
     PrintStrings(strings_vector);
     // cat dog code
- 
+
     ForEach(execution::par, strings_vector, reverser);
     PrintStrings(strings_vector);
     // tac god edoc
- 
+
     return 0;
 } 
