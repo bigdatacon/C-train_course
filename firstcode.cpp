f7c03110e2012ea5204fd04f02893242286889b6
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-03-25 16:03:50 +0300

    Update firstcode.cpp

--- firstcode.cpp
+++ firstcode.cpp
@@ -1,145 +1,100 @@
-#include <algorithm>
-#include <execution>
-#include <iostream>
-#include <list>
-#include <random>
-#include <string>
-#include <string_view>
-#include <type_traits>
-#include <vector>
-#include <thread>
 #include <future>
+#include <mutex>
+#include <numeric>
+#include <queue>
+#include <string>
+#include <thread>
+#include <vector>
 
-#include "log_duration.h"
+#include "test_framework.h"
 
 using namespace std;
 
-template <typename Strings>
-void PrintStrings(const Strings& strings) {
-	for (string_view s : strings) {
-		cout << s << " ";
-	}
-	cout << endl;
-}
+// Реализуйте шаблон Synchronized<T>.
+// Метод GetAccess должен возвращать структуру, в которой есть поле T& ref_to_value.
+template <typename T>
+class Synchronized {
+public:
+    explicit Synchronized(T initial = T());
 
-string GenerateWord(mt19937& generator, int max_length) {
-	const int length = uniform_int_distribution(1, max_length)(generator);
-	string word;
-	word.reserve(length);
-	for (int i = 0; i < length; ++i) {
-		word.push_back(uniform_int_distribution('a', 'z')(generator));
-	}
-	return word;
-}
+    struct Access {
+        T& ref_to_value;
+    };
 
-template <template <typename> typename Container>
-Container<string> GenerateDictionary(mt19937& generator, int word_count, int max_length) {
-	vector<string> words;
-	words.reserve(word_count);
-	for (int i = 0; i < word_count; ++i) {
-		words.push_back(GenerateWord(generator, max_length));
-	}
-	return Container(words.begin(), words.end());
-}
+    Access GetAccess();
 
-struct Reverser {
-	void operator()(string& value) const {
-		reverse(value.begin(), value.end());
-	}
+private:
+    T value_;
 };
 
-template <typename Container, typename Function>
-void Test(string_view mark, Container keys, Function function) {
-	LOG_DURATION(mark);
-	function(keys, Reverser{});
+void TestConcurrentUpdate() {
+    Synchronized<string> common_string;
+
+    const size_t add_count = 50000;
+    auto updater = [&common_string, add_count] {
+        for (size_t i = 0; i < add_count; ++i) {
+            auto access = common_string.GetAccess();
+            access.ref_to_value += 'a';
+        }
+    };
+
+    auto f1 = async(updater);
+    auto f2 = async(updater);
+
+    f1.get();
+    f2.get();
+
+    ASSERT_EQUAL(common_string.GetAccess().ref_to_value.size(), 2 * add_count);
 }
 
-#define TEST(function) Test(#function, keys, function<remove_const_t<decltype(keys)>, Reverser>)
+vector<int> Consume(Synchronized<deque<int>>& common_queue) {
+    vector<int> got;
 
-//обычная функция если не передана даже политика
-template <typename ForwardRange, typename Function> void ForEach(ForwardRange& range, Function function) {
-	return for_each(execution::par, range.begin(), range.end(), function); // если без политики то базовую  функцию 
+    for (;;) {
+        deque<int> q;
+
+        {
+            // Мы специально заключили эти две строчки в операторные скобки, чтобы
+            // уменьшить размер критической секции. Поток-потребитель захватывает
+            // мьютекс, перемещает всё содержимое общей очереди в свою
+            // локальную переменную и отпускает мьютекс. После этого он обрабатывает
+            // объекты в очереди за пределами критической секции, позволяя
+            // потоку-производителю параллельно помещать в очередь новые объекты.
+            //
+            // Размер критической секции существенно влияет на быстродействие
+            // многопоточных программ.
+            auto access = common_queue.GetAccess();
+            q = move(access.ref_to_value);
+        }
+
+        for (int item : q) {
+            if (item > 0) {
+                got.push_back(item);
+            } else {
+                return got;
+            }
+        }
+    }
 }
 
-// проверку какую функцию вызвать - если и параллельная политика и итератор не произвольного доступа(не random то вызывается моя функция , в остальных случаях обычная )
-template <typename ForwardRange, typename Function, typename Policy>
-void ForEach(const Policy& /*policy*/, ForwardRange& range, Function function) {
-	if constexpr (!(is_same_v<decay_t<random_access_iterator_tag>, typename ForwardRange::iterator>) && is_same_v<decay_t<Policy>, execution::parallel_policy > ) {// если итератор не рандомный и политика пололлельная то свою
+void TestProducerConsumer() {
+    Synchronized<deque<int>> common_queue;
 
-		int size_ = range.size();
-		unsigned int task_count = std::thread::hardware_concurrency();
-		int chunc_size = size_ / task_count;
-		int left_size = size_ % task_count;
-		int first_chunc_size = 0;
-		vector<future<void>> asyncs;
-		if (left_size) {
-			first_chunc_size = chunc_size + left_size;
-		}
-		auto left_boarder = range.begin();
-		bool first = true;
-		for (auto itr = left_boarder; itr != range.end(); next(itr, first_chunc_size)) {
-			if (first) {
-				auto begin_it = itr; // получают итератор на начало отрезка 
-				std::advance(itr, first_chunc_size); // получаю итератор на конец  отрезка 
-				asyncs.push_back(async([function, begin_it, itr] { for_each(begin_it, itr, function); }));
-				first = false;
-				left_boarder = itr;
-				first_chunc_size = chunc_size;
-			}
-			else {
-				auto begin_it = itr; // получают итератор на начало отрезка 
-				std::advance(itr, first_chunc_size); // получаю итератор на конец  отрезка 
-				asyncs.push_back(async([function, begin_it, itr] { for_each(begin_it, itr, function); }));
-				left_boarder = itr;
-			}
-		}
-		for (auto& task : asyncs) { task.get(); }
+    auto consumer = async(Consume, ref(common_queue));
 
-	}
-	else { 
-    
-    ForEach(range, function); 
-    
-    //во всех остальных обычную   рандомный то обычную} 
-		
+    const size_t item_count = 100000;
+    for (size_t i = 1; i <= item_count; ++i) {
+        common_queue.GetAccess().ref_to_value.push_back(i);
+    }
+    common_queue.GetAccess().ref_to_value.push_back(-1);
 
-
-	}
+    vector<int> expected(item_count);
+    iota(begin(expected), end(expected), 1);
+    ASSERT_EQUAL(consumer.get(), expected);
 }
 
-
-	int main() {
-		auto reverser = [](string& s) { reverse(s.begin(), s.end()); };
-
-		list<string> strings_list = { "cat", "dog", "code" };
-
-		ForEach(strings_list, reverser);
-		PrintStrings(strings_list);
-		// tac god edoc
-
-		ForEach(execution::seq, strings_list, reverser);
-		PrintStrings(strings_list);
-		// cat dog code
-
-		// единственный из вызовов, где должна работать ваша версия
-		// из предыдущего задания
-		ForEach(execution::par, strings_list, reverser);
-		PrintStrings(strings_list);
-		// tac god edoc
-
-		vector<string> strings_vector = { "cat", "dog", "code" };
-
-		ForEach(strings_vector, reverser);
-		PrintStrings(strings_vector);
-		// tac god edoc
-
-		ForEach(execution::seq, strings_vector, reverser);
-		PrintStrings(strings_vector);
-		// cat dog code
-
-		ForEach(execution::par, strings_vector, reverser);
-		PrintStrings(strings_vector);
-		// tac god edoc
-
-		return 0;
-	}
+int main() {
+    TestRunner tr;
+    RUN_TEST(tr, TestConcurrentUpdate);
+    RUN_TEST(tr, TestProducerConsumer);
+}
