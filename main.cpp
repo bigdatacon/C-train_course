e2cc576eecd65ea3d9984a2e752ec0bbac7f558e
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-03-25 17:25:25 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,143 +1,104 @@
-#include <iostream>
-#include <string>
-#include <algorithm>
-#include <vector>
-#include <iostream>
-#include <sstream>
-
-using std::istringstream;
-using std::string;
-using std::cout;
-
-using namespace std;
-// тут пример разбиения строк на слова через issstream 
-/*int main() {
-    std::vector<std::string> my_vector{ "Hello, world!", "How are you?", "Nice to meet you." };
-
-    std::vector<std::vector<std::string>> words_vector;
-
-    std::transform(my_vector.begin(), my_vector.end(), std::back_inserter(words_vector), [](std::string s) {
-    std::vector<std::string> words;
-    std::string word;
-    std::istringstream stream(s); // превращаем строку s в поток
-    while (stream >> word) { // парсим слова из потока
-        words.push_back(word);
-    }
-    return words;
-        });
-    
-
-    for (auto el : words_vector) { for (auto e : el) { cout << e << endl; } }
-    // выводим каждое слово вместе с номером строки
-    for (int i = 0; i < words_vector.size(); ++i) {
-        for (int j = 0; j < words_vector[i].size(); ++j) {
-            std::cout << "Строка " << i << ", слово " << j << ": " << words_vector[i][j] << std::endl;
-        }
-    }
-
-    return 0;
-}*/
-
-#include <functional>
-#include <iostream>
-#include <map>
-#include <set>
-#include <sstream>
-#include <string>
-#include <execution>
 #include <future>
-#include <algorithm>
+#include <mutex>
+#include <numeric>
+#include <queue>
+#include <string>
+#include <thread>
+#include <vector>
+
+#include "test_framework.h"
 
 using namespace std;
 
-/* Подсказка Разные строки текста или наборы этих строк можно обрабатывать параллельно, а затем складывать полученные словари.
-Если собираетесь добавлять future в вектор, имейте в виду: это некопируемый тип.*/
+// Реализуйте шаблон Synchronized<T>.
+// Метод GetAccess должен возвращать структуру, в которой есть поле T& ref_to_value.
+template <typename T>
+class Synchronized {
+public:
+    explicit Synchronized(T initial = T());
 
-vector<string> SplitIntoWords(const string& text) {
-    vector<string> words;
-    string word;
-    for (const char c : text) {
-        if (c == ' ') {
-            if (!word.empty()) {
-                words.push_back(word);
-                word.clear();
-            }
-        }
-        else {
-            if (isprint(c) ) {
-                word += c;
-            }
-        }
-    }
-    if (!word.empty()) {
-        words.push_back(word);
-    }
+    struct Access {
+        T& ref_to_value;
+    };
 
-    return words;
-}
+    Access GetAccess(){ //Метод GetAccess должен возвращать структуру, в которой есть поле T& ref_to_value
+        Access structura;
+        structura.ref_to_value = value_;
+        return structura;
+    };
 
-
-struct Stats {
-    map<string, int> word_frequences;
-
-    void operator+=(const Stats& other) {
-        // сложить частоты
-    }
+private:
+    T value_;
 };
 
-using KeyWords = set<string, less<>>;
-/* Подсказка Разные строки текста или наборы этих строк можно обрабатывать параллельно, а затем складывать полученные словари.
-Если собираетесь добавлять future в вектор, имейте в виду: это некопируемый тип.*/
-Stats ExploreKeyWords(const KeyWords& key_words, istream& input) {
+void TestConcurrentUpdate() {
+    Synchronized<string> common_string;
 
-    Stats stat;
-    vector<string> all_words;
-    std::string line;
-    //std::istream& input = std::cin; // можно использовать любой 
-    while (std::getline(input, line)) { // читаем строку из входного потока, пока не достигнем конца
-        cout << "line before : " << line << endl;
-        //line.erase(std::remove(line.begin(), line.end(), '"'));
-        //cout << "line after : " << line << endl;
-        
-        /*for_each(line.begin(), line.end(), [&all_words](auto& s) {
-            auto async_ = async([&s] { return SplitIntoWords(*s); });
-        all_words.insert(all_words.end(), async_.get().begin(), async_.get().end() );
-            });*/
+    const size_t add_count = 50000;
+    auto updater = [&common_string, add_count] {
+        for (size_t i = 0; i < add_count; ++i) {
+            auto access = common_string.GetAccess();
+            access.ref_to_value += 'a';
+        }
+    };
 
+    auto f1 = async(updater);
+    auto f2 = async(updater);
 
+    f1.get();
+    f2.get();
 
-        vector<string> words = SplitIntoWords(line);
-        cout << "words.size() : " << words.size() << endl;
-        //all_words.insert(all_words.end(), words.begin(), words.begin() );
-        all_words.insert(all_words.end(), words.begin(), words.end());
+    ASSERT_EQUAL(common_string.GetAccess().ref_to_value.size(), 2 * add_count);
+}
 
-        //words.clear();
-    }
+vector<int> Consume(Synchronized<deque<int>>& common_queue) {
+    vector<int> got;
 
-    for (auto word : all_words) {
-        if (key_words.count(word)) {
-            int freq = count(all_words.begin(), all_words.end(), word);
-            stat.word_frequences[word] = freq;
+    for (;;) {
+        deque<int> q;
+
+        {
+            // Мы специально заключили эти две строчки в операторные скобки, чтобы
+            // уменьшить размер критической секции. Поток-потребитель захватывает
+            // мьютекс, перемещает всё содержимое общей очереди в свою
+            // локальную переменную и отпускает мьютекс. После этого он обрабатывает
+            // объекты в очереди за пределами критической секции, позволяя
+            // потоку-производителю параллельно помещать в очередь новые объекты.
+            //
+            // Размер критической секции существенно влияет на быстродействие
+            // многопоточных программ.
+            auto access = common_queue.GetAccess();
+            q = move(access.ref_to_value);
+        }
+
+        for (int item : q) {
+            if (item > 0) {
+                got.push_back(item);
+            } else {
+                return got;
+            }
         }
     }
+}
 
-    return stat;
+void TestProducerConsumer() {
+    Synchronized<deque<int>> common_queue;
 
+    auto consumer = async(Consume, ref(common_queue));
+
+    const size_t item_count = 100000;
+    for (size_t i = 1; i <= item_count; ++i) {
+        common_queue.GetAccess().ref_to_value.push_back(i);
+    }
+    common_queue.GetAccess().ref_to_value.push_back(-1);
+
+    vector<int> expected(item_count);
+    iota(begin(expected), end(expected), 1);
+    ASSERT_EQUAL(consumer.get(), expected);
 }
 
 int main() {
-    const KeyWords key_words = { "yangle", "rocks", "sucks", "all" };
-
-    stringstream ss;
-    ss << "this new yangle service really rocks\n";
-    ss << "It sucks when yangle isn't available\n";
-    ss << "10 reasons why yangle is the best IT company\n";
-    ss << "yangle rocks others suck\n";
-    ss << "Goondex really sucks, but yangle rocks. Use yangle\n";
-
-    for (const auto& [word, frequency] : ExploreKeyWords(key_words, ss).word_frequences) {
-        cout << word << " " << frequency << endl;
-    }
-
-    return 0;
+    TestRunner tr;
+    RUN_TEST(tr, TestConcurrentUpdate);
+    RUN_TEST(tr, TestProducerConsumer);
 }
