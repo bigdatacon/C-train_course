36e782513e6e54f7f0347a51081ba96bef8355e6
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-03-28 21:15:12 +0300

    Update firstcode.cpp

--- firstcode.cpp
+++ firstcode.cpp
@@ -1,50 +1,103 @@
+#include <array>
+#include <iomanip>
 #include <iostream>
+#include <optional>
+#include <sstream>
+#include <string>
 #include <vector>
 
 using namespace std;
 
-class MoneyBox {
+/*
+Используйте в классе HashableContainer одно поле типа vector<vector<T>>.
+operator== для класса VehiclePlate должен проверять равенство всех компонентов номера.
+При добавлении номера примените алгоритм find, чтобы выяснить, дублируется ли он.
+*/
+
+class VehiclePlate {
 public:
-    explicit MoneyBox(vector<int64_t> nominals)
-        : nominals_(move(nominals))
-        , counts_(nominals_.size()) {
+    VehiclePlate(char l0, char l1, int digits, char l2, int region)
+        : letters_{l0, l1, l2}
+        , digits_(digits)
+        , region_(region) {
     }
 
-    const vector<int>& GetCounts() const {
-        return counts_;
+    string ToString() const {
+        ostringstream out;
+        out << letters_[0] << letters_[1];
+        // чтобы дополнить цифровую часть номера слева нулями
+        // до трёх цифр, используем подобные манипуляторы:
+        // setfill задаёт символ для заполнения,
+        // right задаёт выравнивание по правому краю,
+        // setw задаёт минимальное желаемое количество знаков
+        out << setfill('0') << right << setw(3) << digits_;
+        out << letters_[2] << setw(2) << region_;
+
+        return out.str();
     }
 
-    void PushCoin(int64_t value) {
-        // реализуйте метод добавления купюры или монеты
-    }
-
-    void PrintCoins(ostream& out) const {
-        // реализуйте метод печати доступных средств
+    int Hash() const {
+        return digits_;
     }
 
 private:
-    const vector<int64_t> nominals_;
-    vector<int> counts_;
+    array<char, 3> letters_;
+    int digits_;
+    int region_;
 };
 
-ostream& operator<<(ostream& out, const MoneyBox& cash) {
-    cash.PrintCoins(out);
+ostream& operator<<(ostream& out, VehiclePlate plate) {
+    out << plate.ToString();
     return out;
 }
 
-int main() {
-    MoneyBox cash({10, 50, 100, 200, 500, 1000, 2000, 5000});
+template <typename T>
+class HashableContainer {
+public:
+    void Insert(T elem) {
+        int index = elem.Hash();
 
-    int times;
-    cout << "Enter number of coins you have:"s << endl;
-    cin >> times;
+        // если вектор недостаточно велик для этого индекса,
+        // то увеличим его, выделив место с запасом
+        if (index >= int(elements_.size())) {
+            elements_.resize(index * 2 + 1);
+        }
 
-    cout << "Enter all nominals:"s << endl;
-    for (int i = 0; i < times; ++i) {
-        int64_t value;
-        cin >> value;
-        cash.PushCoin(value);
+        elements_[index] = move(elem);
     }
 
-    cout << cash << endl;
+    void PrintAll(ostream& out) const {
+        for (auto& e : elements_) {
+            if (!e.has_value()) {
+                continue;
+            }
+            out << e.value() << endl;
+        }
+    }
+
+    const auto& GetVector() const {
+        return elements_;
+    }
+
+private:
+    vector<optional<T>> elements_;
+};
+
+int main() {
+    HashableContainer<VehiclePlate> plate_base;
+    plate_base.Insert({'B', 'H', 840, 'E', 99});
+    plate_base.Insert({'O', 'K', 942, 'K', 78});
+    plate_base.Insert({'O', 'K', 942, 'K', 78});
+    plate_base.Insert({'O', 'K', 942, 'K', 78});
+    plate_base.Insert({'O', 'K', 942, 'K', 78});
+    plate_base.Insert({'H', 'E', 968, 'C', 79});
+    plate_base.Insert({'T', 'A', 326, 'X', 83});
+    plate_base.Insert({'H', 'H', 831, 'P', 116});
+    plate_base.Insert({'P', 'M', 884, 'K', 23});
+    plate_base.Insert({'O', 'C', 34, 'P', 24});
+    plate_base.Insert({'M', 'Y', 831, 'M', 43});
+    plate_base.Insert({'K', 'T', 478, 'P', 49});
+    plate_base.Insert({'X', 'P', 850, 'A', 50});
+
+    plate_base.PrintAll(cout);
 }
