1a6732903c0861ef5e0833084fb7f9daf1c2aec7
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-03-28 10:07:27 +0300

    Update request_queue.h

--- fin_3/request_queue.h
+++ fin_3/request_queue.h
@@ -8,32 +8,32 @@
 
 
 
-
 class RequestQueue {
 public:
-    explicit RequestQueue(const SearchServer& search_server);
-    std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentStatus status) ;
-    std::vector<Document> AddFindRequest(const std::string& raw_query) ;
-    int GetNoResultRequests() const ;
+	explicit RequestQueue(const SearchServer& search_server);
+	std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentStatus status);
+	std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate);
+	std::vector<Document> AddFindRequest(const std::string& raw_query);
+	int GetNoResultRequests() const;
 
 
 private:
-    struct QueryResult {
-        uint64_t timestamp;
-        int results;
-    };
-    std::deque<QueryResult> requests_;
-    const SearchServer& search_server_;
-    int no_results_requests_;
-    uint64_t current_time_;
-    const static int min_in_day_ = 1440;
- 
-    void AddRequest(int results_num) ;
+	struct QueryResult {
+		uint64_t timestamp;
+		int results;
+	};
+	std::deque<QueryResult> requests_;
+	const SearchServer& search_server_;
+	int no_results_requests_;
+	uint64_t current_time_;
+	const static int min_in_day_ = 1440;
+
+	void AddRequest(int results_num);
 };
 
 template <typename DocumentPredicate>
-    std::vector<Document> AddFindRequest(const SearchServer& search_server_, const std::string& raw_query,           DocumentPredicate document_predicate) {
-    const auto result = search_server_.FindTopDocuments(raw_query, document_predicate);
-    AddRequest(result.size());
-    return result;
-    }
+std::vector<Document> RequestQueue::AddFindRequest(const SearchServer& search_server_, const std::string& raw_query, DocumentPredicate document_predicate) {
+	const auto result = search_server_.FindTopDocuments(raw_query, document_predicate);
+	AddRequest(result.size());
+	return result;
+}
