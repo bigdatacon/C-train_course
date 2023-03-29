217cad53f2d873868bf4953e320a696e75ecd31e
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-03-29 21:50:42 +0300

    Update firstcode.cpp

--- firstcode.cpp
+++ firstcode.cpp
@@ -1,53 +1,37 @@
-#include <iostream>
-#include <string>
+#include <cassert>
 
-/*
-Подсказка:
-Можете использовать хешер окружности из урока. Хешер текста — стандартный.
-Объединять хеши компонентов структуры нужно с умом, чтобы использовались разные степени числа 37 при разных компонентах: хеш первой окружности должен умножаться на 37⁴, хеш второй — на 37¹ или 37, а хеш строки — на 37⁰ или 1.
-
-Задание 2
-Реализуйте хороший хешер для структуры Dumbbell, состоящей из двух окружностей и текста.
-struct Circle {
-    double x;
-    double y;
-    double r;
+template <typename T>
+struct TreeNode {
+    T value;
+    TreeNode* left = nullptr;
+    TreeNode* right = nullptr;
 };
 
-struct Dumbbell {
-    Circle circle1;
-    Circle circle2;
-    string text;
-}; 
-Используйте для вычисления хеша степени простого числа, но учтите: никакие два параметра не должны умножаться на одну и ту же степень, иначе хеш-функция получится некачественной.
-Ограничения
-Не меняйте название класса хешера DumbbellHash. Не меняйте структуры Dumbbell и Circle.
-Как будет тестироваться ваш код
-Проверим качество вашей хеш-функции.
+template <typename T>
+void DeleteTree(TreeNode<T>* node) {
+    if (!node) {
+        return;
+    }
+    DeleteTree(node->left);
+    DeleteTree(node->right);
+    delete node;
+}
 
-
-*/
-
-
-using namespace std;
-
-struct Circle {
-    double x;
-    double y;
-    double r;
-};
-
-struct Dumbbell {
-    Circle circle1;
-    Circle circle2;
-    string text;
-};
-
-struct DumbbellHash {
-// реализуйте хешер для Dumbbell
-};
+template <typename T>
+bool CheckTreeProperty(const TreeNode<T>* node) {
+    // реализуйте функцию
+}
 
 int main() {
-    DumbbellHash hash;
-    Dumbbell dumbbell{{10, 11.5, 2.3}, {3.14, 15, -8}, "abc"s};
-    cout << "Dumbbell hash "s << hash(dumbbell);
+    using T = TreeNode<int>;
+    T* root1 = new T{6, 
+        new T{4, new T{3}, new T{5}}, new T{7}};
+    assert(CheckTreeProperty(root1));
+
+    T* root2 = new T{6, 
+        new T{4, new T{3}, new T{5}}, new T{7, new T{8}}};
+    assert(!CheckTreeProperty(root2));
+
+    DeleteTree(root1);
+    DeleteTree(root2);
+}
