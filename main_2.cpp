6c3b82af50d7164f76eb6ed519699fba7be43dbb
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-03-29 10:23:36 +0300

    Update main_2.cpp

--- main_2.cpp
+++ main_2.cpp
@@ -1,127 +1,257 @@
+#include <array>
+#include <cassert>
+#include <chrono>
+#include <iomanip>
 #include <iostream>
+#include <sstream>
 #include <string>
-#include <algorithm>
+#include <tuple>
+#include <unordered_map>
 #include <vector>
-#include <iostream>
-#include <sstream>
-
-using std::istringstream;
-using std::string;
-using std::cout;
-
+ 
 using namespace std;
-
-
-#include <functional>
-#include <iostream>
-#include <map>
-#include <set>
-#include <sstream>
-#include <string>
-#include <execution>
-#include <future>
-#include <algorithm>
-
-using namespace std;
-
-/* Подсказка Разные строки текста или наборы этих строк можно обрабатывать параллельно, а затем складывать полученные словари.
-Если собираетесь добавлять future в вектор, имейте в виду: это некопируемый тип.*/
-
-struct Stats {
-    map<string, int> word_frequences;
-
-    void operator+=(const Stats& other) {
-        for (const auto& [word, frequence] : other.word_frequences) {
-            word_frequences[word] += frequence;
-        }
-    }
+ 
+class VehiclePlate {
+private:
+	auto AsTuple() const {
+		return tie(letters_, digits_, region_);
+	}
+ 
+public:
+	bool operator==(const VehiclePlate& other) const {
+		return AsTuple() == other.AsTuple();
+	}
+ 
+	VehiclePlate(char l0, char l1, int digits, char l2, int region)
+		: letters_{ l0, l1, l2 }
+		, digits_(digits)
+		, region_(region) {
+	}
+ 
+	string ToString() const {
+		ostringstream out;
+		out << letters_[0] << letters_[1];
+ 
+		// чтобы дополнить цифровую часть номера слева нулями
+		// до трёх цифр, используем подобные манипуляторы:
+		// setfill задаёт символ для заполнения,
+		// right задаёт выравнивание по правому краю,
+		// setw задаёт минимальное желаемое количество знаков
+		out << setfill('0') << right << setw(3) << digits_;
+		out << letters_[2] << setw(2) << region_;
+ 
+		return out.str();
+	}
+ 
+private:
+	array<char, 3> letters_;
+	int digits_;
+	int region_;
 };
-
-using KeyWords = set<string, less<>>;
-
-vector<string> SplitIntoWords(const string& text) {
-    vector<string> words;
-    string word;
-    for (const char c : text) {
-        if (c == ' ') {
-            if (!word.empty()) {
-                words.push_back(word);
-                word.clear();
-            }
-        }
-        else {
-            if (isprint(c)) {
-                word += c;
-            }
-        }
-    }
-    if (!word.empty()) {
-        words.push_back(word);
-    }
-
-    return words;
+ 
+ostream& operator<<(ostream& out, VehiclePlate plate) {
+	out << plate.ToString();
+	return out;
 }
-
-
-Stats SplittoStat(const KeyWords& key_words, const string& s) {
-    Stats result;
-    vector<string> words = SplitIntoWords(s);
-
-    for (string word : words) {
-        if (key_words.count(word)) {
-            result.word_frequences[word] += 1;
-        }
-    }
-    return result;
-}
-
-
-
-
-
-std::string safe_getline(istream& input) {
-    std::string line;
-    getline(input, line);
-    return move(line);
-}
-
-/* Подсказка Разные строки текста или наборы этих строк можно обрабатывать параллельно, а затем складывать полученные словари.
-Если собираетесь добавлять future в вектор, имейте в виду: это некопируемый тип.*/
-Stats ExploreKeyWords(const KeyWords& key_words, istream& input) {
-
-    Stats stat;
-    vector<future<Stats>> all_words;
-    std::string line;
-    while (!(line = safe_getline(input)).empty())
-
-    { // читаем строку из входного потока, пока не достигнем конца
-        //std::string line = safe_getline(input);
-        cout << "line before : " << line << endl;
-
-        all_words.push_back(async(SplittoStat, cref(key_words),  line));
-    }
-
-
-    for_each(execution::par, all_words.begin(), all_words.end(),
-        [&stat](auto& item) {stat += item.get(); });
-
-    return stat;
-
-}
-
-int main() {
-    const KeyWords key_words = { "yangle", "rocks", "sucks", "all" };
-
-    stringstream ss;
-    ss << "this new yangle service really rocks\n";
-    ss << "It sucks when yangle isn't available\n";
-    ss << "10 reasons why yangle is the best IT company\n";
-    ss << "yangle rocks others suck\n";
-    ss << "Goondex really sucks, but yangle rocks. Use yangle\n";
-
-    for (const auto& [word, frequency] : ExploreKeyWords(key_words, ss).word_frequences) {
-        cout << word << " " << frequency << endl;
-    }
-
-    return 0;
-}
+ 
+// возьмите реализацию хешера из прошлого задания
+class VehiclePlateHasher {
+public:
+	size_t operator()(const VehiclePlate& plate) const {
+		// измените эту функцию, чтобы она учитывала все данные номера
+		// рекомендуется использовать метод ToString() и существующий 
+		// //return static_cast<size_t>(plate.Hash());
+		// класс hash<string>
+		return hasher_(plate.ToString());
+ 
+	}
+private:
+	hash<string> hasher_;
+};
+ 
+// выбросьте это исключение в случае ошибки парковки
+struct ParkingException {};
+ 
+template <typename Clock>
+class Parking {
+	// при обращении к типу внутри шаблонного параметра мы обязаны использовать 
+	// typename; чтобы этого избежать, объявим псевдонимы для нужных типов
+	using Duration = typename Clock::duration;
+	using TimePoint = typename Clock::time_point;
+ 
+public:
+	Parking(int cost_per_second) : cost_per_second_(cost_per_second) {}
+ 
+	// запарковать машину с указанным номером
+	void Park(VehiclePlate car) {
+		// место для вашей реализации
+ 
+		if (now_parked_.count(car)) {
+			throw ParkingException();
+		}
+		else {
+			now_parked_[car] = Clock::now();
+ 
+		}
+	}
+ 
+	// забрать машину с указанным номером
+	void Withdraw(const VehiclePlate& car) {
+		// место для вашей реализации
+		auto it = now_parked_.find(car);
+		if (it != now_parked_.end()) {
+			auto end_time = Clock::now();
+			auto duration_time = end_time - now_parked_.at(car);
+			complete_parks_[car] += duration_time; // тут не в секундах 
+			now_parked_.erase(car); // удаление элемента по ключу
+		}
+	}
+ 
+	// получить счёт за конкретный автомобиль
+	int64_t GetCurrentBill(const VehiclePlate& car) const {
+		// место для вашей реализации
+		auto it = now_parked_.find(car);
+		if (it != now_parked_.end()) {
+ 
+			// Преобразование продолжительности времени в секунды
+			auto end_time = Clock::now();
+			auto duration_time = end_time - now_parked_.at(car);
+			if (complete_parks_.count(car)) {
+				duration_time += complete_parks_[car];
+			}
+			auto seconds = std::chrono::duration_cast<std::chrono::seconds>(duration_time);
+			// Получение количества секунд
+			auto seconds_count = seconds.count();
+ 
+			int64_t money = cost_per_second_ * (seconds_count);
+			return money;
+ 
+		}
+		else {
+			auto it = complete_parks_.find(car);
+			if (it != complete_parks_.end()) {
+				auto seconds = std::chrono::duration_cast<std::chrono::seconds>(complete_parks_.at(car));
+				// Получение количества секунд
+				auto seconds_count = seconds.count();
+				int64_t money = cost_per_second_ * (seconds_count);
+				return money;
+			}
+			else {
+ 
+				return 0;
+			}
+		}
+	}
+ 
+	// завершить расчётный период
+	// те машины, которые находятся на парковке на данный момент, должны 
+	// остаться на парковке, но отсчёт времени для них начинается с нуля
+	unordered_map<VehiclePlate, int64_t, VehiclePlateHasher> EndPeriodAndGetBills() {
+		// место для вашей реализации
+		for (auto& pair : now_parked_) {
+			complete_parks_[pair.first] += (Clock::now() - pair.second);
+			now_parked_[pair.first] = Clock::now();
+		}
+		return complete_parks_;
+	}
+ 
+		// не меняйте этот метод
+		auto& GetNowParked() const {
+			return now_parked_;
+		}
+ 
+		// не меняйте этот метод
+		auto& GetCompleteParks() const {
+			return complete_parks_;
+		}
+ 
+private:
+	int cost_per_second_;
+	unordered_map<VehiclePlate, TimePoint, VehiclePlateHasher> now_parked_;
+	unordered_map<VehiclePlate, Duration, VehiclePlateHasher> complete_parks_;
+	};
+ 
+	// эти часы удобно использовать для тестирования
+	// они покажут столько времени, сколько вы задали явно
+	class TestClock {
+	public:
+		using time_point = chrono::system_clock::time_point;
+		using duration = chrono::system_clock::duration;
+ 
+		static void SetNow(int seconds) {
+			current_time_ = seconds;
+		}
+ 
+		static time_point now() {
+			return start_point_ + chrono::seconds(current_time_);
+		}
+ 
+	private:
+		inline static time_point start_point_ = chrono::system_clock::now();
+		inline static int current_time_ = 0;
+	};
+ 
+	int main() {
+		Parking<TestClock> parking(10);
+ 
+		TestClock::SetNow(10);
+		parking.Park({ 'A', 'A', 111, 'A', 99 });
+ 
+		TestClock::SetNow(20);
+		parking.Withdraw({ 'A', 'A', 111, 'A', 99 });
+		parking.Park({ 'B', 'B', 222, 'B', 99 });
+ 
+		TestClock::SetNow(40);
+		assert(parking.GetCurrentBill({ 'A', 'A', 111, 'A', 99 }) == 100);
+		assert(parking.GetCurrentBill({ 'B', 'B', 222, 'B', 99 }) == 200);
+		parking.Park({ 'A', 'A', 111, 'A', 99 });
+ 
+		TestClock::SetNow(50);
+		assert(parking.GetCurrentBill({ 'A', 'A', 111, 'A', 99 }) == 200);
+		assert(parking.GetCurrentBill({ 'B', 'B', 222, 'B', 99 }) == 300);
+		assert(parking.GetCurrentBill({ 'C', 'C', 333, 'C', 99 }) == 0);
+		parking.Withdraw({ 'B', 'B', 222, 'B', 99 });
+ 
+		TestClock::SetNow(70);
+		{
+			// проверим счёт
+			auto bill = parking.EndPeriodAndGetBills();
+ 
+			// так как внутри макроса используется запятая,
+			// нужно заключить его аргумент в дополнительные скобки
+			assert((bill
+				== unordered_map<VehiclePlate, int64_t, VehiclePlateHasher>{
+					{ {'A', 'A', 111, 'A', 99}, 400},
+					{ {'B', 'B', 222, 'B', 99}, 300 },
+			}));
+		}
+ 
+		TestClock::SetNow(80);
+		{
+			// проверим счёт
+			auto bill = parking.EndPeriodAndGetBills();
+ 
+			// так как внутри макроса используется запятая,
+			// нужно заключить его аргумент в дополнительные скобки
+			assert((bill
+				== unordered_map<VehiclePlate, int64_t, VehiclePlateHasher>{
+					{ {'A', 'A', 111, 'A', 99}, 100},
+			}));
+		}
+ 
+		try {
+			parking.Park({ 'A', 'A', 111, 'A', 99 });
+			assert(false);
+		}
+		catch (ParkingException) {
+		}
+ 
+		try {
+			parking.Withdraw({ 'B', 'B', 222, 'B', 99 });
+			assert(false);
+		}
+		catch (ParkingException) {
+		}
+ 
+		cout << "Success!"s << endl;
+	}
