646aed2fdf0e911ddf0455b3ef44e618443651d2
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-03-30 13:32:27 +0300

    Update main_2.cpp

--- main_2.cpp
+++ main_2.cpp
@@ -62,38 +62,39 @@ int main() {
 
 
 
-	T* root5 = new T{ 26,
-		new T{22, new T{18, new T{14, new T{8}, new T{16}}
-		
-		, new T{19}}
-		
-		, new T{24}
-	}
-	};
+
+	T* root5 = new T{ 26 , new T{24, new T{22, new T{20}, nullptr}, nullptr}, nullptr }; // только левые узлы 
 	assert(CheckTreeProperty(root5));
 
 
 
-	T* root6 = new T{ 26,
-		new T{28, new T{30, new T{40, new T{46 , new T {60}}
+	T* root6 = new T{ 26 , nullptr, new T{30, nullptr , new T{50 , nullptr , new T {60} }} };  // только првые узлы
+	assert(CheckTreeProperty(root6));
 
 
-	}
-	}
-	} };
-
-	assert(!CheckTreeProperty(root6));
-
-	T* root7 = new T{ 26,
-	new T{24, new T{23, new T{22, new T{21 , new T {20 , new T {19}}}
 
 
-}
-}
-} };
+	T* root6_5 = new T{100,
+		new T { 80, new T{60}, new T{350, new T{40}, new  T {550}} } , new T {800} };
+	assert(CheckTreeProperty(root6_5));
 
-	assert(CheckTreeProperty(root7));
 
+	T* root7 = new T{ 100,
+		new T { 80, new T{60}, new T{600, new T{40}, new  T {550}} } , new T {800} };
+	assert(!CheckTreeProperty(root7));
+
+	T* root7_5 = new T{ 100,
+	new T { 80, new T{60}, new T{190, new T{40}, new  T {550}} } , new T {800} }; // значение в правой и левой ветке больше коря 
+	assert(CheckTreeProperty(root7_5));
+
+
+	T* root8 = new T{ 100,
+		new T{80, new T{60, new T {40} , new T {600}}, new T{700}} , new T {800, new T{100} , new T {1500}} }; //значение в правой ветке равное корню 
+	assert(CheckTreeProperty(root8));
+
+	T* root9 = new T{ 100,
+	new T{80, new T{60, new T {40} , new T {100}}, new T{700}} , new T {800, new T{100} , new T {1500}} }; //значение в левой ветке равное корню 
+		assert(CheckTreeProperty(root9));
 
 
 	DeleteTree(root1);
@@ -103,5 +104,9 @@ int main() {
 	DeleteTree(root5);
 	DeleteTree(root6);
 	DeleteTree(root7);
+	DeleteTree(root7_5);
+	DeleteTree(root6_5);
+	DeleteTree(root8);
+	DeleteTree(root9);
 
 }
