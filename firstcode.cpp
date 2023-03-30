0c0e7ea35699cdc5fb1c5ac03472cb7e844d1448
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-03-30 22:05:12 +0300

    Update firstcode.cpp

--- firstcode.cpp
+++ firstcode.cpp
@@ -1,13 +1,17 @@
+#include <iostream>
 #include <cassert>
 
-template <typename T>
+using namespace std;
+
+template <class T>
 struct TreeNode {
     T value;
+    TreeNode* parent = nullptr;
     TreeNode* left = nullptr;
     TreeNode* right = nullptr;
 };
 
-template <typename T>
+template <class T>
 void DeleteTree(TreeNode<T>* node) {
     if (!node) {
         return;
@@ -17,21 +21,71 @@ void DeleteTree(TreeNode<T>* node) {
     delete node;
 }
 
-template <typename T>
-bool CheckTreeProperty(const TreeNode<T>* node) {
-    // реализуйте функцию
+template <class T>
+void PrintTree(const TreeNode<T>* root, ostream& out = cout) {
+    out << " ( "s;
+    out << root->value;
+    if (root->left || root->right) {
+        if (root->left) {
+            PrintTree(root->left, out);
+        } else {
+            out << "*"s;
+        }
+        if (root->right) {
+            PrintTree(root->right, out);
+        } else {
+            out << "*"s;
+        }
+    }
+    out << " ) "s;
+}
+
+template <class T>
+ostream& operator << (ostream& out, const TreeNode<T>* node) {
+    PrintTree(node, out);
+    return out;
+}
+/*
+Если у узла есть правое поддерево, ищите следующий в нём
+Вам пригодится функция begin для поиска следующего в правом поддереве
+Если справа ничего нет, идите в родителя. Родитель будет следующим, если вы пришли в него из левого поддерева. А если из правого, то отправляйтесь ещё выше
+*/
+template <class T>
+TreeNode<T>* begin(TreeNode<T>* node) {
+    // место для вашей реализации
+}
+
+template <class T>
+TreeNode<T>* next(TreeNode<T>* node) {
+    // место для вашей реализации
+}
+
+// функция создаёт новый узел с заданным значением и потомками
+TreeNode<int>* N(int val, TreeNode<int>* left = nullptr, TreeNode<int>* right = nullptr) {
+    auto res = new TreeNode<int>{val, nullptr, left, right};
+    if (left) {
+        left->parent = res;
+    }
+    if (right) {
+        right->parent = res;
+    }
+
+    return res;
 }
 
 int main() {
     using T = TreeNode<int>;
-    T* root1 = new T{6, 
-        new T{4, new T{3}, new T{5}}, new T{7}};
-    assert(CheckTreeProperty(root1));
 
-    T* root2 = new T{6, 
-        new T{4, new T{3}, new T{5}}, new T{7, new T{8}}};
-    assert(!CheckTreeProperty(root2));
+    T* root = N(6, N(4, N(3), N(5)), N(8, N(7)));
+    cout << root << endl;
 
-    DeleteTree(root1);
-    DeleteTree(root2);
+    T* iter = begin(root);
+
+    while (iter) {
+        cout << iter->value << " "s;
+        iter = next(iter);
+    }
+    cout << endl;
+
+    DeleteTree(root);
 }
