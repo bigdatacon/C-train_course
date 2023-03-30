e757e43f588c945cda91173123bf4f38865104a8
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-03-30 08:23:12 +0300

    Update main_2.cpp

--- main_2.cpp
+++ main_2.cpp
@@ -13,40 +13,44 @@ struct TreeNode {
 };
 
 
-	template <typename T>
-	void DeleteTree(TreeNode<T>* node) {
-		if (!node) {
-			return;
-		}
-		DeleteTree(node->left);
-		DeleteTree(node->right);
-		delete node;
+template <typename T>
+void DeleteTree(TreeNode<T>* node) {
+	if (!node) {
+		return;
 	}
+	DeleteTree(node->left);
+	DeleteTree(node->right);
+	delete node;
+}
 
-	template <typename T>
-	bool CheckTreeProperty(const TreeNode<T>* node, const T* min, const T* max) {
-		return (! (node->left) || node->value > *min) && (! (node->right) || node->value < *max);
+template <typename T>
+bool CheckTreeProperty(const TreeNode<T>* node, const T* min, const T* max) {
+	return (!(node->left) || node->value > *min) && (!(node->right) || node->value < *max);
 
-	}
+}
 
 
-	template <typename T>
-	bool CheckTreeProperty(const TreeNode<T>* node) {
-			return	 CheckTreeProperty(node, &(node->left->value), &(node->right->value))  &&
-				 CheckTreeProperty(node->left) && CheckTreeProperty(node->right) ;
+template <typename T>
+bool CheckTreeProperty(const TreeNode<T>* node) {
 
-	}
+	return	!node || (
+		CheckTreeProperty(node, &(node->left->value), &(node->right->value)) &&
+		CheckTreeProperty(node->left) && CheckTreeProperty(node->right)
+		)
+		;
 
-	int main() {
-		using T = TreeNode<int>;
-		T* root1 = new T{ 6,
-			new T{4, new T{3}, new T{5}}, new T{7} };
-		assert(CheckTreeProperty(root1));
+}
 
-		T* root2 = new T{ 6,
-			new T{4, new T{3}, new T{5}}, new T{7, new T{8}} };
-		assert(!CheckTreeProperty(root2));
+int main() {
+	using T = TreeNode<int>;
+	T* root1 = new T{ 6,
+		new T{4, new T{3}, new T{5}}, new T{7} };
+	assert(CheckTreeProperty(root1));
 
-		DeleteTree(root1);
-		DeleteTree(root2);
-	}
+	T* root2 = new T{ 6,
+		new T{4, new T{3}, new T{5}}, new T{7, new T{8}} };
+	assert(!CheckTreeProperty(root2));
+
+	DeleteTree(root1);
+	DeleteTree(root2);
+}
