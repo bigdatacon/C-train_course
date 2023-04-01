259e7afa248d0daa551df4d0005df54fbb627054
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-04-01 18:45:34 +0300

    Update ini.h

--- Ini_half/ini.h
+++ Ini_half/ini.h
@@ -5,23 +5,24 @@
 
 // поместите все имена библиотеки INI в пространство имён ini
 namespace ini {
-using Section = std::unordered_map<std::string, std::string>;
+    using Section = std::unordered_map<std::string, std::string>;
 
-class Document {
-public:
-    // реализация методов должна быть в файле ini.cpp
-    Section& AddSection(std::string name);
-    const Section& GetSection(const std::string& name) const;
-    
-    std::size_t GetSectionCount() const;
-    
-    Section GetSectionSimple(const std::string& name) const;
-    
+    class Document {
+    public:
+        // реализация методов должна быть в файле ini.cpp
+        Section& AddSection(std::string name);
+        const Section& GetSection(const std::string& name) const;
 
-private:
-    std::unordered_map<std::string, Section> sections_;
-};
+        std::size_t GetSectionCount() const;
 
-// определение этой функции должно быть в файле ini.cpp
-Document Load(std::istream& input);
+        Section GetSectionSimple(const std::string& name) const;
+        void AddData( std::string& name,  std::string& key,  std::string& value) ;
+
+
+    private:
+        std::unordered_map<std::string, Section> sections_;
+    };
+
+    // определение этой функции должно быть в файле ini.cpp
+    Document Load(std::istream& input);
 }
