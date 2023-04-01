166ac3bd5eaa1b56c852d7b6a004582ad2c26474
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-04-01 17:46:13 +0300

    Add files via upload

---
+++ Ini_half/ini.cpp
@@ -0,0 +1,60 @@
+#include "ini.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <unordered_map>
+using namespace std;
+
+
+namespace ini {
+    //using Section = std::unordered_map<std::string, std::string>;
+    
+    // методы Document 
+    //class Document {
+        //public:
+        Section& Document::AddSection(std::string name) {
+            return sections_[std::move(name)];
+        }
+
+        const Section& Document::GetSection(const std::string& name) const {
+            static const Section empty_section;
+            auto it = sections_.find(name);
+            if (it == sections_.end()) {
+                return empty_section;
+            }
+            return it->second;
+        }
+
+        std::size_t Document::GetSectionCount() const {
+            return sections_.size();
+        }
+
+        //private:
+        //std::unordered_map<std::string, Section> sections_;
+    //};
+
+    Document Load(std::istream& input) {
+        Document doc;
+        std::string line;
+        std::string current_section;
+
+        while (std::getline(input, line)) {
+            if (line.empty() || line[0] == '#') {
+                continue;
+            }
+
+            if (line[0] == '[' && line[line.size() - 1] == ']') {
+                current_section = line.substr(1, line.size() - 2);
+                doc.AddSection(current_section);
+            }
+            else {
+                auto delimiter_pos = line.find('=');
+                auto name = line.substr(0, delimiter_pos);
+                auto value = line.substr(delimiter_pos + 1);
+                doc.GetSection(current_section).emplace(name, value);
+            }
+        }
+
+        return doc;
+    }
+}
