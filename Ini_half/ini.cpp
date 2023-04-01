c1bccf06ae3d83e1a3143ddf393ceb716fd61cf1
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-04-01 19:08:27 +0300

    Update ini.cpp

--- Ini_half/ini.cpp
+++ Ini_half/ini.cpp
@@ -59,6 +59,23 @@ namespace ini {
         "guest1_name = Ivan Durak\n"
         "guest2_name =  Vasilisa Premudraya\n"
         "[guest black list]" };*/
+
+    std::string RemSpace(std::string line) {
+        size_t lastChar = line.find_last_not_of(" \t\n\v\f\r");
+        if (lastChar != std::string::npos) {
+            line.erase(lastChar + 1);
+        }
+        return line;
+    }
+
+
+    std::string EraseFirstSpace(std::string line) {
+        if (!line.empty() && line[0] == ' ') {
+            line.erase(0, 1);
+        }
+        return line;
+    }
+
     Document Load(std::istream& input) {
         Document doc;
         std::string line;
@@ -69,6 +86,10 @@ namespace ini {
                 continue;
             }
 
+
+            line = RemSpace(line);
+
+
             if (line[0] == '[' && line[line.size() - 1] == ']') {
                 current_section = line.substr(1, line.size() - 2);
                 doc.AddSection(current_section);
@@ -77,6 +98,8 @@ namespace ini {
                 auto delimiter_pos = line.find('=');
                 auto name = line.substr(0, delimiter_pos);
                 auto value = line.substr(delimiter_pos + 1);
+                name = RemSpace(name);
+                value = EraseFirstSpace(value);
                 //doc.GetSectionSimple(current_section).emplace(name, value
                 doc.AddData(current_section, name, value);
             }
