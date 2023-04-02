17f96c578b54a1c53f86b88e5ec7dde8d9e6497a
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-04-02 17:09:14 +0300

    Update main.cpp

--- Ini_half/main.cpp
+++ Ini_half/main.cpp
@@ -1,51 +1,121 @@
-#include <cassert>
-#include <sstream>
-
-// в этой задаче ситуация обратная:
-// меняйте только файлы ini.cpp, ini.h
-// а main.cpp трогать не нужно
 #include "ini.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <unordered_map>
+#include <algorithm>
 
 using namespace std;
 
-int main() {
-    std::istringstream input{
+
+namespace ini {
+    //using Section = std::unordered_map<std::string, std::string>;
+
+    // методы Document 
+    //class Document {
+        //public:
+    Section& Document::AddSection(std::string name) {
+        return sections_[std::move(name)];
+    }
+
+    const Section& Document::GetSection(const std::string& name) const {
+        static const Section empty_section;
+        auto it = sections_.find(name);
+        if (it == sections_.end()) {
+            return empty_section;
+        }
+        return it->second;
+    }
+
+    std::size_t Document::GetSectionCount() const {
+        return sections_.size();
+    }
+
+    Section& Document::GetSectionSimple(const std::string& name) {
+        static Section empty_section;
+        empty_section.clear();
+        auto it = sections_.find(name);
+        if (it == sections_.end()) {
+            return empty_section;
+        }
+        return it->second;
+    }
+
+
+
+    /* void Document::AddData(const std::string& name, const std::string& key,  const std::string& value)  {
+         sections_.at(name)[key] = value;
+     }*/
+
+
+
+    std::string RemSpace(std::string str) {
+
+        str.erase(std::find_if(str.rbegin(), str.rend(), [](int ch) {
+            return !std::isspace(ch);
+            }).base(), str.end());
+        return str;
+    }
+
+
+    std::string EraseFirstSpace(std::string line) {
+        while (!line.empty() && line[0] == ' ') {
+            line.erase(0, 1);
+        }
+
+        return line;
+    }
+
+    Document Load(std::istream& input) {
+        Document doc;
+        std::string line;
+        std::string raw_val;
+        std::string name;
+        std::string value;
+
+        std::string current_section;
+        /*
         "   [first]\n"
         " a=b\n"
         "    b=a"
-        
-        
-        /*"[vegetables]\n"
-        "potatoes=10\n"
-        "onions=1 \n"
-        "\n"
-        "cucumbers=12\n"
-        "\n"
-        "[guests] \n"
-        "guest1_name = Ivan Durak\n"
-        "guest2_name =  Vasilisa Premudraya\n"
-        "[guest black list]"*/
-        
-        
-        };
-    ini::Document doc = ini::Load(input);
-    
-     assert(doc.GetSectionCount() == 1);
-    
-    
-    /*assert(doc.GetSectionCount() == 3);
-    
-    assert((doc.GetSection("vegetables"s)
-            == ini::Section{
-                {"potatoes"s, "10"s},
-                {"onions"s, "1"s},
-                {"cucumbers"s, "12"s},
-            }));
-    assert((doc.GetSection("guests"s)
-            == ini::Section{{"guest1_name"s, "Ivan Durak"s}, {"guest2_name"s, "Vasilisa Premudraya"s}}));
-    assert((doc.GetSection("dragons"s) == ini::Section{}));
-    assert((doc.GetSection("guest black list"s) == ini::Section{}));
+        */
+        while (std::getline(input, line)) {
+            if (line.empty() || line[0] == '#') {
+                continue;
+            }
 
-    doc.AddSection("pets"s) = ini::Section{{"nasty"s, "rat"s}};
-    assert(doc.GetSectionCount() == 4);*/
+            size_t pos_f_nospase = line.find_first_not_of(" ");
+            size_t pos_l_nospase = line.find_last_not_of(" ");
+
+            //line = RemSpace(line);
+            //line = EraseFirstSpace(line);
+            if (line[0] == '[' && line[line.size() - 1] == ']') {
+                current_section = line.substr(1, line.size() - 2);
+                doc.AddSection(current_section);
+            }
+            else {
+                std::string excludedChars = " \t\r\n\f\v!@#$%^&*()-_=+[{]}\\|;:'   \",<.>/?";
+                std::string inludedChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_";
+                size_t pos = line.find_first_not_of(inludedChars);
+                if (pos != string::npos) {
+                    auto name = line.substr(0, pos); // обрезаю строку для ключа 
+                    auto raw_val = line.substr(pos); // обрезаю строку со значением до конца
+                    size_t pos_2 = raw_val.find_first_not_of(excludedChars);
+                    size_t pos_3 = raw_val.find_last_not_of(" \t\r\n\f\v");  //size_t pos_3 = raw_val.find_last_not_of(excludedChars);
+                    if (pos_2 != string::npos && pos_3 != string::npos) {
+
+
+                        auto value = raw_val.substr(pos_2, pos_3);
+                        doc.GetSectionSimple(current_section).emplace(name, value);
+                    }
+                }
+
+
+
+
+            }
+        }
+
+        return doc;
+    }
 }
