e78ab9abb99c95243b745f1f18e5ba9f3df3c192
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-04-02 21:51:46 +0300

    Add files via upload

---
+++ name_space_2/libstat.cpp
@@ -0,0 +1,68 @@
+#include "libstat.h"
+
+namespace statistics {
+
+namespace aggregations {
+
+void Sum/*Aggregation*/::PutValue(double value) {
+    sum_ += value;
+}
+
+optional<double> Sum/*Aggregation*/::Get() const {
+    return sum_;
+}
+
+void /*Aggregate*/Max/*imum*/::PutValue(double value) {
+    cur_max_ = max(value, cur_max_.value_or(value));
+}
+
+optional<double> /*Aggregate*/Max/*imum*/::Get() const {
+    return cur_max_;
+}
+
+void /*AggregatorAverage*/ Mean::PutValue(double value) {
+    sum_.PutValue(value);
+    ++count_;
+}
+
+optional<double> /*AggregatorAverage*/ Mean::Get() const {
+    auto val = sum_.Get();
+    if (!val || count_ == 0) {
+        return nullopt;
+    }
+
+    return *val / count_;
+}
+
+void /*AggregStd*/ StandardDeviation::PutValue(double value) {
+    sum_.PutValue(value);
+    sum_sq_.PutValue(value * value);
+    ++count_;
+}
+
+optional<double> /*AggregStd*/ StandardDeviation::Get() const {
+    auto val = sum_.Get();
+    auto val2 = sum_sq_.Get();
+
+    if (!val || !val2 || count_ < 2) {
+        return nullopt;
+    }
+
+    return ::std::sqrt((*val2 - *val * *val / count_) / count_);
+}
+
+void Mode::PutValue(double value) {
+    const size_t new_count = ++counts_[round(value)];
+
+    if (new_count > cur_count_) {
+        cur_max_ = value;
+        cur_count_ = new_count;
+    }
+}
+
+optional<double> Mode::Get() const {
+    return cur_max_;
+}
+    
+} // закрыт aggregations
+} // закрыт statistics
---
+++ name_space_2/libstat.h
@@ -0,0 +1,127 @@
+#pragma once
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <optional>
+#include <string>
+#include <string_view>
+#include <unordered_map>
+
+//using namespace std; //6.уберите using namespace std; из h-файлов,
+
+
+namespace statistics {
+
+namespace aggregations {
+
+class /*SumAggregation*/ Sum {
+public:
+    void PutValue(double value);
+    optional<double> Get() const;
+
+    static string_view GetValueName() {
+        return "sum"sv;
+    }
+
+private:
+    double sum_ = 0;
+};
+
+class /*AggregateMaximum*/  Max{
+public:
+    void PutValue(double value);
+    optional<double> Get() const;
+
+    static string_view GetValueName() {
+        return "max"sv;
+    }
+
+private:
+    optional<double> cur_max_;
+};
+
+class /*AggregatorAverage*/ Mean{
+public:
+    void PutValue(double value);
+    optional<double> Get() const;
+
+    static string_view GetValueName() {
+        return "mean"sv;
+    }
+
+private:
+    ::SumAggregation sum_;
+    size_t count_ = 0;
+};
+
+class /*AggregStd*/ StandardDeviation {
+public:
+    void PutValue(double value);
+    optional<double> Get() const;
+
+    static string_view GetValueName() {
+        return "standard deviation"sv;
+    }
+
+private:
+    ::SumAggregation sum_;
+    ::SumAggregation sum_sq_;
+    size_t count_ = 0;
+};
+
+class Mode {
+public:
+    void PutValue(double value);
+    optional<double> Get() const;
+
+    static string_view GetValueName() {
+        return "mode"sv;
+    }
+
+private:
+    ::unordered_map<double, size_t> counts_;
+    optional<double> cur_max_;
+    size_t cur_count_ = 0;
+};
+
+    
+} // закрыт блок aggregation 
+
+namespace tests {
+    //В пространстве имён statistics::tests используйте using namespace aggregations, чтобы не прибегать каждый раз к квалификации. При этом могут возникнуть конфликты
+using namespace aggregations    
+    
+
+//переименуйте тесты, убрав общий префикс TestStat, но оставив Aggreg,    
+void /*TestStat*/ AggregSum();
+void /*TestStat*/ AggregMax();
+void /*TestStat*/ AggregMean();
+void /*TestStat*/ AggregStandardDeviation();
+void /*TestStat*/ AggregMode();
+void /*TestStat*/ AggregPrinter();   
+
+} // закрыт блок tests
+
+template <typename Aggreg>
+class AggregPrinter {
+public:
+    void PutValue(double value) {
+        inner_.PutValue(value);
+    }
+
+    void Print(ostream& out) const {
+        auto val = inner_.Get();
+        out << inner_.GetValueName() << " is "sv;
+        if (val) {
+            out << *val;
+        } else {
+            out << "undefined"sv;
+        }
+        out << endl;
+    }
+
+private:
+    Aggreg inner_;
+};
+
+} // закрыт statistics
