f4254458702fcafb2f29cd3d55c191d18035e201
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-04-03 15:45:43 +0300

    Update main.cpp

--- name_space_2/main.cpp
+++ name_space_2/main.cpp
@@ -4,12 +4,12 @@
 using namespace std;
 
 int main() {
-    TestStatAggregSum();
-    TestStatAggregMax();
-    TestStatAggregMean();
-    TestStatAggregStandardDeviation();
-    TestStatAggregMode();
-    TestStatAggregPrinter();
+    /*TestStat*/statistics::tests::AggregSum();
+    /*TestStat*/statistics::tests::AggregMax();
+    /*TestStat*/statistics::tests::AggregMean();
+    /*TestStat*/statistics::tests::AggregStandardDeviation();
+    /*TestStat*/statistics::tests::AggregMode();
+    /*TestStat*/statistics::tests::AggregPrinter();
 
     cout << "Test passed!"sv << endl;
 }
