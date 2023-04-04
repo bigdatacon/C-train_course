1e10c04654b7978fcce59419167b218f7f8c13c6
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-04-04 20:01:05 +0300

    Update input_reader.h

--- spr_10_1_3_init/spr_10_1_3_v1/input_reader.h
+++ spr_10_1_3_init/spr_10_1_3_v1/input_reader.h
@@ -111,37 +111,39 @@ istream& operator>>(istream& is, Query& q) {
     return is;
 }
 
-int main() {
-    deque<pair <string, string>> deq_; // тут перечень запросов на вывод 
-
+class Input_reader {
+public:
+    istream& operator>>(istream& is, Query& q, TransportCatalogue tc) {
     int query_count;
     Query q;
     cin >> query_count;
     for (int i = 0; i < query_count; ++i) {
         cin >> q;
         switch (q.type) {
-        case QueryType::Bus:
-            continue;
-        case QueryType::Stop:
-            continue;
-        }
-
-        int query_count2;
-        cin >> query_count2;
-
-        for (int i = 0; i < query_count2; ++i) {
-            string line;
-            // После заполнения базы читаю и записываю запросы на вывод 
-            while (std::getline(std::cin, line)) {
-                auto space_colon = line.find_first_not_of(" ");  // : отделяет название запроса
-                string type_req = line.substr(0, space_colon);
-                string number = line.substr(space_colon);
-                deq_.push_back(std::make_pair(type_req, number));
-
-            }
-        }
-
-
+            case QueryType::Bus:
+                tc.AddBus(q);
+                break;
+            case QueryType::Stop:
+                tc.AddStop(q);
+                break;
+         // заполняю класс сначала всеми данными 
     }
-};
+        
+    int query_count2;
+    cin >> query_count2;
 
+    for (int i = 0; i < query_count2; ++i) {
+    string line;
+    // После заполнения базы читаю и записываю запросы на вывод 
+    while (std::getline(std::cin, line)) {
+        auto space_colon =  line.find_first_not_of(" ");  // : отделяет название запроса
+        string type_req   = line.substr(0, space_colon);
+        string number   = line.substr(space_colon);
+        deq_.push_back(std::make_pair(type_req, number));
+        
+    }     
+    }
+
+private:
+    deque<pair <string, string>> deq_; // тут перечень запросов на вывод 
+};
