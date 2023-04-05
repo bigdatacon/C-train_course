1fba923282372351e315b53f6ed9afbe04e0eb0d
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-04-05 21:37:25 +0300

    Add files via upload

---
+++ spr_10_1_3_init/10_1_v2/main.cpp
@@ -0,0 +1,17 @@
+﻿// напишите решение с нуля
+// код сохраните в свой git-репозиторий
+#include "input_reader.h"
+#include "transport_catalogue.h"
+
+
+
+int main()
+{
+    InputReader reader(std::cin);
+    int count = reader.getNumUpdateQueries();
+    std::cout << count << std::endl;
+    for (int i = 0; i < count; i++) {
+        UpdateQuery q = reader.getUpdateQuery();
+        std::cout << q.type << std::endl;
+    }
+}
