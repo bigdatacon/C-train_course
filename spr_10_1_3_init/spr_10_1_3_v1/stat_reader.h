2bcfd0793ae32eb7bfb479e8754ecbd989929c70
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-04-05 03:22:42 +0300

    Update stat_reader.h

--- spr_10_1_3_init/spr_10_1_3_v1/stat_reader.h
+++ spr_10_1_3_init/spr_10_1_3_v1/stat_reader.h
@@ -1,16 +1,23 @@
 // напишите решение с нуля
 // код сохраните в свой git-репозиторий
+#include <cassert>
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+#include <set>
+#include <algorithm>
+#include <deque>
+#include <algorithm>
+#include <fstream>
+#include <sstream>
+#include <unordered_map>
+#include <utility>
+#include <math.h>
+#include <regex>
+using namespace std;
 
 
-//AllBusInfoBusResponse
-/*
-struct AllBusInfoBusResponse {
-    // Наполните полями эту структуру
-	    int stops;
-        int uniq_stops;
-        int r_length ;
-};
-*/
 ostream& operator<<(ostream& os,  const AllBusInfoBusResponse& r) {
     // Реализуйте эту функцию
     //cout << "BusesForStopResponse"s << endl;
@@ -28,20 +35,16 @@ public:
     istream& operator>>(istream& is, Query& q, TransportCatalogue tc) {
     int query_count;
     cin >> query_count;
-
-    for (int i = 0; i < query_count; ++i) {
-    string line;
-    // После заполнения базы читаю и записываю запросы на вывод 
-    while (std::getline(std::cin, line)) {
-        auto space_colon =  line.find_first_not_of(" ");  // : отделяет название запроса
-        string type_req   = line.substr(0, space_colon);
-        string number   = line.substr(space_colon);
-        deq_.push_back(std::make_pair(type_req, number));        
-    }     
-    }
-        
-        
-
+    for (int j = 0; j < query_count; ++j) {
+        string line;
+        getline(std::cin, line);
+        if (!line.empty()) {
+                auto space_colon = line.find(" ");  // : отделяет название запроса
+                string type_req = line.substr(0, space_colon);
+                string number = line.substr(space_colon+1);
+                deq_.push_back(std::make_pair(type_req, number));
+            }
+        }
         
     void Output(){
         for (const auto& element : deq_) {
