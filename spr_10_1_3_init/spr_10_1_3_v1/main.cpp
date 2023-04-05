72850c4d26ab30ceecf4b504f7ba5582140124b4
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-04-05 17:42:55 +0300

    Update main.cpp

--- spr_10_1_3_init/spr_10_1_3_v1/main.cpp
+++ spr_10_1_3_init/spr_10_1_3_v1/main.cpp
@@ -1,2 +1,24 @@
 // напишите решение с нуля
 // код сохраните в свой git-репозиторий
+
+int main() {
+	deque<pair <string, string>> deq_; // тут перечень запросов на вывод 
+	TransportCatalogue tc;
+	ir = InputReader(cin);
+	UpdateQuery q;
+	int query_count = ir.getNumUpdateQueries();
+
+
+	for (int i = 0; i < query_count; ++i) {
+		q = ir.getUpdateQuery();
+		switch (q.type) {
+		case QueryType::Bus:
+			tc.AddBus(q);
+		case QueryType::Stop:
+			tc.AddStop(q);
+		}
+
+	}
+
+	return 0;
+};
