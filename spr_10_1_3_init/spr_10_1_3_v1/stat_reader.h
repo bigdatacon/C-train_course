7cea8b44ffae02e418ce620acf71b38b51c983d4
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-04-05 21:23:55 +0300

    Update stat_reader.h

--- spr_10_1_3_init/spr_10_1_3_v1/stat_reader.h
+++ spr_10_1_3_init/spr_10_1_3_v1/stat_reader.h
@@ -1,3 +1,4 @@
+#pragma once
 // напишите решение с нуля
 // код сохраните в свой git-репозиторий
 #include <cassert>
@@ -15,13 +16,17 @@
 #include <utility>
 #include <math.h>
 #include <regex>
+#include <stat_reader.h>
+#include "transport_catalogue.h"
+
+
 using namespace std;
 
 
-ostream& operator<<(ostream& os,  const AllBusInfoBusResponse& r) {
+ostream& operator<<(ostream& os, const AllBusInfoBusResponse& r) {
     // Реализуйте эту функцию
     //cout << "BusesForStopResponse"s << endl;
-    if (r.stops == 0 ) {
+    if (r.stops == 0) {
         cout << "No stop for Bus";
     }
     else {
@@ -30,31 +35,28 @@ ostream& operator<<(ostream& os,  const AllBusInfoBusResponse& r) {
     return os;
 }
 
-class Stat_reader {
-public:
-    istream& operator>>(istream& is, Query& q, TransportCatalogue tc) {
+
+
+istream& Stat_reader::operator>>(istream& is, UpdateQuery& q) {
     int query_count;
     cin >> query_count;
     for (int j = 0; j < query_count; ++j) {
         string line;
         getline(std::cin, line);
         if (!line.empty()) {
-                auto space_colon = line.find(" ");  // : отделяет название запроса
-                string type_req = line.substr(0, space_colon);
-                string number = line.substr(space_colon+1);
-                deq_.push_back(std::make_pair(type_req, number));
-            }
+            auto space_colon = line.find(" ");  // : отделяет название запроса
+            string type_req = line.substr(0, space_colon);
+            string number = line.substr(space_colon + 1);
+            deq_.push_back(std::make_pair(type_req, number));
         }
-        
-    void Output(){
+    }
+
+    void Output() {
         for (const auto& element : deq_) {
-        if (element.first == "Bus"s) {cout <<tc.GetAllBusInfo(element.second); } 
+            if (element.first == "Bus"s) { cout << tc.GetAllBusInfo(element.second); }
         }
         break;
 
     }
-        
-        
-private:
-    deque<pair <string, string>> deq_; // тут перечень запросов на вывод 
-};
+}
+
