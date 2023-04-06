d63bcf8eec6a6a6c5ce67ed1ab455164f2f3aa85
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-04-07 00:53:54 +0300

    Update input_reader.cpp

--- spr_10_1_3_init/10_1_v3/input_reader.cpp
+++ spr_10_1_3_init/10_1_v3/input_reader.cpp
@@ -1,3 +1,4 @@
+#pragma once
 #include <cassert>
 #include <iostream>
 #include <map>
@@ -9,86 +10,74 @@
 #include <algorithm>
 #include <fstream>
 #include <sstream>
-#include <unordered_map>
-#include <utility>
-#include <math.h>
-#include <regex>
+
 #include "input_reader.h"
+#include "transport_catalogue.h"
 
 using namespace std;
 
-vector<string> SplitStringBySign(std::string str)
-{
-    vector<string> tokens;
-    string token;
-
-    while (true) {
-        size_t pos = str.find_first_of(">-");
-        if (pos != std::string::npos) {
-            token = str.substr(0, pos);
-            tokens.push_back(token);
-            str = str.substr(pos + 1);
-        }
-        else {
-            if (str.size() != 0) {
-                tokens.push_back(str);
-            }
-            return tokens;
-        }
-    }
-}
-
-
-pair<double, double> SplitStringByComma(string str) {
-    pair<double, double> coordinates;
-    stringstream ss(str);
-    string token;
-
-    getline(ss, token, ',');
-    coordinates.first = stod(token);
-
-    getline(ss, token, ',');
-    coordinates.second = stod(token);
-
-    return coordinates;
-}
 
 InputReader::InputReader(istream& is) : is_(is) {
 }
 
-int InputReader::getNumUpdateQueries() {
-    string line;
-    getline(is_, line);
-    return std::stoi(line); 
+int InputReader::GetNumUpdateQueries() {
+	string line;
+	getline(is_, line);
+	num_update_q_ = std::stoi(line);
+	return num_update_q_;
 }
 
-UpdateQuery InputReader::getUpdateQuery() {
-    UpdateQuery q;
-    string line;
-    getline(is_, line);
-    auto pos_colon = line.find(":");
-    string request_section = line.substr(0, pos_colon);
-    string list_section = line.substr(pos_colon + 1);
-    auto space_colon = line.find(" ");
-    string req_name = request_section.substr(0, space_colon);
+void InputReader::FillRequests() {
+	for (int i = 0; i < num_update_q_; ++i) {
+		string line;
+		getline(is_, line);
+		auto pos_colon = line.find(":");
+		string request_section = line.substr(0, pos_colon);
+		string list_section = line.substr(pos_colon + 1);
+		auto space_colon = line.find(" ");
+		string req_name = request_section.substr(0, space_colon);
 
-    if (req_name == "Bus"s) {
-        vector<string> bus_stops = SplitStringBySign(list_section);
-        q.type = QueryType::Bus  /*"Bus"s*/;
-        q.bus.bus = request_section.substr(space_colon + 1, pos_colon);
-        q.bus.stops = bus_stops;
-    }
-    else if (req_name == "Stop"s) {
-        q.type = QueryType::Stop;
-        q.stop.stop = request_section.substr(space_colon + 1, pos_colon);
-        pair<double, double> coordinates = SplitStringByComma(list_section);
-        q.stop.coordinates.lat = coordinates.first;
-        q.stop.coordinates.lng = coordinates.second;
+		if (req_name == "Bus"s) {
+			Bus q;
+			auto circle = list_section.find(">");
+			if (circle != std::string::npos) {
+				vector<string> bus_stops = SplitStringBySignCircle(list_section);
+				q.type = "c"s;
+				q.bus = request_section.substr(space_colon + 1, pos_colon);
+				q.stops = bus_stops;
+				upd_req_bus_.push_back(q);
+			}
+			else {
+				vector<string> bus_stops = SplitStringBySignPlain(list_section);
+				q.type = "p"s;
+				q.bus = request_section.substr(space_colon + 1, pos_colon);
+				q.stops = bus_stops;
+				upd_req_bus_.push_back(q);
+			}
+		}
+		else if (req_name == "Stop"s) {
+			Stop q;
+			q.stop = request_section.substr(space_colon + 1, pos_colon);
+			pair<double, double> coordinates = SplitStringByComma(list_section);
+			q.coordinates.lat = coordinates.first;
+			q.coordinates.lng = coordinates.second;
+			upd_req_stop_.push_back(q);
+		}
+		else { continue; }
+	}
+}
 
-    }
-    /*else if (line == ""s) {
-        return is_ >> q;
-    }*/
-    return q;
+void InputReader::GetUpdStop(TransportCatalogue& tc) {
+	for (int i = 0; i < upd_req_stop_.size(); ++i) {
+		//cout << "UPDT : " << upd_req_stop_[i].stop << endl;
+		tc.AddStop(upd_req_stop_[i]);
+	}
+}
+
+void InputReader::GetUpdBus(TransportCatalogue& tc) {
+	for (int i = 0; i < upd_req_bus_.size(); ++i) {
+		//cout << "UPDT BUS : " << upd_req_bus_[i].bus << endl;
+		tc.AddBus(upd_req_bus_[i]);
+	}
 }
 
