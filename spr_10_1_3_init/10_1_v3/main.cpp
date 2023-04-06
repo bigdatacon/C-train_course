c72d8cc5000b2c18cccda087959776d02f65762c
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-04-07 00:52:00 +0300

    Update main.cpp

--- spr_10_1_3_init/10_1_v3/main.cpp
+++ spr_10_1_3_init/10_1_v3/main.cpp
@@ -1,18 +1,62 @@
-// напишите решение с нуля
-// код сохраните в свой git-репозиторий
-#include "input_reader.h"
+#include <cassert>
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+#include <set>
+#include <algorithm>
+#include <deque>
+#include <algorithm>
+#include <fstream>
+#include <sstream>
+#include <unordered_map>
+#include <utility>
+#include <math.h>
+#include <regex>
+#include <cmath>
+#include <unordered_set>
+#include "stat_reader.h"
+#include "geo.h"
 #include "transport_catalogue.h"
+#include "input_reader.h"
+
+using namespace std;
+
 
 
 
 int main()
 {
+	TransportCatalogue tc;
     InputReader reader(std::cin);
-    int count = reader.getNumUpdateQueries();
-    std::cout << "COUNT : " << count << std::endl;
-    for (int i = 0; i < count; i++) {
-        UpdateQuery q = reader.getUpdateQuery();
-        std::cout << "Q : " << q.toString() << std::endl;
-    }
+    int count = reader.GetNumUpdateQueries();
+    //std::cout << "COUNT : " << count << std::endl;
+	reader.FillRequests();
+
+	reader.GetUpdStop(tc);
+	reader.GetUpdBus(tc);
+
+	string b = "750"s;
+
+	tc.GetAllBusInfo(b);
+
+	StatReader streader(std::cin);
+	int count_out = streader.GetNumOutQueries();
+	streader.FillRequests();
+	streader.Output(tc);
+	
+
+
+
+	// как говорили на вэбинаре для класса out не нужно записывать запросы в контейнер поэтому делаю обычный вывод из cin
+	for (int i = 0; i < count_out; ++i) {
+		string line;
+		getline(cin, line);
+		auto space_colon = line.find(" ");  // : отделяет название запроса
+		string str = line.substr(space_colon + 1);
+		cout << tc.GetAllBusInfo(str) << endl;
+	}
+
+	
     return 0;
 }
