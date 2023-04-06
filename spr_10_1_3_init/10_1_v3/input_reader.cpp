3becd95b04e67f2edb62fbc1bf8400cec716b188
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-04-06 12:38:08 +0300

    Update input_reader.cpp

--- spr_10_1_3_init/10_1_v3/input_reader.cpp
+++ spr_10_1_3_init/10_1_v3/input_reader.cpp
@@ -1,4 +1,4 @@
-﻿#include <cassert>
+#include <cassert>
 #include <iostream>
 #include <map>
 #include <string>
@@ -27,12 +27,10 @@ vector<string> SplitStringBySign(std::string str)
         if (pos != std::string::npos) {
             token = str.substr(0, pos);
             tokens.push_back(token);
-            //std::cout << "THIS OSTANOVKA : " <<  token << std::endl;
             str = str.substr(pos + 1);
         }
         else {
             if (str.size() != 0) {
-                //std::cout << "THIS LAST !! OSTANOVKA : " << str << std::endl;
                 tokens.push_back(str);
             }
             return tokens;
@@ -55,49 +53,13 @@ pair<double, double> SplitStringByComma(string str) {
     return coordinates;
 }
 
-istream& operator>>(istream& is, UpdateQuery& q) {
-    string line;
-    getline(is, line);
-    auto pos_colon = line.find(":");  // : îòäåëÿåò íàçâàíèå àâòîáóñà èëè îñòàíîâêè 
-    // : îòäåëÿåò íàçâàíèå çàïðîñà 
-
-    string request_section = line.substr(0, pos_colon);
-    string list_section = line.substr(pos_colon + 1);
-    auto space_colon = line.find(" ");
-    string req_name = request_section.substr(0, space_colon);
-
-    if (req_name == "Bus"s) {
-        vector<string> bus_stops = SplitStringBySign(list_section);
-        q.type = QueryType::Bus  /*"Bus"s*/;
-        q.bus.bus = request_section.substr(space_colon + 1, pos_colon);
-        q.bus.stops = bus_stops;
-    }
-    else if (req_name == "Stop"s) {
-        q.type = QueryType::Stop;
-        //q.type = /*QueryType::Bus*/  "Stop"s;
-        q.stop.stop = request_section.substr(space_colon + 1, pos_colon);
-        pair<double, double> coordinates = SplitStringByComma(list_section);
-        q.stop.coordinates.lat = coordinates.first;
-        q.stop.coordinates.lng = coordinates.second;
-
-        //deq_.push_back(q);
-    }
-    else if (line == ""s) {
-        return is >> q;
-    }
-    return is;
-
-}
-
-
-
 InputReader::InputReader(istream& is) : is_(is) {
 }
 
 int InputReader::getNumUpdateQueries() {
-    //int query_count;
-    cin /*is_*/ >> num_update_q_;
-    return num_update_q_;
+    string line;
+    getline(is_, line);
+    return std::stoi(line); 
 }
 
 UpdateQuery InputReader::getUpdateQuery() {
