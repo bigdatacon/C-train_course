284e2abf97c8c363d8c751fc57838b50f6f44215
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-04-07 19:32:27 +0300

    Update main.cpp

--- spr_10_1_3_init/spr_10_1_v4/main.cpp
+++ spr_10_1_3_init/spr_10_1_v4/main.cpp
@@ -47,11 +47,11 @@ int main()
 
 
 	//сравниваю файлы 
-    //ifstream file1("file1.txt");
-    //ifstream file2("file2.txt");
+    ifstream file1("file1.txt");
+    ifstream file2("file2.txt");
 
-    ifstream file1("file11.txt");
-    ifstream file2("file22.txt");
+    //ifstream file1("file11.txt");
+    //ifstream file2("file22.txt");
 
     string line1, line2;
     int line_num = 1;
@@ -59,6 +59,8 @@ int main()
     while (getline(file1, line1) && getline(file2, line2)) {
         if (line1 != line2) {
             cout << "Files differ at line " << line_num << endl;
+            cout << "line first ff : " << line1 << endl;
+            cout << "line secon ff : " << line2 << endl;
             return 0;
         }
         line_num++;
@@ -71,20 +73,35 @@ int main()
 
     cout << "Files are identical" << endl;
 
-    std::ifstream file("file11.txt");  // открываем файл для чтения
+    /*cout << "/////////////////////////////////////// READ File1 " << endl;
+
+    std::ifstream file("file1.txt");  // открываем файл для чтения
     std::string line;
     while (std::getline(file, line)) {  // читаем строки по очереди
         std::cout << line << std::endl;  // выводим каждую строку на экран
     }
+     
+    cout << "/////////////////////////////////////// READ File2 " << endl;
+    std::ifstream file_s("file2.txt");  // открываем файл для чтения
+    std::string line_s;
+    while (std::getline(file_s, line2)) {  // читаем строки по очереди
+        std::cout << line2 << std::endl;  // выводим каждую строку на экран
+    }
 
-    
+    cout << "/////////////////////////////////////// READ File11 " << endl;
+    std::ifstream file_t("file11.txt");  // открываем файл для чтения
+    std::string line3;
+    while (std::getline(file_t, line3)) {  // читаем строки по очереди
+        std::cout << line3 << std::endl;  // выводим каждую строку на экран
+    }
 
 
+    cout << "/////////////////////////////////////// READ File22 " << endl;
+    std::ifstream file_f("file22.txt");  // открываем файл для чтения
+    std::string line4;
+    while (std::getline(file_f, line4)) {  // читаем строки по очереди
+        std::cout << line4 << std::endl;  // выводим каждую строку на экран
+    }*/
 
-    return 0;
-
-
-
-	
     return 0;
 }
