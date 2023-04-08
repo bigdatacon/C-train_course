4b0726fb685b8f41987aa0e39f95269d5eea1348
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-04-08 13:48:49 +0300

    Add files via upload

---
+++ spr_10_1_3_init/spr_10_3_v5/input_reader.h
@@ -0,0 +1,53 @@
+#pragma once
+#include <cassert>
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+#include <set>
+#include <algorithm>
+#include <deque>
+#include <algorithm>
+#include <fstream>
+#include <sstream>
+#include <unordered_map>
+#include <utility>
+#include <math.h>
+#include <regex>
+
+#include "transport_catalogue.h"
+#include "geo.h"
+
+
+std::vector<std::pair<std::string, int>> SplitStringByForDist(std::string distance_section);
+
+std::vector<std::string> SplitStringBySignPlain(std::string str);
+
+std::vector<std::string> SplitStringBySignCircle(std::string str);
+
+std::pair<double, double> SplitStringByComma(std::string str);
+
+
+class InputReader {
+public:
+	InputReader(std::istream& is);
+
+	int GetNumUpdateQueries();
+
+	void FillRequests();
+
+	void GetUpdStop(TransportCatalogue& tc);
+
+	void GetUpdBus(TransportCatalogue& tc);
+
+	void GetUpdStopDist(TransportCatalogue& tc);
+
+
+
+private:
+	std::istream& is_;
+	std::deque<Bus> upd_req_bus_;
+	std::deque<Stop> upd_req_stop_;
+	std::deque<Stop> upd_req_stop_with_dist_;
+	int num_update_q_;
+};
