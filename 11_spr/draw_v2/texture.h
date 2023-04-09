c53bc15d4740a1e88a9e51fa62782ad7878dabb2
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-04-09 18:25:44 +0300

    Update texture.h

--- 11_spr/draw_v2/texture.h
+++ 11_spr/draw_v2/texture.h
@@ -11,27 +11,27 @@ public:
         // Заглушка. Реализуйте метод самостоятельно
         return GetImageSize(image_);
     }
-   
-    
-    char GetPixelColor(Point p) const {
-    Size s = GetSize();
-    if (!IsPointInEllipse(p, s) {
-        return '.'; 
-    }
-    else { return image_[y][x]; }
-}
-    
-   /* 
-    char GetPixelColor(Point p) const {
-    const auto [x, y] = p;
-    const auto [width, height] = GetSize();
-    if (x < 0 || y < 0 || x >= width || y >= height) {
-        return '.'; 
-    }
-    return image_[y][x];
-}*/
 
-    
+
+    char GetPixelColor(Point p) const {
+        Size s = GetSize();
+        if (!IsPointInEllipse(p, s)) {
+            return '.';
+        }
+        else { return image_[p.y][p.x]; }
+    }
+
+    /*
+     char GetPixelColor(Point p) const {
+     const auto [x, y] = p;
+     const auto [width, height] = GetSize();
+     if (x < 0 || y < 0 || x >= width || y >= height) {
+         return '.';
+     }
+     return image_[y][x];
+ }*/
+
+
 
 private:
     Image image_;
