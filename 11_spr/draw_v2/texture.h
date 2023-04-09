aaf648ed9ee627e549f715c3363dc9279fd79f64
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-04-09 17:52:03 +0300

    Add files via upload

---
+++ 11_spr/draw_v2/texture.h
@@ -0,0 +1,38 @@
+#pragma once
+#include "common.h"
+
+class Texture {
+public:
+    explicit Texture(Image image)
+        : image_(std::move(image)) {
+    }
+
+    Size GetSize() const {
+        // Заглушка. Реализуйте метод самостоятельно
+        return GetImageSize(image_);
+    }
+   
+    
+    char GetPixelColor(Point p) const {
+    Size s = GetSize();
+    if (!IsPointInEllipse(p, s) {
+        return '.'; 
+    }
+    else { return image_[y][x]; }
+}
+    
+   /* 
+    char GetPixelColor(Point p) const {
+    const auto [x, y] = p;
+    const auto [width, height] = GetSize();
+    if (x < 0 || y < 0 || x >= width || y >= height) {
+        return '.'; 
+    }
+    return image_[y][x];
+}*/
+
+    
+
+private:
+    Image image_;
+};
