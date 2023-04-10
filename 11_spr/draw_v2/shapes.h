30b596b7347d99dda0234a13d42bde64772c424f
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-04-10 21:05:25 +0300

    Update shapes.h

--- 11_spr/draw_v2/shapes.h
+++ 11_spr/draw_v2/shapes.h
@@ -36,8 +36,9 @@ public:
 
     void DrawRectangle(Image& image) const {
         Size s = texture_->GetSize();
+        (void)texture_->PrintImage();
         //for (int y = position_.y; y < position_.y + size_.height; ++y) {
-        for (int y = 0; y < size_.height; ++y) {
+        for (int y = /*position_.y*/ 0; y < size_.height; ++y) {
             if (y > s.height) {
                 std::string row(s.width, '.');
                 std::cout << row << std::endl;
@@ -46,7 +47,7 @@ public:
 
             std::string& row = image[y];
             //for (int x = position_.x; x < position_.x + size_.width; ++x) {
-            for (int x = 0; x < size_.width; ++x) {
+            for (int x = /*position_.x*/ 0; x < size_.width; ++x) {
         
                 if (! (x > s.width) ) {
                     char pixel = texture_->GetPixelColor({ x, y });
@@ -63,19 +64,22 @@ public:
 
     void DrawEllipse(Image& image) const {
         Size s = texture_->GetSize();
+        (void)texture_->PrintImage();
         //for (int y = position_.y; y < position_.y + size_.height; ++y) {
-        for (int y=0; y <= size_.height; ++y) {
+        for (int y= position_.y; y <= size_.height; ++y) {
             std::string& row = image[y];
             //for (int x = position_.x; x < position_.x + size_.width; ++x) {
-            for (int x = 0; x <= size_.width; ++x) {
+            for (int x = position_.x; x <= size_.width; ++x) {
 
                 if (IsPointInEllipse({ x, y }, s)) {
 
                     char pixel = texture_->GetPixelColor({ x, y });
                         row[x] = pixel;
+                        image[y][x] = pixel;
                     }
                     else {
                         row[x] = '.';
+                        image[y][x] = '.';
                     }
                 }
             
