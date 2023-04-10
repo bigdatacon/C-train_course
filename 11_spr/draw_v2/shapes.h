9355fcb393c09e3111a0874e82dc7d61d5ce5e54
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-04-10 10:19:12 +0300

    Update shapes.h

--- 11_spr/draw_v2/shapes.h
+++ 11_spr/draw_v2/shapes.h
@@ -5,7 +5,7 @@
 #include <iostream>
 #include <cmath>
 
-// Ïîääåðæèâàåìûå âèäû ôèãóð: ïðÿìîóãîëüíèê è ýëëèïñ
+// ÐŸÐ¾Ð´Ð´ÐµÑ€Ð¶Ð¸Ð²Ð°ÐµÐ¼Ñ‹Ðµ Ð²Ð¸Ð´Ñ‹ Ñ„Ð¸Ð³ÑƒÑ€: Ð¿Ñ€ÑÐ¼Ð¾ÑƒÐ³Ð¾Ð»ÑŒÐ½Ð¸Ðº Ð¸ ÑÐ»Ð»Ð¸Ð¿Ñ
 enum class ShapeType { RECTANGLE, ELLIPSE };
 
 
@@ -26,21 +26,59 @@ public:
     }
 
     void Draw(Image& image) const {
+        if (type_ == ShapeType::RECTANGLE) {
+            DrawRectangle(image);
+        }
+        else if (type_ == ShapeType::ELLIPSE) {
+            DrawEllipse(image);
+        }
+    }
+
+    void DrawRectangle(Image& image) const {
+        Size s = texture_->GetSize();
         for (int y = position_.y; y < position_.y + size_.height; ++y) {
-            if (y >= image.size()) {
-                break;
-            }
             std::string& row = image[y];
             for (int x = position_.x; x < position_.x + size_.width; ++x) {
-                if (x >= row.size()) {
-                    break;
+        
+                if (IsPointInEllipse({ x, y }, s)) {
+                    char pixel = texture_->GetPixelColor({ x, y });
+                    row[x] = pixel; 
                 }
-                char pixel = texture_->GetPixelColor({ x, y });
-                row[x] = pixel;
+                else { 
+                row[x] = '.';
+                }     
+            }
+            std::cout << row << std::endl;
+            
+        }
+    }
+
+    void DrawEllipse(Image& image) const {
+        double a = size_.width / 2.0;
+        double b = size_.height / 2.0;
+        double x0 = position_.x + a;
+        double y0 = position_.y + b;
+        Size s = texture_->GetSize();
+        for (int y = position_.y; y < position_.y + size_.height; ++y) {
+            std::string& row = image[y];
+            for (int x = position_.x; x < position_.x + size_.width; ++x) {
+
+                if (IsPointInEllipse({ x, y }, s)) {
+                    double dx = x - x0;
+                    double dy = y - y0;
+                    if (std::abs(dx / a) < 0.5 && std::abs(dy / b) < 0.5) {
+                        char pixel = texture_->GetPixelColor({ x, y });
+                        row[x] = pixel;
+                    }
+                    else {
+                        row[x] = '.';
+                    }
+                }
             }
             std::cout << row << std::endl;
         }
     }
+
 
 private:
     ShapeType type_;
