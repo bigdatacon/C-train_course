188bff4355cb533d224ea3d3c2b100cf39fe9f30
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-04-11 11:45:25 +0300

    Update shapes.h

--- 11_spr/draw_v2/shapes.h
+++ 11_spr/draw_v2/shapes.h
@@ -34,11 +34,21 @@ public:
         }
     }
 
+
+
+
     void DrawRectangle(Image& image) const {
-        Size s = texture_->GetSize();
+        Size s = /*texture_->GetSize();*/ GetImageSize(image);
         (void)texture_->PrintImage();
-        //for (int y = position_.y; y < position_.y + size_.height; ++y) {
-        for (int y = position_.y /*0*/; y < position_.y+ size_.height; ++y) {
+        int start_draw_to_x = position_.x;
+        // Начало рисования по оси Y: точка pos_.y
+        int start_draw_to_y = position_.y;
+        // Конец рисования по оси Х: ограничена размером фигуры (size_.width), если фигура выходит за пределы холста (image_size.width), то конец - размер холста.
+        int end_draw_to_x = size_.width > s.width ? s.width - 1 : size_.width + position_.x - 1;
+        // Конец рисования по оси Y: ограничена размером фигуры (size_.height), если фигура выходит за пределы холста (image_size.height), то конец - размер холста.
+        int end_draw_to_y = size_.height > s.height ? s.height - 1 : size_.height + position_.y;
+ 
+        for (int y = position_.y; y < end_draw_to_y + size_.height; ++y) {
             if (y > s.height) {
                 std::string row(s.width, '.');
                 std::cout << row << std::endl;
@@ -47,7 +57,7 @@ public:
 
             std::string& row = image[y];
             //for (int x = position_.x; x < position_.x + size_.width; ++x) {
-            for (int x = position_.x-1 /*0*/; x < position_.x + size_.width; ++x) {
+            for (int x = position_.x ; x < end_draw_to_x; ++x) {
         
                 if (! (x > s.width) ) {
                     char pixel = texture_->GetPixelColor({ x, y });
@@ -67,11 +77,20 @@ public:
     void DrawEllipse(Image& image) const {
         Size s = texture_->GetSize();
         (void)texture_->PrintImage();
+        int start_draw_to_x = position_.x;
+        // Начало рисования по оси Y: точка pos_.y
+        int start_draw_to_y = position_.y;
+        // Конец рисования по оси Х: ограничена размером фигуры (size_.width), если фигура выходит за пределы холста (image_size.width), то конец - размер холста.
+        int end_draw_to_x = size_.width > s.width ? s.width - 1 : size_.width + position_.x - 1;
+        // Конец рисования по оси Y: ограничена размером фигуры (size_.height), если фигура выходит за пределы холста (image_size.height), то конец - размер холста.
+        int end_draw_to_y = size_.height > s.height ? s.height - 1 : size_.height + position_.y;
+
+
         //for (int y = position_.y; y < position_.y + size_.height; ++y) {
-        for (int y= position_.y; y <= size_.height; ++y) {
+        for (int y= position_.y; y <= end_draw_to_y; ++y) {
             std::string& row = image[y];
             //for (int x = position_.x; x < position_.x + size_.width; ++x) {
-            for (int x = position_.x; x <= size_.width; ++x) {
+            for (int x = position_.x; x <= end_draw_to_x; ++x) {
 
                 if (IsPointInEllipse({ x, y }, s)) {
 
