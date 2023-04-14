a0cd64195ccaf481b0f35b062f0415c8210575f1
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-04-14 23:17:51 +0300

    Update main.cpp

--- 11_spr/graph_v2/main.cpp
+++ 11_spr/graph_v2/main.cpp
@@ -1,35 +1,37 @@
 #define _USE_MATH_DEFINES
 #include "svg.h" // Объявления классов библиотеки должны быть расположены в файле svg.h
-
+ 
 #include <cmath>
-
+ 
 using namespace std::literals;
 using namespace svg;
-
-Polyline CreateStar(Point center, double outer_rad, double inner_rad, int num_rays) {
-    Polyline polyline;
-    for (int i = 0; i <= num_rays; ++i) {
-        double angle = 2 * M_PI * (i % num_rays) / num_rays;
-        polyline.AddPoint({center.x + outer_rad * sin(angle), center.y - outer_rad * cos(angle)});
-        if (i == num_rays) {
-            break;
+using namespace std;
+void TestText() {
+        {
+            Text().Render(cout);
+            cout << "endl";
+        } {
+            Text().SetOffset({0, 6}).SetFontSize(12).Render(cout);
+            cout << "endl";
+        } {
+            Text().SetPosition({35, 20})
+                    .SetFontFamily("Verdana"s)
+                    .SetFontSize(12)
+                    .SetOffset({0, 6})
+                    .SetFontWeight("bold"s)
+                    .SetData("Hello C++"s)
+                .Render(cout);
+            cout << "endl";
+        } {
+            Text().SetFontFamily("Verdana"s)
+                    .SetPosition({35, 20})
+                    .SetOffset({0, 6})
+                    .SetFontSize(12)
+                    .SetFontWeight("bold"s)
+                    .SetData("Hello C++"s)
+                .Render(cout);
         }
-        angle += M_PI / num_rays;
-        polyline.AddPoint({center.x + inner_rad * sin(angle), center.y - inner_rad * cos(angle)});
     }
-    return polyline;
+int main(){
+    TestText();
 }
-
-int main() {
-    Document doc;
-    doc.Add(Circle().SetCenter({20, 20}).SetRadius(10));
-    doc.Add(Text()
-                .SetFontFamily("Verdana"s)
-                .SetPosition({35, 20})
-                .SetOffset({0, 6})
-                .SetFontSize(12)
-                .SetFontWeight("bold"s)
-                .SetData("Hello C++"s));
-    doc.Add(CreateStar({20, 50}, 10, 5, 5));
-    doc.Render(std::cout);
-} 
