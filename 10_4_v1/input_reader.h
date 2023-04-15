e2cb1cdcc5b1ba5747fc2cc8a9dfd78be54449db
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-04-15 12:22:42 +0300

    Add files via upload

---
+++ 10_4_v1/input_reader.h
@@ -0,0 +1,41 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+#include <set>
+#include <algorithm>
+#include <deque>
+#include <fstream>
+#include <sstream>
+
+
+#include "transport_catalogue.h"
+#include "geo.h"
+
+namespace transport_catalogue {
+
+
+	class InputReader {
+	public:
+		explicit InputReader(std::istream& is);
+
+		int GetNumUpdateQueries();
+
+		void FillRequests();
+
+		void UpdStop(TransportCatalogue& tc);
+
+		void UpdBus(TransportCatalogue& tc);
+
+		void UpdStopDist(TransportCatalogue& tc);
+
+
+
+	private:
+		std::istream& is_;
+		std::deque<BusDescription> upd_req_bus_;
+		std::deque<Stop_Input> upd_req_stop_;
+		int num_update_q_;
+	};
+
+}
