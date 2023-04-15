cc2d7f8b083b620bf3eb25262aab4860fd67fd71
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-04-15 15:56:28 +0300

    Update main.cpp

--- 11_spr/Graph_base/main.cpp
+++ 11_spr/Graph_base/main.cpp
@@ -1,67 +1,37 @@
 #define _USE_MATH_DEFINES
-#include "svg.h"
-
+#include "svg.h" // Объявления классов библиотеки должны быть расположены в файле svg.h
+ 
 #include <cmath>
-
+ 
 using namespace std::literals;
 using namespace svg;
-
-/*
-Пример использования библиотеки. Он будет компилироваться и работать, когда вы реализуете
-все классы библиотеки.
-*/
-/*
-namespace {
-
-Polyline CreateStar(Point center, double outer_rad, double inner_rad, int num_rays) {
-    Polyline polyline;
-    for (int i = 0; i <= num_rays; ++i) {
-        double angle = 2 * M_PI * (i % num_rays) / num_rays;
-        polyline.AddPoint({center.x + outer_rad * sin(angle), center.y - outer_rad * cos(angle)});
-        if (i == num_rays) {
-            break;
+using namespace std;
+void TestText1() {
+        {
+            Text().Render(cout);
+            cout << "endl";
+        } {
+            Text().SetOffset({0, 6}).SetFontSize(12).Render(cout);
+            cout << "endl";
+        } {
+            Text().SetPosition({35, 20})
+                    .SetFontFamily("Verdana"s)
+                    .SetFontSize(12)
+                    .SetOffset({0, 6})
+                    .SetFontWeight("bold"s)
+                    .SetData("Hello C++"s)
+                .Render(cout);
+            cout << "endl";
+        } {
+            Text().SetFontFamily("Verdana"s)
+                    .SetPosition({35, 20})
+                    .SetOffset({0, 6})
+                    .SetFontSize(12)
+                    .SetFontWeight("bold"s)
+                    .SetData("Hello C++"s)
+                .Render(cout);
         }
-        angle += M_PI / num_rays;
-        polyline.AddPoint({center.x + inner_rad * sin(angle), center.y - inner_rad * cos(angle)});
     }
-    return polyline;
-}
-
-// Выводит приветствие, круг и звезду
-void DrawPicture() {
-    Document doc;
-    doc.Add(Circle().SetCenter({20, 20}).SetRadius(10));
-    doc.Add(Text()
-                .SetFontFamily("Verdana"s)
-                .SetPosition({35, 20})
-                .SetOffset({0, 6})
-                .SetFontSize(12)
-                .SetFontWeight("bold"s)
-                .SetData("Hello C++"s));
-    doc.Add(CreateStar({20, 50}, 10, 5, 5));
-    doc.Render(std::cout);
-}
-
-}  // namespace
-*/
-
-int main() {
-    /*
-       Это пример для иллюстрации работы класса Circle, данного в заготовке решения.
-       После того как вы реализуете реализуете класс Document, аналогичного результата
-       можно будет достичь так:
-
-       Document doc;
-       doc.Add(Circle().SetCenter({20, 20}).SetRadius(10));
-       doc.Render(std::cout);
-    */
-    std::cout << "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>"sv << std::endl;
-    std::cout << "<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\">"sv << std::endl;
-
-    Circle c;
-    c.SetCenter({20, 20}).SetRadius(10);
-    RenderContext ctx(std::cout, 2, 2);
-    c.Render(ctx);
-
-    std::cout << "</svg>"sv;
+int main(){
+    TestText1();
 }
