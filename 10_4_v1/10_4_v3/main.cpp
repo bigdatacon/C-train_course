ead8f99b360aa231b37cf74b729f190f1ad5d4f9
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-04-15 14:43:56 +0300

    Add files via upload

---
+++ 10_4_v1/10_4_v3/main.cpp
@@ -0,0 +1,26 @@
+﻿#include "stat_reader.h"
+#include "geo.h"
+#include "transport_catalogue.h"
+#include "input_reader.h"
+
+using namespace std;
+
+
+int main()
+{
+	transport_catalogue::TransportCatalogue tc;
+	transport_catalogue::InputReader reader(std::cin);
+	(void)reader.GetNumUpdateQueries();
+	reader.FillRequests();
+
+	reader.UpdStop(tc);
+	reader.UpdBus(tc);
+	reader.UpdStopDist(tc);
+
+	transport_catalogue::StatReader streader(std::cin);
+	(void)streader.GetNumOutQueries();
+	streader.FillRequests();
+	streader.Output(tc);
+
+	return 0;
+}
