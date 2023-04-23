121812b1c2fea466f0de5758638444905dc59cbe
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-04-23 14:58:38 +0300

    Update json_reader.h

--- 11_spr_transp/11_itg_1_v1/json_reader.h
+++ 11_spr_transp/11_itg_1_v1/json_reader.h
@@ -12,8 +12,30 @@ using namespace std;
 
 
 
-json::Document LoadJSON(const std::string& s); 
+inline json::Document LoadJSON(const std::string& s) {
+    std::istringstream strm(s);
+    return json::Load(strm);
+}
 
-std::string Print(const Node& node); 
+inline std::string Print(const Node& node) {
+    std::ostringstream out;
+    Print(Document{ node }, out);
+    return out.str();
+}
 
-void ReadInputJsonRequest(); 
+inline void ReadInputJsonRequest() {
+    std::string input_str;
+    std::getline(std::cin, input_str);
+
+    json::Document d = LoadJSON(input_str);
+    cout << d.GetRoot().AsString() << endl;
+
+    std::istringstream input_stream(input_str);
+    //json::Document res = json::Load(input_stream);
+
+    //Node dict_node(input_str);
+    //cout << res.GetRoot().AsString() << endl;
+    //cout << dict_node.AsMap().AsString() << endl;
+
+
+}
