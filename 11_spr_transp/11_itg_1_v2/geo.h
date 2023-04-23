a851f10197276126259121faa379e1be956fcc0c
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-04-23 23:40:31 +0300

    Update geo.h

--- 11_spr_transp/11_itg_1_v2/geo.h
+++ 11_spr_transp/11_itg_1_v2/geo.h
@@ -1,44 +1,10 @@
 #pragma once
-#include <cmath>
-
-namespace transport_catalogue {
-    namespace geo {
-
-        struct Coordinates {
-            double lat;
-            double lng;
-            bool operator==(const Coordinates& other) const {
-                return lat == other.lat && lng == other.lng;
-            }
-            bool operator!=(const Coordinates& other) const {
-                return !(*this == other);
-            }
-        };
-
-        inline double ComputeDistance(Coordinates from, Coordinates to) {
-            using namespace std;
-            if (from == to) {
-                return 0;
-            }
-            static const double dr = 3.1415926535 / 180.;
-            return acos(sin(from.lat * dr) * sin(to.lat * dr)
-                + cos(from.lat * dr) * cos(to.lat * dr) * cos(abs(from.lng - to.lng) * dr))
-                * 6371000;
-        }
-
-    }
-}
-
-/// new
-
-/*
-#pragma once
 
 namespace geo {
 
 struct Coordinates {
-    double lat; // Широта
-    double lng; // Долгота
+    double lat; // РЁРёСЂРѕС‚Р°
+    double lng; // Р”РѕР»РіРѕС‚Р°
     bool operator==(const Coordinates& other) const {
         return lat == other.lat && lng == other.lng;
     }
@@ -50,4 +16,3 @@ struct Coordinates {
 double ComputeDistance(Coordinates from, Coordinates to);
 
 }  // namespace geo
-*/
