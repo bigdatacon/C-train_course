71c5a1e58add170cf2e74da3a7c7f5ff6bfaff74
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-04-23 15:54:58 +0300

    Update json_reader.h

--- 11_spr_transp/11_itg_1_v1/json_reader.h
+++ 11_spr_transp/11_itg_1_v1/json_reader.h
@@ -3,6 +3,9 @@
 #include <chrono>
 #include <sstream>
 #include <string_view>
+#include <iostream>
+#include <fstream>
+#include <string>
 
 #include "json.h"
 
@@ -23,6 +26,7 @@ inline std::string Print(const Node& node) {
     return out.str();
 }
 
+/*
 inline void ReadInputJsonRequest() {
     std::string input_str;
     std::getline(std::cin, input_str);
@@ -31,11 +35,28 @@ inline void ReadInputJsonRequest() {
     cout << d.GetRoot().AsString() << endl;
 
     std::istringstream input_stream(input_str);
-    //json::Document res = json::Load(input_stream);
 
-    //Node dict_node(input_str);
-    //cout << res.GetRoot().AsString() << endl;
-    //cout << dict_node.AsMap().AsString() << endl;
+}*/
 
+inline void ReadInputJsonRequest() {
+    std::ifstream inFile("input.json.txt"); // открытие файла для чтения
+    if (!inFile) { // проверка успешности открытия файла
+        std::cerr << "Ошибка при открытии файла " << std::endl;
+        return;
+    }
+
+    std::string full_line;
+    std::string line;
+    while (std::getline(inFile, line)) {
+        full_line += line;
+    }
+
+    inFile.close(); // закрытие файла
+    std::cout << "eto fulL_line : " << full_line << std::endl;
+
+    json::Document d = LoadJSON(full_line);
+    cout << d.GetRoot().AsString() << endl;
+
+    std::istringstream input_stream(full_line);
 
 }
