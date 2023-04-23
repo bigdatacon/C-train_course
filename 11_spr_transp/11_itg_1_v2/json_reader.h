c7d8b02845aebace3f28241bef1fcd7db2039101
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-04-23 23:13:47 +0300

    Update json_reader.h

--- 11_spr_transp/11_itg_1_v2/json_reader.h
+++ 11_spr_transp/11_itg_1_v2/json_reader.h
@@ -8,6 +8,8 @@
 #include <string>
 
 #include "json.h"
+#include "geo.h"
+
 
 using namespace json;
 using namespace std::literals;
@@ -120,11 +122,11 @@ inline void ReadInputJsonRequest() {
     string stat_r = GetStringRequests(full_line)[1];
     string base_r = GetStringRequests(full_line)[0];
 
-    cout << "stat_r       " << stat_r << endl;
+    //cout << "stat_r       " << stat_r << endl;
     std::cout << std::endl;
     std::cout << std::endl;
     std::cout << std::endl;
-    cout << "base_r        " << base_r << endl;
+    //cout << "base_r        " << base_r << endl;
 
     // разбиаю stat_r на подстроки по знака },
     
@@ -133,19 +135,122 @@ inline void ReadInputJsonRequest() {
     //for (string s : stat_result) { cout << "This stat req : " << s << endl; }
     //for (string s : base_result) { cout << "This base req : " << s << endl; }
 
+    /* {
+        "type": "Stop",
+            "name" : "Морской вокзал",
+            "latitude" : 43.581969,
+            "longitude" : 39.719848,
+            "road_distances" : {"Ривьерский мост": 850}
+    }*/
+
+    struct StopDistancesDescriptionJson {
+        std::string stop_name;
+        std::vector<std::pair<std::string, int>> distances;
+    };
+
+    struct StopJson {
+        std::string stop_name;
+        geo::Coordinates coordinates;
+    };
+
+    struct BusDescriptionJson {
+        std::string bus_name;
+        std::vector<std::string> stops;
+        std::string type;
+    };
+
+    struct OutputRequestStop {
+        int id;
+        std::string stop_name;
+    };
+
+    struct OutputRequestBus {
+        int id;
+        std::string bus_name;
+    };
+
+    std::deque<OutputRequestStop> out_req_stop;
+    std::deque<OutputRequestBus> out_req_bus;
+
+    std::deque<BusDescriptionJson> upd_req_bus;
+    std::deque<StopJson> upd_req_stop;
+    std::vector<StopDistancesDescriptionJson> distances;
+
+    
+    OutputRequestBus out_bus;
 
     for (string s : stat_result) {
+
+        json::Document stat_req = LoadJSON(s); 
+        if (stat_req.GetRoot().AsMap().at("type").AsString() == "Stop") {
+            OutputRequestStop out_stop;
+            out_stop.id = stat_req.GetRoot().AsMap().at("id").AsInt();
+            out_stop.stop_name = stat_req.GetRoot().AsMap().at("name").AsString();
+            out_req_stop.push_back(out_stop);
+        }
+        else {
+            OutputRequestBus out_bus;
+            out_bus.id = stat_req.GetRoot().AsMap().at("id").AsInt();
+            out_bus.bus_name = stat_req.GetRoot().AsMap().at("name").AsString();
+            out_req_bus.push_back(out_bus);
         
-        json::Document stat_req = LoadJSON(s); cout << true << endl;
+        }
     }
 
     for (string s : base_result) {
+        json::Document base_req = LoadJSON(s);
+        if (base_req.GetRoot().AsMap().at("type").AsString() == "Stop") {
+            StopJson input_stop;
+            
+            input_stop.stop_name = base_req.GetRoot().AsMap().at("name").AsString();
+            input_stop.coordinates.lat = base_req.GetRoot().AsMap().at("latitude").AsDouble();
+            input_stop.coordinates.lng = base_req.GetRoot().AsMap().at("longitude").AsDouble();
 
-        json::Document base_req = LoadJSON(s); cout << true << endl;
+            auto dist = base_req.GetRoot().AsMap().at("road_distances").AsString();
+            cout << "Here dist : " << dist << endl;
+
+            if (base_req.GetRoot().AsMap().at("road_distances").size() != 0) {
+                StopDistancesDescriptionJson input_stop_dist;
+                input_stop_dist.stop_name = base_req.GetRoot().AsMap().at("name").AsString();
+                auto dist_in_stop = base_req.GetRoot().AsMap().at("road_distances");
+                for (auto el : dist_in_stop) {
+                    input_stop_dist.distances.push_back({el.first, el.second})
+                }
+
+                distances.push_back(input_stop_dist);
+            }
+            upd_req_stop.push_back(input_stop);
+            
+
+        }
+        else {
+            BusDescriptionJson input_bus;
+            input_bus.bus_name = base_req.GetRoot().AsMap().at("name").AsString();
+            input_bus.type = base_req.GetRoot().AsMap().at("is_roundtrip").AsString()[0];
+            auto stops_in_bus  = base_req.GetRoot().AsMap().at("stops").AsArray();
+            for (auto el : stops_in_bus) {
+                input_bus.stops.push_back(el);
+            }
+            upd_req_bus.push_back(input_bus);
+
+        }
     }
 
-    //json::Document stat_req = LoadJSON(stat_r);
-    //json::Document base_req = LoadJSON(base_r);
+
+    /*for (string s : stat_result) {
+        cout << "stat" << endl;
+        json::Document stat_req = LoadJSON(s); Print(stat_req, cout); cout << endl;
+        auto id_s =  stat_req.GetRoot().AsMap().at("id");
+        cout << stat_req.GetRoot().AsMap().at("id").AsInt() << endl;
+    }
+
+    for (string s : base_result) {
+        cout << "base" << endl;
+        json::Document base_req = LoadJSON(s);  Print(base_req, cout); cout << endl;
+        cout << base_req.GetRoot().AsMap().at("type").AsString() << endl;
+        cout << base_req.GetRoot().AsMap().at("name").AsString() << endl;
+    }*/
+
     
     
     //cout << d.GetRoot().AsString() << endl;
