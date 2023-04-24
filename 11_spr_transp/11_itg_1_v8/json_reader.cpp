4a1a3fc23ca8e5cd66b641b9333768bef0794e60
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-04-24 19:19:14 +0300

    Add files via upload

---
+++ 11_spr_transp/11_itg_1_v8/json_reader.cpp
@@ -0,0 +1,153 @@
+#include "transport_catalogue.h"
+#include "json.h"
+#include "geo.h"
+#include "json_reader.h"
+
+
+using namespace json;
+using namespace std;
+
+
+namespace transport_catalogue {
+
+
+	InputReaderJson::InputReaderJson(istream& is) : is_(is) {
+	}
+
+	void InputReaderJson::ReadInputJsonRequest() {
+
+		using namespace std::literals;
+		//auto doc = json::Load(std::cin);
+		auto doc = json::Load(is_);
+		const auto& json_array = ((doc.GetRoot()).AsMap()).at("base_requests"s);
+
+		for (const auto& file : json_array.AsArray()) {
+			const auto& json_obj = file.AsMap();
+			if (json_obj.at("type"s) == "Stop"s) {
+				Stop stopjson;
+				stopjson.stop_name = json_obj.at("name").AsString();
+				stopjson.coordinates.lat = json_obj.at("latitude").AsDouble();
+				stopjson.coordinates.lng = json_obj.at("longitude").AsDouble();
+
+				StopDistancesDescription input_stop_dist;
+				input_stop_dist.stop_name = json_obj.at("name").AsString();
+				auto heighbors = json_obj.at("road_distances");
+
+				for (auto el : heighbors.AsMap()) {
+					input_stop_dist.distances.push_back(make_pair(el.first, el.second.AsInt()));
+
+				}
+				upd_req_stop_.push_back(stopjson);
+				distances_.push_back(input_stop_dist);
+			}
+			else if (json_obj.at("type"s) == "Bus"s) {
+				BusDescription bs;
+				auto stop_list = json_obj.at("stops");
+				for (auto el : stop_list.AsArray()) {
+
+					bs.stops.push_back(el.AsString());
+
+				}
+				bs.bus_name = json_obj.at("name").AsString();
+
+				if (json_obj.at("is_roundtrip").AsBool()) { bs.type = "true"s; }
+				else { bs.type = "false"s; }
+				upd_req_bus_.push_back(bs);
+
+			}
+		}
+
+	}
+
+	void InputReaderJson::ReadOutputJsonRequest() {
+
+		using namespace std::literals;
+		auto doc = json::Load(std::cin);
+		const auto& json_array = ((doc.GetRoot()).AsMap()).at("stat_requests"s);
+
+		for (const auto& file : json_array.AsArray()) {
+			const auto& json_obj = file.AsMap();
+			OutputRequest outputstopjson;
+			outputstopjson.name = json_obj.at("name").AsString();
+			outputstopjson.id = json_obj.at("id").AsInt();
+			outputstopjson.type = json_obj.at("type").AsString();
+			out_req_.push_back(outputstopjson);
+		}
+	}
+
+	void InputReaderJson::UpdStop(TransportCatalogue& tc) {
+		for (int i = 0; i < upd_req_stop_.size(); ++i) {
+			tc.AddStop(upd_req_stop_[i]);
+		}
+	}
+
+	void InputReaderJson::UpdStopDist(TransportCatalogue& tc) {
+		for (int i = 0; i < distances_.size(); ++i) {
+			tc.AddStopDistance(distances_[i]);
+		}
+	}
+
+	void InputReaderJson::UpdBus(TransportCatalogue& tc) {
+		for (int i = 0; i < upd_req_bus_.size(); ++i) {
+			tc.AddBus(upd_req_bus_[i]);
+		}
+	}
+
+	void InputReaderJson::ManageOutputRequests(TransportCatalogue& tc) {
+		std::ostream& out = std::cout;
+		json::Array queries;
+		for (auto el : out_req_) {
+			if (el.type == "Bus"s) {
+				AllBusInfoBusResponse r = tc.GetAllBusInfo(el.name);
+				if (r.quant_stops == 0) {
+					json::Node answer_empty_bus = json::Dict{
+							{"request_id", el.id},
+							{"error_message" ,  "not found"}
+					};
+					queries.emplace_back(answer_empty_bus);
+
+				}
+				else {
+					json::Node non_empty_bus = json::Dict{
+							{"curvature", r.route_curvature},
+							{"request_id" , el.id},
+							{"route_length" , r.route_length},
+							{"stop_count" , r.quant_stops},
+							{"unique_stop_count" , r.quant_uniq_stops}
+					};
+					queries.emplace_back(non_empty_bus);
+				}
+			}
+
+			if (el.name == "Stop"s) {
+				const Stop* myStop = tc.FindStop(el.name);
+				if (myStop == nullptr) {
+					json::Node answer_empty_stop = json::Dict{
+							{"request_id", el.id},
+							{"error_message" ,  "not found"}
+					};
+					queries.emplace_back(answer_empty_stop);
+
+				}
+				else {
+					set<string> r = tc.GetStopInfo(el.name);
+					json::Array routes;
+					std::copy(r.begin(), r.end(), std::back_inserter(routes));
+					json::Node answer_stop = json::Dict{
+						{"buses", routes},
+						{"request_id" ,  el.id}
+
+					};
+					queries.emplace_back(answer_stop);
+
+					
+				}
+			}
+
+
+		}
+		json::Print(json::Document{ queries }, out);
+
+	};
+
+}
---
+++ 11_spr_transp/11_itg_1_v8/main.cpp
@@ -0,0 +1,154 @@
+﻿#include "geo.h"
+#include "transport_catalogue.h"
+#include "json_reader.h"
+#include "iostream"
+
+using namespace std;
+
+
+// проверка можно ли закинуть струкутру с ответом в json
+struct AllBusInfoBusResponse {
+	int id = 25;
+	std::string bus_name = "BB";
+	int quant_stops = 10;
+	int quant_uniq_stops = 9;
+	double route_length = 10;
+	double route_curvature = 1.30;
+};
+
+
+
+
+
+// тут проверочный запуск функции вне класса 
+
+struct OutputRequestStop {
+	int id;
+	std::string stop_name;
+};
+
+struct OutputRequestBus {
+	int id;
+	std::string bus_name;
+};
+
+struct StopDistancesDescription {
+	std::string stop_name;
+	std::vector<std::pair<std::string, int>> distances;
+};
+
+struct Stop {
+	std::string stop_name;
+	geo::Coordinates coordinates;
+
+};
+
+struct BusDescription {
+	std::string bus_name;
+	std::vector<std::string> stops;
+	std::string type;
+};
+
+
+void ReadInputJsonRequest() {
+
+	using namespace std::literals;
+	auto doc = json::Load(std::cin);
+
+	const auto& json_array = ((doc.GetRoot()).AsMap()).at("base_requests"s);
+
+	for (const auto& file : json_array.AsArray()) {
+		const auto& json_obj = file.AsMap();
+		if (json_obj.at("type"s) == "Stop"s) {
+			Stop stopjson;
+			stopjson.stop_name = json_obj.at("name").AsString();
+			stopjson.coordinates.lat = json_obj.at("latitude").AsDouble();
+			stopjson.coordinates.lng = json_obj.at("longitude").AsDouble();
+
+			cout << "stopjson.stop_name: " << stopjson.stop_name << " stopjson.coordinates.lat :" << stopjson.coordinates.lat << " stopjson.coordinates.lng :" << stopjson.coordinates.lng << endl;
+
+			StopDistancesDescription input_stop_dist;
+			input_stop_dist.stop_name = json_obj.at("name").AsString();
+			auto heighbors = json_obj.at("road_distances");
+
+			for (auto el : heighbors.AsMap()) {
+				input_stop_dist.distances.push_back(make_pair(el.first, el.second.AsInt()));
+				cout << "el.first : " << el.first << " el.second.AsInt() : " << el.second.AsInt() << endl;
+			}
+
+		}
+		else if (json_obj.at("type"s) == "Bus"s) {
+			BusDescription bs;
+			auto stop_list = json_obj.at("stops");
+			for (auto el : stop_list.AsArray()) {
+
+				bs.stops.push_back(el.AsString());
+
+				cout << "el.AsString() : " << el.AsString() << endl;
+
+			}
+			bs.bus_name = json_obj.at("name").AsString();
+			cout << "bus_name : " << json_obj.at("name").AsString()  << endl;
+
+			if (json_obj.at("is_roundtrip").AsBool()) { bs.type = "true"s; }
+			else { bs.type = "false"s; }
+
+			cout << "bs.type: "s  << bs.type << endl;
+
+		}
+	}
+
+}
+struct OutputRequestTest {
+	int id = 11;
+	std::string type = "Bus";
+	std::string name = "FRGR_BUS";
+};
+
+int main()
+{
+	std::ostream& out = std::cout;
+	AllBusInfoBusResponse tr;
+	OutputRequestTest req;
+	json::Array queries;
+	cout << "tr.bus_name = " << tr.bus_name << endl;
+
+	set<string> r{ "value1", "value2", "value3" };
+	json::Array routes; 
+	std::copy(r.begin(), r.end(), std::back_inserter(routes));
+
+
+	json::Node answer_stop = json::Dict{
+	{"buses", routes},
+	{"request_id" ,  tr.id}
+
+	};
+	queries.emplace_back(answer_stop);
+
+	json::Node res = json::Dict{
+		{"curvature", tr.route_curvature},
+		{"request_id" , tr.id},
+		{"route_length" , tr.route_length},
+		{"stop_count" , tr.quant_stops},
+		{"unique_stop_count" , tr.quant_uniq_stops}
+	};
+	queries.emplace_back(res);
+	cout << "PRINT JSON :" << endl;
+	json::Print(json::Document{ queries }, out);
+
+	ReadInputJsonRequest();
+
+
+	/*transport_catalogue::TransportCatalogue tc;
+	transport_catalogue::InputReaderJson reader(std::cin);
+	(void)reader.ReadInputJsonRequest();
+	(void)reader.ReadOutputJsonRequest();
+
+	reader.UpdStop(tc);
+	reader.UpdBus(tc);
+	reader.UpdStopDist(tc);*/
+
+
+	return 0;
+}
+
