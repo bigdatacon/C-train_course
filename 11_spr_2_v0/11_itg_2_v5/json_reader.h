0c7315ae2b5d80f3ed06bb7b61d3cf891203879c
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-04-27 15:43:52 +0300

    Add files via upload

---
+++ 11_spr_2_v0/11_itg_2_v5/json_reader.h
@@ -0,0 +1,70 @@
+#pragma once
+
+#include <sstream>
+#include <string>
+#include <deque>
+#include <iostream>
+#include <vector>
+
+#include "transport_catalogue.h"
+#include "json.h"
+#include "geo.h"
+#include "map_renderer.h"
+
+
+using namespace json;
+using namespace std::literals;
+
+
+
+inline json::Document LoadJSON(const std::string& s) {
+    std::istringstream strm(s);
+    return json::Load(strm);
+}
+
+inline std::string Print(const Node& node) {
+    std::ostringstream out;
+    Print(json::Document{ node }, out);
+    return out.str();
+}
+
+
+namespace transport_catalogue {
+
+
+    class InputReaderJson {
+    public:
+        explicit InputReaderJson(std::istream& is);
+
+        void ReadInputJsonRequest();
+
+        void UpdStop(TransportCatalogue& tc);
+
+        void UpdBus(TransportCatalogue& tc);
+
+        void UpdStopDist(TransportCatalogue& tc);
+
+        void ManageOutputRequests(TransportCatalogue& tc);
+
+        Render_data GetRenderData();
+
+
+
+    private:
+        std::istream& is_;
+
+        std::deque<OutputRequest> out_req_;
+
+        std::deque<BusDescription> upd_req_bus_;
+        std::deque<Stop> upd_req_stop_;
+        std::vector<StopDistancesDescription> distances_;
+        Render_data render_data_;
+
+    };
+
+}
+
+
+
+
+
