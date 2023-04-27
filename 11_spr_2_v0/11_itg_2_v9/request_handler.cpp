01421278676bbce05a46873582faf8bddcbee9e4
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-04-27 23:13:06 +0300

    Add files via upload

---
+++ 11_spr_2_v0/11_itg_2_v9/request_handler.cpp
@@ -0,0 +1,22 @@
+#include "request_handler.h"
+
+/*
+ * Здесь можно было бы разместить код обработчика запросов к базе, содержащего логику, которую не
+ * хотелось бы помещать ни в transport_catalogue, ни в json reader.
+ *
+ * Если вы затрудняетесь выбрать, что можно было бы поместить в этот файл,
+ * можете оставить его пустым.
+ */
+
+
+    std::deque<transport_catalogue::Bus>  RequestToTc::GetBuses() {
+        return TransportCatalogue::GetBuses();
+
+    };
+    std::deque<transport_catalogue::Stop>  RequestToTc::GetStops() {
+        return TransportCatalogue::GetStops();
+    };
+
+    const transport_catalogue::Stop* RequestToTc::FindStop(std::string_view stop) {
+        return TransportCatalogue::FindStop(stop);
+    };
