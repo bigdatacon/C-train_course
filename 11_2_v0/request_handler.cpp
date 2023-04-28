d84e2cd454d07f01c6571c441c6967cf02a01980
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-04-28 22:54:38 +0300

    Add files via upload

---
+++ 11_2_v0/request_handler.cpp
@@ -0,0 +1,25 @@
+#include "request_handler.h"
+
+/*
+ * Здесь можно было бы разместить код обработчика запросов к базе, содержащего логику, которую не
+ * хотелось бы помещать ни в transport_catalogue, ни в json reader.
+ *
+ * Если вы затрудняетесь выбрать, что можно было бы поместить в этот файл,
+ * можете оставить его пустым.
+ */
+
+
+RequestHandler::RequestHandler( TransportCatalogue& tc,  /*renderer::*/ MapRenderer& renderer) : tc_(tc), renderer_(renderer) {}
+
+// Возвращает информацию о маршруте (запрос Bus)
+//std::optional<BusStat> GetBusStat(const std::string_view& bus_name) const;
+
+// Возвращает маршруты, проходящие через
+//const std::unordered_set<BusPtr>* GetBusesByStop(const std::string_view& stop_name) const;
+
+// Этот метод будет нужен в следующей части итогового проекта
+void RequestHandler::RenderMap()  {
+    
+    renderer_.DrawRoute(tc_);
+}
+
