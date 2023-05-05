3855a90dba768840c7abf7d6967fef0d3a6a4885
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-05-05 12:36:39 +0300

    Update json_builder.cpp

--- spr_12/json_1/12_1_0/json_builder.cpp
+++ spr_12/json_1/12_1_0/json_builder.cpp
@@ -5,92 +5,101 @@
 namespace json {
 
 
-        Node Builder::Build() const {
-            return root_;
+    Node Builder::Build() const {
+        /*if (nodes_stack_.size() != 1) {
+            throw std::logic_error("Build called with incomplete object");
+        }*/
+        //is_built_ = true;
+        return root_;
+    }
+
+
+    Builder& Builder::Key(const std::string& key) {
+        if (!is_dict_) {
+            throw std::logic_error("Call Key method behinde dict.");
         }
-
-
-        Builder& Builder::Key(const std::string& key) {
-            if (in_dict()) {
-                current_key_ = key;
-            }
-            else {
-                throw std::logic_error("Get not a dict in Key func.");
-            }
-            return *this;
+        if (in_dict()) {
+            current_key_ = key;
         }
-
-        Builder& Builder::StartDict() {
-            if (in_array()) {
-                nodes_stack_.back()->AsArray().push_back(json::Dict());
-                nodes_stack_.push_back(&nodes_stack_.back()->AsArray().back());
-            }
-            else if (in_dict()) {
-                nodes_stack_.back()->AsDict()[current_key_] = Dict();
-                nodes_stack_.push_back(&nodes_stack_.back()->AsDict()[current_key_]);
-                current_key_.clear();
-            }
-            else {
-                root_ = Dict();
-                nodes_stack_.push_back(&root_);
-            }
-            return *this;
+        else {
+            throw std::logic_error("Get not a dict in Key func.");
         }
+        return *this;
+    }
 
-        Builder& Builder::EndDict() {
-            if (in_dict()) {
-                nodes_stack_.pop_back();
-            }
-            else {
-                throw std::logic_error("Get not a EndDict in Key func.");
-            }
-            return *this;
+    Builder& Builder::StartDict() {
+        if (in_array()) {
+            nodes_stack_.back()->AsArray().push_back(json::Dict());
+            nodes_stack_.push_back(&nodes_stack_.back()->AsArray().back());
         }
-
-        Builder& Builder::StartArray() {
-            if (in_array()) {
-                nodes_stack_.back()->AsArray().push_back(Array());
-                nodes_stack_.push_back(&nodes_stack_.back()->AsArray().back());
-            }
-            else if (in_dict()) {
-                nodes_stack_.back()->AsDict()[current_key_] = Array();
-                nodes_stack_.push_back(&nodes_stack_.back()->AsDict()[current_key_]);
-                current_key_.clear();
-            }
-            else {
-                root_ = Array();
-                nodes_stack_.push_back(&root_);
-            }
-            return *this;
+        else if (in_dict()) {
+            nodes_stack_.back()->AsDict()[current_key_] = Dict();
+            nodes_stack_.push_back(&nodes_stack_.back()->AsDict()[current_key_]);
+            current_key_.clear();
         }
-
-        Builder& Builder::EndArray() {
-            if (in_array()) {
-                nodes_stack_.pop_back();
-            }
-            else {
-                throw std::logic_error("Get not a EndArray in Key func.");
-            }
-            return *this;
+        else {
+            root_ = Dict();
+            nodes_stack_.push_back(&root_);
         }
+        is_dict_ = true;
+        return *this;
+    }
 
-
-        bool Builder::in_array() {
-            if (!nodes_stack_.empty()) {
-                if (nodes_stack_.back()->IsArray()) {
-                    return true;
-                }
-            }
-            return false;
+    Builder& Builder::EndDict() {
+        if (in_dict()) {
+            nodes_stack_.pop_back();
         }
-
-        bool Builder::in_dict() {
-            if (!nodes_stack_.empty()) {
-                if (nodes_stack_.back()->IsDict()) {
-                    return true;
-                }
-            }
-            return false;
+        else {
+            throw std::logic_error("Get not a EndDict in Key func.");
         }
+        is_dict_ = false;
+        return *this;
+    }
+
+    Builder& Builder::StartArray() {
+        if (in_array()) {
+            nodes_stack_.back()->AsArray().push_back(Array());
+            nodes_stack_.push_back(&nodes_stack_.back()->AsArray().back());
+        }
+        else if (in_dict()) {
+            nodes_stack_.back()->AsDict()[current_key_] = Array();
+            nodes_stack_.push_back(&nodes_stack_.back()->AsDict()[current_key_]);
+            current_key_.clear();
+        }
+        else {
+            root_ = Array();
+            nodes_stack_.push_back(&root_);
+        }
+        return *this;
+    }
+
+    Builder& Builder::EndArray() {
+        if (in_array()) {
+            nodes_stack_.pop_back();
+        }
+        else {
+            throw std::logic_error("Get not a EndArray in Key func.");
+        }
+        return *this;
+    }
+
+
+    bool Builder::in_array() {
+        if (!nodes_stack_.empty()) {
+            if (nodes_stack_.back()->IsArray()) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    bool Builder::in_dict() {
+        if (!nodes_stack_.empty()) {
+            if (nodes_stack_.back()->IsDict()) {
+                return true;
+            }
+        }
+        return false;
+    }
 
 }
