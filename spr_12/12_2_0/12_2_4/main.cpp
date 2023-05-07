6da903b02a6c07e6333d81c08474eec584789520
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-05-07 20:10:08 +0300

    Update main.cpp

--- spr_12/12_2_0/12_2_4/main.cpp
+++ spr_12/12_2_0/12_2_4/main.cpp
@@ -5,9 +5,13 @@
 using namespace std;
 
 int main() {
- json::Builder{}.StartDict().Build();
-    json::Builder{}.StartDict().StartArray();
-    json::Builder{}.StartDict().Value();
-        
+    //json::Builder{}.StartDict().Build();
+    //json::Builder{}.StartDict().StartArray();
+    //json::Builder{}.StartDict().Value();
 
+    json::Builder b;
+    b.StartDict().Key("key").Value(1).Value(2).EndDict();
+    b.StartDict().Key("key").Value(1).EndArray().EndDict();
+    b.StartDict().Key("key").Value(1).Build();
+    b.StartDict().Key("key").Value(1).StartArray().EndDict();
 }
