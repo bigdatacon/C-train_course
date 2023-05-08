309e3567b8aeacd29aaa53bac17349cc40795b27
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-05-08 13:29:35 +0300

    Add files via upload

---
+++ spr_12/12_3_0/12_3_1_not_work/main.cpp
@@ -0,0 +1,58 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include "geo.h"
+#include "transport_catalogue.h"
+#include "json_reader.h"
+#include "iostream"
+#include "map_renderer.h"
+#include "request_handler.h"
+#include <fstream>
+using namespace transport_catalogue;
+using namespace std;
+#include <chrono>
+
+
+int main() {
+#ifdef _DEBUG
+	if (freopen("input.txt.txt", "r", stdin) == nullptr) {
+		puts("can't open input.txt.txt");
+		return 1;
+	}
+#endif 
+	
+	
+	transport_catalogue::TransportCatalogue tc;
+	transport_catalogue::InputReaderJson reader(std::cin);
+	(void)reader.ReadInputJsonRequest();
+
+	reader.UpdStop(tc);
+	reader.UpdBus(tc);
+	reader.UpdStopDist(tc);
+	RenderData rd = reader.GetRenderData();
+	MapRenderer mapdrawer(rd);
+	reader.ManageOutputRequests(tc, mapdrawer);
+	
+	
+	
+	/*
+	transport_catalogue::TransportCatalogue tc;
+	transport_catalogue::InputReaderJson reader(std::cin);
+	(void)reader.ReadInputJsonRequest();
+
+	reader.UpdStop(tc);
+	reader.UpdBus(tc);
+	reader.UpdStopDist(tc);
+
+
+	RenderData rd = reader.GetRenderData();
+
+	MapRenderer mapdrawer(rd);
+	RequestHandler reqhandleq(tc, mapdrawer);
+	//reqhandleq.RenderMap();
+	reqhandleq.RenderMapByString();
+	
+	*/
+
+	return 0;
+
+
+}
