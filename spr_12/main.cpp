edb41e7a2c20a9211072e82533a43389ea60b679
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-05-11 14:20:52 +0300

    Update main.cpp

--- spr_12/main.cpp
+++ spr_12/main.cpp
@@ -1,182 +1,126 @@
-#include <cassert>
-#include <filesystem>
-#include <fstream>
+#define _CRT_SECURE_NO_WARNINGS
+#include <algorithm>
 #include <iostream>
-#include <regex>
-#include <sstream>
 #include <string>
-#include <string_view>
 #include <vector>
+#include <set>
+#include <numeric>
 
 using namespace std;
-using filesystem::path;
-namespace fs = std::filesystem;
 
-path operator""_p(const char* data, std::size_t sz) {
-    return path(data, data + sz);
-}
+/*
+12
+CHEER 5
+READ 1 10
+CHEER 1
+READ 2 5
+READ 3 7
+CHEER 2
+CHEER 3
+READ 3 10
+CHEER 3
+READ 3 11
+CHEER 3
+CHEER 1
+*/
 
+/*
+0
+1
+0
+0.5
+0.5
+1
+0.5
+*/
 
-bool Preprocess(const path& in_file, const path& out_file, const vector<path>& include_directories);
+class BookManager {
 
-string GetFileContents(string file) {
-    ifstream stream(file);
-    return { (istreambuf_iterator<char>(stream)), istreambuf_iterator<char>() };
-}
+public:
+    BookManager() {
+        quant_users_to_page_.resize(12);
+    };
+    void ReadInput(istream&  cin) {
+        int n;
+        cin >> n;
+        user_and_page_.resize(n + 1);
+        for (int i = 1; i <= n; i++) {
+            string op;
+            cin >> op;
+            if (op == "CHEER") {
+                int user_id;
+                cin >> user_id;
 
-bool ProcessInclude(const smatch& match, const path& current_file_path, const vector<path>& include_directories, stringstream& result, int line_number) {
-    string include_path_str = match[1].str();
-    path include_path;
+                // Проверка наличия элемента в векторе
+                if (!users_.count(user_id)) {
+                    cout << 0 << endl;
+                }
+                else {
+                    int current_page = user_and_page_[user_id];
+                    if (current_page != 0) {
+                        std::vector<int> slice(quant_users_to_page_.begin(), quant_users_to_page_.begin() + current_page);
 
-    // Check if it's a relative path include directive
-    if (include_path_str[0] == '.') {
-        include_path = current_file_path.parent_path() / include_path_str;
-        if (!fs::exists(include_path)) {
-            for (const auto& dir : include_directories) {
-                include_path = dir / include_path_str;
-                if (fs::exists(include_path)) {
-                    break;
+                        // вычисление суммы элементов среза
+                        double sum = std::accumulate(slice.begin(), slice.end(), 0);
+
+                        double part = sum / (users_.size()-1);
+                        if (users_.size() == 1) { cout << 1 << endl; }
+                        else {
+
+                            cout << part << endl;
+                        }
+                    }
+                    else {
+
+                        cout << 0 << endl;
+                    }
+
                 }
             }
-        }
-    }
-    else { // It's a system path include directive
-        for (const auto& dir : include_directories) {
-            include_path = dir / include_path_str;
-            if (fs::exists(include_path)) {
-                break;
+            else if (op == "READ") {
+                    int user_id, num_page;
+                    cin >> user_id >> num_page;
+                    // отнимаю юзера от предыдущей страницы чтобы не было задвоения 
+                    if (user_and_page_[user_id]!=0) {
+                        quant_users_to_page_[user_and_page_[user_id]] -= 1;
+
+                    }
+
+                    // добавляю новые данные
+                    user_and_page_[user_id] = num_page; // по индексу страницы записываю юзера 
+                    quant_users_to_page_[num_page] += 1; // увеличиваю количество дочитавших до стр на 1
+                    if (!users_.count(user_id)) {
+                        users_.insert(user_id);
+                    }
+
+
+
+
+                }
             }
+
         }
-    }
+    
 
-    if (!fs::exists(include_path)) {
-        cout << "unknown include file " << include_path_str << " at file " << current_file_path.string() << " at line " << line_number << endl;
-        return false;
-    }
-
-    ifstream include_file(include_path);
-    if (!include_file) {
-        cout << "unknown include file " << include_path_str << " at file " << current_file_path.string() << " at line " << line_number << endl;
-        return false;
-    }
-
-    result << GetFileContents(include_path.string()) << endl;
-
-    return true;
-}
+private:
+    vector<int> user_and_page_;
+    vector<int> quant_users_to_page_;
+    set<int> users_;
 
 
-bool Preprocess(const path& in_file, const path& out_file, const vector<path>& include_directories) {
-    ifstream input(in_file);
-    if (!input) {
-        return false;
-    }
 
-    ofstream output(out_file);
-    if (!output) {
-        return false;
-    }
-
-    stringstream result;
-    string line;
-    int line_number = 1;
-    regex include_regex(R"/(\s*#\s*include\s*"([^"]*)"\s*)/");
-    regex system_include_regex(R"/(\s*#\s*include\s*<([^>]*)>\s*)/");
-    smatch include_match;
-
-    while (getline(input, line)) {
-        if (regex_search(line, include_match, include_regex)) {
-            if (!ProcessInclude(include_match, in_file, include_directories, result, line_number)) {
-                return false;
-            }
-        }
-        else if (regex_search(line, include_match, system_include_regex)) {
-            if (!ProcessInclude(include_match, in_file, include_directories, result, line_number)) {
-                return false;
-            }
-        }
-        else {
-            result << line << endl;
-        }
-
-        line_number++;
-    }
-
-    output << result.str();
-
-    return true;
-
-
-}
-
-
-void Test() {
-    error_code err;
-    filesystem::remove_all("sources"_p, err);
-    filesystem::create_directories("sources"_p / "include2"_p / "lib"_p, err);
-    filesystem::create_directories("sources"_p / "include1"_p, err);
-    filesystem::create_directories("sources"_p / "dir1"_p / "subdir"_p, err);
-
-    {
-        ofstream file("sources/a.cpp");
-        file << "// this comment before include\n"
-            "#include \"dir1/b.h\"\n"
-            "// text between b.h and c.h\n"
-            "#include \"dir1/d.h\"\n"
-            "\n"
-            "int SayHello() {\n"
-            "    cout << \"hello, world!\" << endl;\n"
-            "#   include<dummy.txt>\n"
-            "}\n"sv;
-    }
-    {
-        ofstream file("sources/dir1/b.h");
-        file << "// text from b.h before include\n"
-            "#include \"subdir/c.h\"\n"
-            "// text from b.h after include"sv;
-    }
-    {
-        ofstream file("sources/dir1/subdir/c.h");
-        file << "// text from c.h before include\n"
-            "#include <std1.h>\n"
-            "// text from c.h after include\n"sv;
-    }
-    {
-        ofstream file("sources/dir1/d.h");
-        file << "// text from d.h before include\n"
-            "#include \"lib/std2.h\"\n"
-            "// text from d.h after include\n"sv;
-    }
-    {
-        ofstream file("sources/include1/std1.h");
-        file << "// std1\n"sv;
-    }
-    {
-        ofstream file("sources/include2/lib/std2.h");
-        file << "// std2\n"sv;
-    }
-
-    assert((!Preprocess("sources"_p / "a.cpp"_p, "sources"_p / "a.in"_p,
-        { "sources"_p / "include1"_p,"sources"_p / "include2"_p })));
-
-    ostringstream test_out;
-    test_out << "// this comment before include\n"
-        "// text from b.h before include\n"
-        "// text from c.h before include\n"
-        "// std1\n"
-        "// text from c.h after include\n"
-        "// text from b.h after include\n"
-        "// text between b.h and c.h\n"
-        "// text from d.h before include\n"
-        "// std2\n"
-        "// text from d.h after include\n"
-        "\n"
-        "int SayHello() {\n"
-        "    cout << \"hello, world!\" << endl;\n"sv;
-
-    assert(GetFileContents("sources/a.in"s) == test_out.str());
-}
+};
 
 int main() {
-    Test();
+#ifdef _DEBUG
+    if (freopen("input.txt.txt", "r", stdin) == nullptr) {
+        puts("can't open input.txt.txt");
+        return 1;
+    }
+#endif 
+
+    BookManager bm;
+    bm.ReadInput(cin);
+
+    return 0;
 }
