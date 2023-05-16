d1fc813ef3346a04144dc305eb7b27ce73fa6c54
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-05-16 23:16:42 +0300

    Update v_2.cpp

--- spr_13/v_2.cpp
+++ spr_13/v_2.cpp
@@ -132,7 +132,10 @@ public:
     // разработайте метод IsForbidden, возвращающий true, если домен запрещён
     bool IsForbidden(const Domain& domain) const {
         //return std::find(forbiddenDomains_.begin(), forbiddenDomains_.end(), domain) != forbiddenDomains_.end() ;
-        return  binary_search(forbiddenDomains_, domain);
+        int res =  binary_search(forbiddenDomains_, domain);
+        if (res == -1) { return false; }
+        else { return true; }
+
     }
 
 private:
