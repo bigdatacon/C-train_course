f6c8666a3703a368cbf9fa8c66433ebe0c1894d7
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-05-18 00:17:40 +0300

    Update budget_manager.cpp

--- spr_13/spr_13_1_1/budget_manager.cpp
+++ spr_13/spr_13_1_1/budget_manager.cpp
@@ -11,40 +11,49 @@
 
 using namespace std;
 
+
+
+
+
 BudgetManager::BudgetManager() : days_(Date::ComputeDistance(START_DATE, END_DATE)) {
 }
 
 double  BudgetManager::ComputeIncome(const Date date_from, const Date date_to) {
     int initial_index = Date::ComputeDistance(BudgetManager::START_DATE, date_from);
-    int quant_days = Date::ComputeDistance(date_from, date_to);
-    double sum = std::accumulate(days_.begin() + initial_index, days_.begin() + initial_index + quant_days + 1, 0.0, [](double acc, const DayInfo& day_info) {
+    //int quant_days = Date::ComputeDistance(date_from, date_to);
+    int end_index = Date::ComputeDistance(BudgetManager::START_DATE, date_to);
+    double sum = std::accumulate(days_.begin() + initial_index, days_.begin() +  end_index+1, 0.0, [](double acc, const DayInfo& day_info) {
         return acc + day_info.value;
         });
+    
     return sum;
 
 }
 
 void  BudgetManager::Earn(const DayInfo& initial_info, int start_index) {
+    
     if (days_[start_index].value) {
         days_[start_index].value += initial_info.value;
-    
+
     }
     else {
-        days_.insert(days_.begin() + start_index, initial_info);
+        days_[start_index] =  initial_info;
     }
+
+
 }
 
 void  BudgetManager::PayTax(const Date date_from, const Date date_to) {
     int initial_index = Date::ComputeDistance(BudgetManager::START_DATE, date_from);
     int quant_days = Date::ComputeDistance(date_from, date_to);
-    //double sum = ComputeIncome(date_from, date_to);
+    double sum = ComputeIncome(date_from, date_to);
     //std::cout << "sum before tax :" << sum << std::endl;
 
     for (int i = initial_index; i < initial_index + quant_days+1; ++i) {
         days_[i].value *=(1- DEFAULT_TAX_RATE);
 
     }
-    //double sum_after = ComputeIncome(date_from, date_to);
+    double sum_after = ComputeIncome(date_from, date_to);
     //std::cout << "Pay  tax of sum :" << sum - sum_after << std::endl;
 
 }
