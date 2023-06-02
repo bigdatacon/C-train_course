1dc4fefcf33b8f8160f68f0017bb9a3c61acb63a
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-06-02 11:14:01 +0300

    Add files via upload

---
+++ 13_fin/13_fin_19_vcs/domain.h
@@ -0,0 +1,50 @@
+#pragma once
+#include "geo.h"
+
+#include <string>
+#include <vector>
+#include "deque"
+
+namespace domain {
+
+	struct StopDistancesDescription {
+		std::string stop_name;
+		std::vector<std::pair<std::string, int>> distances;
+	};
+
+	struct Stop {
+		std::string stop_name;
+		geo::Coordinates coordinates;
+
+	};
+
+	struct BusDescription {
+		std::string bus_name;
+		std::vector<std::string> stops;
+		std::string type;
+	};
+
+	struct AllBusInfoBusResponse {
+		std::string bus_name;
+		int quant_stops;
+		int quant_uniq_stops;
+		double route_length = 0;
+		double route_curvature = 0;
+	};
+
+
+
+	struct Bus {
+		std::string bus_name;
+		//std::deque<std::string*> stops;
+		std::deque<std::string_view> stops;
+		std::string type;
+	};
+
+	struct RouteSettings {
+		double bus_velocity;
+		double bus_wait_time;
+
+	};
+
+}
