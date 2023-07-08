98ddb1a33e1c6770067a8f56263f7e6158ed2b6a
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-07-08 20:03:36 +0300

    Add files via upload

---
+++ 16_CPP/final/16_class_design_aft_review_4/FormulaAST.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "FormulaLexer.h"
+#include "common.h"
+
+#include <forward_list>
+#include <functional>
+#include <stdexcept>
+
+namespace ASTImpl {
+    class Expr;
+}
+
+class ParsingError : public std::runtime_error {
+    using std::runtime_error::runtime_error;
+};
+
+class FormulaAST {
+public:
+    explicit FormulaAST(std::unique_ptr<ASTImpl::Expr> root_expr);
+    FormulaAST(FormulaAST&&) = default;
+    FormulaAST& operator=(FormulaAST&&) = default;
+    ~FormulaAST();
+
+    double Execute(std::function<double(Position* pos)> func) const;
+    void Print(std::ostream& out) const;
+    void PrintFormula(std::ostream& out) const;
+
+    std::list<Position>& GetCells();
+
+private:
+    std::unique_ptr<ASTImpl::Expr> root_expr_;
+    std::list<Position> cells_;
+};
+
+FormulaAST ParseFormulaAST(std::istream& in);
+FormulaAST ParseFormulaAST(const std::string& in_str);
