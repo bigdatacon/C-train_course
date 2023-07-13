55a4899a3b9f86e33d037a957b5200627299bf80
bigdatacon <53650079+bigdatacon@users.noreply.github.com>
2023-07-13 14:00:03 +0300

    Update main.cpp

--- 16_CPP/final/main.cpp
+++ 16_CPP/final/main.cpp
@@ -157,10 +157,11 @@ namespace {
         sheet->SetCell("A2"_pos, "2");
         ASSERT_EQUAL(evaluate("A1+A2"), 3);
 
+        // Тест на нули:
         sheet->SetCell("B3"_pos, "");
-        ASSERT_EQUAL(evaluate("A1+B3"), 1);
-        ASSERT_EQUAL(evaluate("A1+B1"), 1);
-        ASSERT_EQUAL(evaluate("A1+E4"), 1);
+        ASSERT_EQUAL(evaluate("A1+B3"), 1);  // Ячейка с пустым текстом
+        ASSERT_EQUAL(evaluate("A1+B1"), 1);  // Пустая ячейка
+        ASSERT_EQUAL(evaluate("A1+E4"), 1);  // Ячейка за пределами таблицы
     }
 
     void TestFormulaExpressionFormatting() {
@@ -296,6 +297,7 @@ namespace {
         ASSERT_EQUAL(sheet->GetCell("A2"_pos)->GetReferencedCells(), std::vector{"A1"_pos});
         ASSERT_EQUAL(sheet->GetCell("B2"_pos)->GetReferencedCells(), std::vector{"A1"_pos});
 
+        // Ссылка на пустую ячейку
         sheet->SetCell("B2"_pos, "=B1");
         ASSERT(sheet->GetCell("B1"_pos)->GetReferencedCells().empty());
         ASSERT_EQUAL(sheet->GetCell("B2"_pos)->GetReferencedCells(), std::vector{"B1"_pos});
@@ -304,6 +306,7 @@ namespace {
         ASSERT(sheet->GetCell("A1"_pos)->GetReferencedCells().empty());
         ASSERT(sheet->GetCell("A2"_pos)->GetReferencedCells().empty());
 
+        // Ссылка на ячейку за пределами таблицы
         sheet->SetCell("B1"_pos, "=C3");
         ASSERT_EQUAL(sheet->GetCell("B1"_pos)->GetReferencedCells(), std::vector{"C3"_pos});
     }
@@ -342,8 +345,7 @@ namespace {
         ASSERT(caught);
         ASSERT_EQUAL(sheet->GetCell("M6"_pos)->GetText(), "Ready");
     }
-
-}//end namespace
+}  // namespace
 
 int main() {
     TestRunner tr;
